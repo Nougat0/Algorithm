d1a66816f74d60623640e2da34aa6625b61c6d1f
hmkim <elppapie@naver.com>
2024-06-16 14:48:35 +0900

    :+1: 2839 틀린원인 발견...
    N의 최대값 5000을 5로 나눈 게 최대값이라 생각해서 1001을 넣었는데
    5000/3을 최대값으로 간주해서 그 값으로 minBags를 초기화했어야 했다...
    
    틀린이유: 최대값으로 넣은 1001이 실제 답일 경우 -1을 출력해서 틀림

--- questions/src/main/java/baekjoon/resolved/Main2839.java
+++ questions/src/main/java/baekjoon/resolved/Main2839.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
@@ -6,14 +6,13 @@ public class Main2839 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        while(true) {
-            int n = Integer.parseInt(br.readLine());
 
-            bw.write(checkBags(n) + "\n");
-            bw.flush();
-        }
-//        bw.close();
-//        br.close();
+        int n = Integer.parseInt(br.readLine());
+
+        bw.write(checkBags(n) + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
     /**
@@ -23,17 +22,16 @@ public class Main2839 {
      */
     private static int checkBags(int n) {
         int max5Bag = n / 5;
-        int minBags = 1001;
+        int minBags = 2000;
 
         for(int i=0; i<=max5Bag; i++) {
             int change = n - 5*i;
             if(change % 3 == 0) {
-                System.out.println("5: "+i+", 3: "+change/3+", total: "+(i+change/3));
                 if(minBags > i + (change/3)){
                     minBags = i + (change/3);
                 }
             }
         }
-        return (minBags == 1001 ? -1 : minBags);
+        return (minBags == 2000 ? -1 : minBags);
     }
 }
