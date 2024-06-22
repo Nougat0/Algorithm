db02494547c9e7a39d7277d9988d44730937c5c8
hmkim <elppapie@naver.com>
2024-06-22 12:32:08 +0900

    :+1: 1075번 해결
    매우 간단

---
+++ questions/src/main/java/baekjoon/processing/Main1075.java
@@ -0,0 +1,21 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+
+public class Main1075 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int f = Integer.parseInt(br.readLine());
+        n = n/100 * 100; //백의 자리 밑 나머지 제거
+        while(n % f != 0) n += 1;
+
+        bw.write(String.format("%02d%n", n % 100));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
