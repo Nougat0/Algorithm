4636d64129709c49eb533dbc3eefc68db2687629
hmkim <elppapie@naver.com>
2024-06-23 15:08:12 +0900

    :arrow_right: 1051번 해결중 (반복문 횟수)
    Testcase 중에서 아래를 실행할 때 현재 코드로는 "반복문 실행"이 450번 실행된다
    조금 더 줄일 수 있는 방법 찾는 중
    
    11 10
    9785409507
    2055103694
    0861396761
    3073207669
    1233049493
    2300248968
    9769239548
    7984130001
    1670020095
    8894239889
    4053971072

---
+++ questions/src/main/java/baekjoon/processing/Main1051.java
@@ -0,0 +1,52 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+
+public class Main1051 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        //입력
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] square = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            char[] row = br.readLine().toCharArray();
+            for (int j = 0; j < m; j++) {
+                square[i][j] = Character.getNumericValue(row[j]);
+            }
+        }
+        //출력
+        bw.write(checkSquare(square)+"\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    /**
+     * 가장 큰 정사각형 찾기 (찾다가 정 안되면 1개짜리도 정사각형 정답 취급)
+     * @return 존재하지 않을 경우 1
+     */
+    public static int checkSquare(int[][] square) {
+        int maxEdge = 0;
+        for(int i=0; i<square.length - 1; i++) {
+            for(int j=0; j<square[0].length - 1; j++) {
+                //왼쪽 상단 꼭지점(i,j)에서 사각형 확인
+                for(int k=j+1; k<square[0].length; k++) {
+                    System.out.println("반복문 실행");
+                    if(square[i][j] == square[i][k]) {
+                        int diff = k-j;
+                        if(i+diff > square.length - 1 || maxEdge >= diff) continue;
+                        if(square[i][j] == square[i+diff][j] && square[i][j] == square[i+diff][k]) {
+                            maxEdge = diff+1;
+                        }
+                    }
+                }
+            }
+        }
+        if(maxEdge == 0) return 1;
+        else return (int) Math.pow(maxEdge, 2);
+    }
+}
