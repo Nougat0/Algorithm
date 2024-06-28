12ed3419323e8e999542c0c887c0796b5e4fb123
hmkim <elppapie@naver.com>
2024-06-28 15:29:21 +0900

    :+1: 1076번 우당탕탕 5번만에 해결
    틀린 이유들
    - 21억을 넘어가지만 Integer.valueOf 사용 --> Long.valueOf 로 교체함
    - String.format("%0숫자d", 0) 에서 숫자에 0이 들어가는 경우(black) NumberFormat 오류 발생 -- black이 곱으로 들어가는 경우
    - 곱으로 들어가는 경우는 10의 n승이라서 10의 0승인 black은 1로 계산했어야 함

---
+++ questions/src/main/java/baekjoon/processing/Main1076.java
@@ -0,0 +1,33 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+
+public class Main1076 {
+    public enum ResistorColor { //값이 정해져 있으므로 enum 으로 정의, 순서 가져오기
+        black, brown, red, orange, yellow, green, blue, violet, grey, white;
+        private static int getOrdinal(String color) {
+            try{
+                return ResistorColor.valueOf(color).ordinal();
+            } catch(Exception e) {
+                return -1;
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String value = String.format("%d%d",
+                ResistorColor.getOrdinal(br.readLine()),
+                ResistorColor.getOrdinal(br.readLine()));
+        //black이 곱으로 올 경우 대비 (*1 이므로 동작 없음)
+        int multiply = ResistorColor.getOrdinal(br.readLine());
+        if(multiply != 0) value += String.format("%0"+multiply+"d", 0);
+
+        //black이 값으로 올 경우 대비
+        bw.write(Long.valueOf(value) + "\n"); //21억을 가뿐히 넘기기 때문에 Long
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
