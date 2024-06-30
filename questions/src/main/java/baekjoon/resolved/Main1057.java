d5450b2d818cbf169383e4891659420daa2a0548
hmkim <elppapie@naver.com>
2024-06-30 15:28:02 +0900

    :+1: 1057번 해결
    처음에는 다음판 만나는지... 다음판에서 홀/짝인지 이런걸 체크하면서 복잡하게 생각하다가
    
    조금 더 생각해보니 그냥 이번판 만나는지 확인하고, 안 만난다면 다음판 번호 구하기 -> 반복하면 해결됨
    
    그 과정에서 부전승도 자연스럽게 해결됨
    
    다만 궁금한 건, 만나지 않는 경우가 있을 수 있나? 문제 조건에 만나지 않을 경우 -1 출력하라고 하던데 이건 어떤 경우에 가능한지 궁금했음 (나는 구현 안하고 정답 맞음)

---
+++ questions/src/main/java/baekjoon/resolved/Main1057.java
@@ -0,0 +1,45 @@
+package baekjoon.resolved;
+
+import java.io.*;
+import java.util.*;
+
+public class Main1057 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int kim = Integer.parseInt(st.nextToken());
+        int lim = Integer.parseInt(st.nextToken());
+        /*
+        그냥 kim과 lim의 바뀌는 번호를 추적하면 될 듯
+        이번판 안 만나면 다음판 번호 구하기
+        테스트해보니 부전승의 경우도 해결됨
+         */
+        int round = 1;
+        while(!meetsThisRound(n, kim, lim)) {
+            kim = getNextNumber(n, kim);
+            lim = getNextNumber(n, lim);
+            n = n/2;
+            round++;
+        }
+        bw.write(round + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static int getNextNumber(int n, int number) {
+        if(number % 4 == 0) return number/4 * 2;
+        else if(number % 4 == 1 || number % 4 == 2) return number / 4 * 2 + 1;
+        else return number / 4 * 2 + 2;
+    }
+
+    public static boolean meetsThisRound(int n, int number1, int number2) {
+        int min = Math.min(number1, number2);
+        int max = Math.max(number1, number2);
+
+        if(min/2 + 1 == max/2 && min%2 == 1 && max%2 == 0) return true;
+        else return false;
+    }
+}
