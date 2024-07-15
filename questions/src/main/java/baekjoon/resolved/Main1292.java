c7d329403e29b129de28af3c8502949995dba938
hmkim <elppapie@naver.com>
2024-07-16 00:20:54 +0900

    :+1: 1292번 해결
    기존 loop 의 if문 1개를 바깥으로 빼는 작업 진행
    - 배열 미리 만들어놓기
    - 기존 if문 바깥으로 빼서 별도의 loop문으로 만들기 (loop 1개 -> loop 2개)
    
    이렇게 했더니 첫 시도보다 시간복잡도, 메모리 모두 감소함
    
    제출 번호	아이디	문제	결과			메모리	시간	언어		코드 길이
    81099889	bcdlife	 1292	맞았습니다!!	15792	112	Java 11	991
    81099563	bcdlife	 1292	맞았습니다!!	15860	120	Java 11 	859

--- questions/src/main/java/baekjoon/resolved/Main1292.java
+++ questions/src/main/java/baekjoon/resolved/Main1292.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
@@ -11,6 +11,7 @@ public class Main1292 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
+        int[] arr = new int[end+1]; arr[0] = 0;
         //계산
         int sum = 0, count = 0, number = 1;
         for(int i=1; i<=end; i++) {
@@ -18,9 +19,11 @@ public class Main1292 {
                 number++;
                 count = 0;
             }
-            if(i>=start) sum += number;
+            arr[i] = number;
             count++;
         }
+        for(int i=start; i<=end; i++)
+            sum += arr[i];
         //출력
         bw.write(sum +"\n");
         bw.flush();
