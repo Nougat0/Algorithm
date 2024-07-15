902654a0640bcdda7f4795402f27aeb4f4ecbaf7
hmkim <elppapie@naver.com>
2024-07-15 09:39:07 +0900

    :arrow_right: 1212번 해결중
    BigInteger로 변환 완료
    
    다만... 중간에 int 보다 큰 값을 BigInteger에 넣어야 하는 경우가 있어서 해당 경우 때문에 틀리는 중
    숫자 자릿수 제한으로 생각해보면 ... 자릿수 34를 지정했는데도 벌써 첫 번째 값이 int 의 범위를 넘어선다 (틀린 이유는 자료형 크기 제한 때문에 int 범위보다 큰 값은 모두 int 최대값으로 줄여서 사용되고 있었음)
    문제에 지정된 최대 자릿수 333,334 를 보면 int 는 물론이고 long 도 빈번하게 넘어갈 듯... 따라서 질문게시판에서 본 힌트(8진수 2진수로 직접 변환)를 따라야 할 것 같다
    
    [ 10^333333 * 맨앞 숫자 ] 의 경우까지 생각해야 하는 문제임

--- questions/src/main/java/baekjoon/processing/Main1212.java
+++ questions/src/main/java/baekjoon/processing/Main1212.java
@@ -1,27 +1,34 @@
 package baekjoon.processing;
 
 import java.io.*;
+import java.math.BigInteger;
 
 public class Main1212 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        char[] number = br.readLine().toCharArray(); //8진수
+        //8진수
+        char[] number = br.readLine().toCharArray();
         //10진수 변환
-        int decimal = 0;
-        for(int i=0; i<number.length; i++) {
-            decimal += (int) (Character.getNumericValue(number[i]) * Math.pow(8, number.length-1-i));
-        }
+        BigInteger zero = new BigInteger("0");
+        BigInteger one = new BigInteger("1");
+        BigInteger two = new BigInteger("2");
+        BigInteger decimal = zero;
+        for(int i=0; i<number.length; i++)
+            decimal = decimal.add(new BigInteger(
+                Character.getNumericValue(number[i]) *
+                (int) Math.pow(8, number.length-1-i) + ""
+            ));
         //2진수 변환
         String binary = "";
-        if(decimal == 0) {
+        if(decimal.compareTo(zero) == 0) {
             binary = "0";
         } else {
-            while(decimal/2 >= 1) {
-                if(decimal/2 == 1) binary = String.format("%d%d", decimal/2, decimal%2) + binary;
-                else binary = decimal%2 + binary;
-                decimal /= 2;
+            while(decimal.divide(two).compareTo(one) == 0 || decimal.divide(two).compareTo(one) == 1) {
+                if(decimal.divide(two).compareTo(one) == 0)
+                    binary = String.format("%d%d", decimal.divide(two), decimal.remainder(two)) + binary;
+                else binary = decimal.remainder(two) + binary;
+                decimal = new BigInteger(decimal.divide(two) + "");
             }
         }
         bw.write(binary + "\n");
