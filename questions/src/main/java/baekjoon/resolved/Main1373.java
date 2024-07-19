e5169dea03c439d91e531074ad6f9eab38d294fa
hmkim <elppapie@naver.com>
2024-07-19 20:25:57 +0900

    :zap: 1373번 성능개선 ?
    최대한 줄여봤는데 (아는 선에서) 여기서 더 줄어들지를 않는다...
    StringBuffer를 써서 그런 것 같긴 한데
    
    for문은 하나 줄였더니 소요시간은 줄어들었다 (조금)
    
    제출 번호	아이디	문제	메모리	시간	언어		코드 길이
    81303070	bcdlife	 1373	70316	536	Java 11	2133
    81302572	bcdlife	 1373	69536	556	Java 11	2185
    81302225	bcdlife	 1373	68616	556	Java 11	2328

--- questions/src/main/java/baekjoon/resolved/Main1373.java
+++ questions/src/main/java/baekjoon/resolved/Main1373.java
@@ -15,21 +15,16 @@ public class Main1373 {
             length += 3 - length%3;
         }
 
-        //8진수로 변환
-        int[] octalNumber = new int[length/3];
-        BinaryFromOctal changer = new BinaryFromOctal();
-        for(int i=length-1; i-2>=0; i-=3){ //3자리씩 8진수로 변환할 2진수 묶음 만들기
+        //8진수로 변환하고 바로 출력
+        for(int i=0; i<length; i+=3){ //3자리씩 8진수로 변환할 2진수 묶음 만들기
             String binary = new StringBuffer()
-                    .append(binaryNumber.charAt(i-2))
-                    .append(binaryNumber.charAt(i-1))
-                    .append(binaryNumber.charAt(i)).toString();
+                    .append(binaryNumber.charAt(i))
+                    .append(binaryNumber.charAt(i+1))
+                    .append(binaryNumber.charAt(i+2)).toString();
             //BinaryFromOctal 클래스 활용하여 8진수 값 가져오기
-            octalNumber[i/3] = changer.getOctal(binary);
+            bw.write(String.valueOf(BinaryFromOctal.getOctal(binary)));
         }
 
-        //출력
-        for(int i=0; i<octalNumber.length; i++)
-            bw.write(String.valueOf(octalNumber[i]));
         bw.newLine();
         bw.flush();
         bw.close();
@@ -40,26 +35,25 @@ public class Main1373 {
      * 8진수의 각 자릿수를 2진수로 변환하는 용도의 클래스
      */
     public static class BinaryFromOctal {
-        private Map<String, Integer> octal = new HashMap<>(); //Memoization용 배열 선언
+        private static final Map<String, Integer> octal = new HashMap<>(); //Memoization용 배열 선언
         /*
         binary 를 8진수로 반환
         계산된 값이 octal에 없을 경우, 직접 계산
         */
-        public int getOctal(String binary) {
-            if(octal.get(binary) == null)
+        public static int getOctal(String binary) {
+            if(!octal.containsKey(binary))
                 octal.put(binary, calculateOctal(binary));
             return octal.get(binary);
         }
         /*
         8진수 변환 값 계산용 메소드
         */
-        public int calculateOctal(String binary) {
+        public static int calculateOctal(String binary) {
             int octal = 0;
             //8진수 구하기
-            for(int i=2; i>=0; i--) {
-                octal += Math.pow(2, binary.length()-1-i) *
+            for(int i=2; i>=0; i--)
+                octal += Math.pow(2, 2-i) *
                         Character.getNumericValue(binary.charAt(i));
-            }
             return octal;
         }
     }
