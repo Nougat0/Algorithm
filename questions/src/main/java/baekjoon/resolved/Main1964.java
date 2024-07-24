a5468daabec2fb9815d8186488f0ab2bd06bdd31
hmkim <elppapie@naver.com>
2024-07-24 22:35:52 +0900

    :+1: 1964번 해결
    재귀로 풀었어도 재밌었을 것 같다

---
+++ questions/src/main/java/baekjoon/resolved/Main1964.java
@@ -0,0 +1,23 @@
+package baekjoon.resolved;
+
+import java.io.*;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.StringTokenizer;
+
+public class Main1964 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        long sum = 5; //초기값 1도형 지정
+        for(int i=2; i<=n; i++) { //단계별 i도형 덧셈
+            sum += i * 5 - (2 * i - 1);
+        }
+        bw.write(sum % 45678 + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
