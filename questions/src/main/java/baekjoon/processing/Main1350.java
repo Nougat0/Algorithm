7b2c9598085d4faf02e89664790a528f99ef0be4
hmkim <elppapie@naver.com>
2024-07-25 00:20:41 +0900

    :arrow_right: 1350번 풀어놓음 (미제출)

---
+++ questions/src/main/java/baekjoon/processing/Main1350.java
@@ -0,0 +1,23 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+
+public class Main1350 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int fileCount = Integer.parseInt(br.readLine());
+        StringTokenizer fileSizes = new StringTokenizer(br.readLine());
+        int clusterSize = Integer.parseInt(br.readLine());
+        long usedDiskSize = 0L;
+        while(fileSizes.hasMoreTokens()) {
+            double fileSize = Double.parseDouble(fileSizes.nextToken());
+            usedDiskSize += (long) Math.ceil(fileSize/clusterSize) * clusterSize;
+        }
+        bw.write(usedDiskSize + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
