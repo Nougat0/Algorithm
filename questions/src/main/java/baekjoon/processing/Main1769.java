fb0479a13712a8630e6a3bc55c6cd60151caa7e3
hmkim <elppapie@naver.com>
2024-07-26 09:03:42 +0900

    :arrow_right: 1769번 해결중..
    그렇다고 count를 맨 마지막에 두자니 큰 숫자들이 1의 자리로 변환된 과정은 count가 되지 않는 문제 발생...

--- questions/src/main/java/baekjoon/processing/Main1769.java
+++ questions/src/main/java/baekjoon/processing/Main1769.java
@@ -10,7 +10,6 @@ public class Main1769 {
         int count = 0; //2번 변경 횟수
         long sum; //2번 변경 결과
         while(true) {
-            count++;
             sum = 0;
             for (char number : numbers) //각 자릿수 더하기
                 sum += Character.getNumericValue(number);
@@ -18,6 +17,7 @@ public class Main1769 {
                 numbers = String.valueOf(sum).toCharArray();
             else //1의 자리 시 종료
                 break;
+            count++;
         }
         bw.write(String.format("%d%n%s%n", count, sum%3 ==0 ? "YES" : "NO"));
         bw.flush();
