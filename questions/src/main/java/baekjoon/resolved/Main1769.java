6052a79d1fe110a280da50606e8f197b6bb237e5
hmkim <elppapie@naver.com>
2024-07-26 09:16:24 +0900

    :zap: 1769번 코드정리...
    while문 안에 있던 if문을 while문 조건으로 빼면서 코드는 간결해짐
    다만 시간이 많이 늘어남 (+24ms)
    
    제출 번호	아이디	문제	메모리	시간	언어		코드 길이
    81610878	bcdlife	 1769	22304	292	Java 11	937
    81610650	bcdlife	 1769	22308	268	Java 11	1151

--- questions/src/main/java/baekjoon/resolved/Main1769.java
+++ questions/src/main/java/baekjoon/resolved/Main1769.java
@@ -10,21 +10,15 @@ public class Main1769 {
         int count = 0; //문제2 변경 횟수
         long sum; //문제2 변경 결과
 
-        //1의 자릿수 입력 시 바로 반환
-        if(numbers.length == 1) {
-            sum = Character.getNumericValue(numbers[0]);
-        } else {
-            while(true) {
-                count++;
-                sum = 0;
-                for (char number : numbers) //각 자릿수 더하기
-                    sum += Character.getNumericValue(number);
-                if (sum / 10 > 0) //10의 자리 이상
-                    numbers = String.valueOf(sum).toCharArray();
-                else //1의 자리 시 종료
-                    break;
-            }
+        while(numbers.length > 1) { //문제2 변경
+            count++;
+            sum = 0;
+            for (char number : numbers) //각 자릿수 더하기
+                sum += Character.getNumericValue(number);
+            numbers = String.valueOf(sum).toCharArray();
         }
+        //1의 자릿수 값 구하기
+        sum = Character.getNumericValue(numbers[0]);
 
         bw.write(String.format("%d%n%s%n", count, sum%3 ==0 ? "YES" : "NO"));
         bw.flush();
