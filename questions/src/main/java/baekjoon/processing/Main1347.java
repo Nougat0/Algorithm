9b3d8bc94709e62b9d3114c3736f502edd206daf
hmkim <90332022+Nougat0@users.noreply.github.com>
2024-07-27 17:35:39 +0900

    :+1: 1347번 해결
    1. 뭔가 더 간단한 방법이 있을 것 같은데... 일단은 List<String> 으로 구현했고 정답을 맞추는 대신 메모리와 시간복잡도를 잃었다... 초반에 정의해놓은 direction 배열을 활용하지 못해서 유감...
    - 제출 번호	아이디	문제	메모리	시간	언어		코드 길이
    - 81670102	bcdlife	 1347	16456	164	Java 11	4533
    
    2. String.substring(i, j) 사용법에 대해 새롭게 알고 감
    - i: 시작 인덱스
    - j: 끝 인덱스+1 :new:
    - String.substring(0,0) -> 빈문자열
    - String.substring(0,1) -> 한자릿수 문자열 (0번 문자)
    - String.substring(0,2) -> 두자릿수 문자열 (0,1번 문자)

--- questions/src/main/java/baekjoon/processing/Main1347.java
+++ questions/src/main/java/baekjoon/processing/Main1347.java
@@ -4,49 +4,96 @@ import java.io.*;
 import java.util.*;
 
 public class Main1347 {
-    static int[][] direction = new int[][] {{1,0}, {0,-1}, {-1,0}, {0,1}};
+    static int totalDirection = 4;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int totalMove = Integer.parseInt(br.readLine());
         String moves = br.readLine();
-        List<List<Boolean>> maze = new ArrayList<>();
-        int facing = 0;
-        int totalDirection = direction.length;
-        int x = 0;
-        int y = 0;
+        List<String> maze = new ArrayList<>(){{add(".");}};
+        int facing = 0; //방향
+        int xLength = 1, yLength = 1; //미로의 크기
+        int x = 0, y = 0; //현재 홍준의 위치
 
+        //입력받은 움직임 그대로 따라가기
         for(int i=0; i<totalMove; i++) {
-            char move = moves.charAt(i);
+            char move = moves.charAt(i); //각 움직임 실행
             switch(move) {
-                case 'F':
+                case 'F': //전진
+                    //시계방향으로 방향 가정 (0:아래, 1:오른쪽, 2:위, 3:왼쪽)
                     switch(facing) {
-                        case 0:
-                            maze.add(new ArrayList<>());
+                        case 0: //아래
+                            y++;
+                            if(y > maze.size()-1) { //새 줄 추가일 경우
+                                //현재 x 자리만 . 으로 표시하고 나머지는 #으로 채워야 함
+                                String line = "#".repeat(x) + "." + "#".repeat(xLength-x-1);
+                                maze.add(y, line);
+                                yLength++;
+                            } else { //이미 줄이 있을 경우
+                                String line = maze.get(y);
+                                line = line.substring(0, x) + "." + line.substring(x + 1, xLength);
+                                maze.set(y, line);
+                            }
                             break;
-                        case 1:
+                        case 1: //왼쪽
+                            x--;
+                            if(x < 0) { //새 칸 추가일 경우
+                                for(int j=0; j<yLength; j++)
+                                    if(j == y) maze.set(j, "." + maze.get(j));
+                                    else maze.set(j, "#" + maze.get(j));
+                                xLength++;
+                                x = 0;
+                            } else {
+                                String line = maze.get(y);
+                                line = line.substring(0, x) + "." + line.substring(x+1, xLength);
+                                maze.set(y, line);
+                            }
                             break;
-                        case 2:
-                            maze.add(new ArrayList<>());
+                        case 2: //위
+                            y--;
+                            if(y < 0) { //새 줄 추가일 경우
+                                y = 0;
+                                String line = "#".repeat(x) + "." + "#".repeat(xLength-x-1);
+                                maze.add(y, line);
+                                yLength++;
+                            } else {
+                                String line = maze.get(y);
+                                line = line.substring(0, x) + "." + line.substring(x+1, xLength);
+                                maze.set(y, line);
+                            }
                             break;
-                        case 3:
+                        case 3: //오른쪽
+                            x++;
+                            if(x > xLength-1) { //새 칸 추가일 경우
+                                for(int j=0; j<yLength; j++)
+                                    if(j == y) maze.set(y, maze.get(y) + ".");
+                                    else maze.set(j, maze.get(j) + "#");
+                                xLength++;
+                            } else {
+                                String line = maze.get(y);
+                                line = line.substring(0, x) + "." + line.substring(x+1, xLength);
+                                maze.set(y, line);
+                            }
                             break;
                     }
                     break;
-                case 'L':
+                case 'L': //좌회전
                     facing--;
                     if(facing < 0)
                         facing += totalDirection;
                     break;
-                case 'R':
+                case 'R': //우회전
                     facing++;
-                    if(facing > totalDirection)
+                    if(facing >= totalDirection)
                         facing -= totalDirection;
                     break;
-
             }
         }
-
+        //출력
+        for(String line: maze) {
+            bw.write(line + "\n");
+        }
         bw.flush();
         bw.close();
         br.close();
