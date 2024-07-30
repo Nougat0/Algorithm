a356ad26b522ffe207b6756588442bd4a4ba94ee
hmkim <elppapie@naver.com>
2024-07-30 22:52:22 +0900

    :zap: 1268번 성능개선 ?
    for문 순서만 바꿨을 뿐인데 훨씬 단순하게 생각 가능
    [ 기존 ]
    	i 학생의
    		j 학년 때
    			k 학생과 같은 반이었는지 확인
    [ 현재 ]
    	i 학생과
    		k 학생이
    			j 학년 때 같은 반이었는지
    (헷갈릴까봐 index 용 변수명 i j k 는 순서를 유지함)
    
    두 학생이 여러 번 같은 반이면 중복 체크되는 걸 방지하기 위해서 boolean[][] friendList 를 사용했었는데,
    for 문 순서를 바꿔서 학생들 둘을 대상으로 만났는지만 체크하면 중복체크를 면할 수 있게 된다...!
    단순히 for문 순서만 바꿨는데도 훨씬 간편해진 로직... 다음에 중첩 for문 사용하게 되면 참고해봐야겠다
    슬프게도 스탯?은 크게 변하지 않았다
    
    제출 번호	아이디	문제	메모리	시간	언어		코드 길이
    81817141	bcdlife	 1268	19704	224	Java 11	1642
    81803033	bcdlife	 1268	19736	236	Java 11	1769

--- questions/src/main/java/baekjoon/resolved/Main1268.java
+++ questions/src/main/java/baekjoon/resolved/Main1268.java
@@ -12,7 +12,7 @@ public class Main1268 {
         StringTokenizer st;
         int studentCount = Integer.parseInt(br.readLine());
         int[][] classNo = new int[studentCount][5]; //학생수*5개학년
-        boolean[][] friendList = new boolean[studentCount][studentCount]; //학생수*학생수
+        int[] friendCount = new int[studentCount];
         for(int i=0; i<studentCount; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<5; j++)
@@ -20,28 +20,27 @@ public class Main1268 {
         }
 
         //연산
-        for(int i=0; i<studentCount; i++) {//i 학생의
-            for (int j = 0; j < 5; j++)  //j+1 학년 때
-                //같은 반이었던 사람 있는지 확인
-                for (int k = 0; k < studentCount; k++)  //k 학생과 같은 반이었는가?
-                    if (i != k && classNo[i][j] == classNo[k][j]) //본인 제외
-                        friendList[i][k] = true;
-        }
-        //친구수 세기
         int studentNo = 0;
-        int friendCount = 0;
-        for(int i=0; i<studentCount; i++) { //각 학생의
-            int count = 0;
-            for(int j=0; j<studentCount; j++) //친구 수 세기
-                if(friendList[i][j]) count++;
-            if(friendCount < count) {//최고 인기쟁이 갱신조건
-                //번호는 오름차순이므로 제어완료, 친구수로만 확인
+        int maxFriends = 0;
+        for(int i=0; i<studentCount; i++) {//i 학생과
+            for(int k=0; k<studentCount; k++) {//k 학생과
+                if(i==k) continue;
+                boolean haveMet = false;
+                for(int j=0; j<5; j++) //학년 통틀어 만났는지
+                    if(haveMet) break;
+                    else if(classNo[i][j] == classNo[k][j]) {
+                        haveMet = true;
+                        friendCount[i]++; //i 학생의 친구수++
+                    }
+            }
+            if(friendCount[i] > maxFriends) { //최고 인기쟁이 선발
                 studentNo = i;
-                friendCount = count;
+                maxFriends = friendCount[i];
             }
         }
-        //1번부터 시작이므로 +1
-        bw.write((studentNo+1)+"\n");
+
+        //출력
+        bw.write((studentNo+1)+"\n"); //1번부터 시작이므로 +1
         bw.flush();
         bw.close();
         br.close();
