4aa98b931e08b1c71f6134332592fae3a9c5f044
hmkim <elppapie@naver.com>
2024-08-01 08:58:28 +0900

    :+1: 2804번 해결
    이런 문제 재밌는데 별로 없어서 아쉽
    
    제출 번호 아이디 문제 메모리 시간 언어 코드 길이
    81878542 bcdlife 2804 14232 120 Java 11 1457

--- questions/src/main/java/baekjoon/resolved/Main2804.java
+++ questions/src/main/java/baekjoon/resolved/Main2804.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
@@ -9,25 +9,25 @@ public class Main2804 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         //공통 글자 확인, horizontal 기준으로 가장 첫 번째로 오는 중복 위치 가져오기
         StringTokenizer st = new StringTokenizer(br.readLine());
-        char[] horizontal = st.nextToken().toCharArray();
-        char[] vertical = st.nextToken().toCharArray();
+        char[] horizontal = st.nextToken().toCharArray(); // 단어 A
+        char[] vertical = st.nextToken().toCharArray(); // 단어 B
         int horizonCross = -1, verticalCross = -1;
 
-        gotCrossPosition:
+        gotCrossPosition: //공통 글자 찾으면 loop 중단
         for(int i=0; i<horizontal.length; i++)
             for(int j=0; j<vertical.length; j++)
                 if(horizontal[i] == vertical[j]) {
+                    //공통글자 위치 가져오기
                     horizonCross = i;
                     verticalCross = j;
                     break gotCrossPosition;
                 }
-        //출력
+        //공통 글자 기준으로 출력
         for(int i=0; i<vertical.length; i++) {
-            for(int j=0; j<horizontal.length; j++) {
+            for(int j=0; j<horizontal.length; j++)
                 if(j==horizonCross)  bw.write(vertical[i]);
                 else if(i==verticalCross)  bw.write(horizontal[j]);
                 else  bw.write(".");
-            }
             bw.newLine();
         }
         bw.flush();
