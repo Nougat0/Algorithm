0c263ba052f2e50586eeff8981a40fda956dcaf8
hmkim <elppapie@naver.com>
2024-08-04 22:31:22 +0900

    :zap: 3181번 성능개선 ?
    메모리는 줄었는데 ... if문이 빠졌으니 시간이 줄어들 거라 생각한 게 빗나감
    
    출 번호	아이디	문제	메모리	시간	언어		코드 길이
    82035134	bcdlife	 3181	14268	116	Java 11	1230
    82034942	bcdlife	 3181	14304	108	Java 11	1312

--- questions/src/main/java/baekjoon/resolved/Main3181.java
+++ questions/src/main/java/baekjoon/resolved/Main3181.java
@@ -13,19 +13,18 @@ public class Main3181 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        while(st.hasMoreTokens()) { //문장의 모든 단어에 대해
+        sb.append(st.nextToken().charAt(0)); //첫 단어는 무사통과
+
+        while(st.hasMoreTokens()) { //문장의 2번째 단어부터
             String word = st.nextToken();
-            if(sb.length() == 0) //첫 단어는 무사통과
-                sb.append(word.charAt(0));
-            else { //무시할 단어에 해당되는지 확인 및 출력
-                boolean freeToGo = true;
-                for(int i=0; i<blockList.length; i++)
-                    if(blockList[i].equals(word)) {
-                        freeToGo = false;
-                        break;
-                    }
-                if(freeToGo) sb.append(word.charAt(0));
-            }
+            //무시할 단어에 해당되는지 확인 및 출력
+            boolean freeToGo = true;
+            for(int i=0; i<blockList.length; i++)
+                if(blockList[i].equals(word)) {
+                    freeToGo = false;
+                    break;
+                }
+            if(freeToGo) sb.append(word.charAt(0));
         }
         //대문자로 출력
         bw.write(sb.toString().toUpperCase() + "\n");
