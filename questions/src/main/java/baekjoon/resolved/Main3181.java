159eefad4b08f99fa339539c1b1860f403e51f9d
hmkim <elppapie@naver.com>
2024-08-04 22:27:05 +0900

    :+1: 3181번 해결
    제출 번호	아이디	문제	문제 제목		메모리	시간	언어		코드 길이
    82034942	bcdlife	3181	줄임말 만들기	14304	108	Java 11	1312

---
+++ questions/src/main/java/baekjoon/resolved/Main3181.java
@@ -0,0 +1,37 @@
+package baekjoon.resolved;
+
+import java.io.*;
+import java.util.*;
+
+public class Main3181 {
+    //무시할 단어 목록
+    static String[] blockList = new String[] {"i", "pa", "te","ni","niti","a","ali","nego", "no", "ili"};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        while(st.hasMoreTokens()) { //문장의 모든 단어에 대해
+            String word = st.nextToken();
+            if(sb.length() == 0) //첫 단어는 무사통과
+                sb.append(word.charAt(0));
+            else { //무시할 단어에 해당되는지 확인 및 출력
+                boolean freeToGo = true;
+                for(int i=0; i<blockList.length; i++)
+                    if(blockList[i].equals(word)) {
+                        freeToGo = false;
+                        break;
+                    }
+                if(freeToGo) sb.append(word.charAt(0));
+            }
+        }
+        //대문자로 출력
+        bw.write(sb.toString().toUpperCase() + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
+
