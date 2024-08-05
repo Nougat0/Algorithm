8cba6a79637337ed921113225bd93ce258405f7c
hmkim <elppapie@naver.com>
2024-08-05 16:42:03 +0900

    :arrow_right: 1598번 풀어놓음 (미해결)

---
+++ questions/src/main/java/baekjoon/processing/Main1598.java
@@ -0,0 +1,36 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main1598 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // 11/4 = 2, 11%4 = 3
+
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+
+//        int num1x = num1 % 4 == 0 ? 3 : num1 % 4 - 1;
+//        int num1y = num1 % 4 == 0 ? num1 / 4 - 1 : num1 / 4;
+//        int num2x = num2 % 4 == 0 ? 3 : num2 % 4 - 1;
+//        int num2y = num2 % 4 == 0 ? num1 / 4 - 1 : num2 / 4;
+
+        bw.write(Math.abs(getX(num1) - getX(num2) + 1) + Math.abs(getY(num1) - getY(num2) + 1) + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static int getX(int number) {
+        if(number % 4 == 0) return 3;
+        else return number % 4 - 1;
+    }
+
+    public static int getY(int number) {
+        if(number % 4 == 0) return number / 4 - 1;
+        else return number / 4;
+    }
+}
