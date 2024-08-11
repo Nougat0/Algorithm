a57bd1ca241668c9dab0bd701f901a61a33b8dac
hmkim <elppapie@naver.com>
2024-08-11 21:53:48 +0900

    :arrow_right: 2033번 풀어놓음 (미제출)

---
+++ questions/src/main/java/baekjoon/processing/Main2033.java
@@ -0,0 +1,33 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main2033 {
+    /*
+    if(n > 10) //반올림
+        n = (n + 5)/10 * 10;
+    if(n > 100) //반올림
+        n = (n + 50)/100 * 100;
+    */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        int num = 1;
+        while(true) {
+            int tenth = (int) Math.pow(10, num);
+            if(n > tenth) {
+                n = (n + 5*tenth/10)/tenth * tenth;
+            } else {
+                break;
+            }
+            num++;
+        }
+        bw.write(n + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
