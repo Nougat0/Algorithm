69e0d8aab0d2a2da3d2ca7ae4f14d3f3e2d0d160
hmkim <elppapie@naver.com>
2024-08-20 17:55:15 +0900

    :arrow_right: 2441번 풀어놓음 (미제출)

---
+++ questions/src/main/java/baekjoon/processing/Main2441.java
@@ -0,0 +1,26 @@
+package baekjoon.processing;
+
+import java.io.*;
+
+public class Main2441 {
+    public static final String star = "*";
+    public static final String blank = " ";
+    public static final String newLine = "\n";
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=n; i>0; i--) {
+            for(int j=0; j<n-i; j++)
+                sb.append(blank);
+            for(int j=n-i; j<n; j++)
+                sb.append(star);
+            sb.append(newLine);
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
