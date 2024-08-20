403e39380ee177c4a0fd2cbbd0310b23553caca9
hmkim <elppapie@naver.com>
2024-08-20 18:26:46 +0900

    :arrow_right: 2443번 풀어놓음 (미제출)

---
+++ questions/src/main/java/baekjoon/processing/Main2443.java
@@ -0,0 +1,26 @@
+package baekjoon.processing;
+
+import java.io.*;
+
+public class Main2443 {
+    public static final String star = "*";
+    public static final String blank = " ";
+    public static final String newLine = "\n";
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=n; i>0; i--) {
+            for(int j=0; j<n-i; j++) //빈칸 찍기
+                sb.append(blank);
+            for(int j=0; j<2*i-1; j++) //별 찍기
+                sb.append(star);
+            sb.append(newLine);
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
