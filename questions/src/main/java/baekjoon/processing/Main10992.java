aeaaae62c57b55b6a475b3fac137572c833d2e57
hmkim <elppapie@naver.com>
2024-08-26 12:54:07 +0900

    :arrow_right: 10992번 풀어놓음 (미제출)
    10990번과 유사함

---
+++ questions/src/main/java/baekjoon/processing/Main10992.java
@@ -0,0 +1,34 @@
+package baekjoon.processing;
+
+import java.io.*;
+
+public class Main10992 {
+    public static final String star = "*";
+    public static final String blank = " ";
+    public static final String newLine = "\n";
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n-1; i++) {
+            //첫번째 별 찍기
+            for(int j=0; j<n-i-1; j++) //앞의 빈칸
+                sb.append(blank);
+            sb.append(star);
+            //두번째 별 찍기
+            for(int j=0; j<2*i-1; j++) //가운데 빈칸
+                sb.append(blank);
+            if(i!=0)
+                sb.append(star);
+            sb.append(newLine);
+        }
+        for(int i=0; i<2*n-1; i++) //마지막 줄은 한줄 찍기
+            sb.append(star);
+        sb.append(newLine);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
