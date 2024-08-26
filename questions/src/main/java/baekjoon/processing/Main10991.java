7a89d517f83d4f342165b1ab2578da25117ff26c
hmkim <elppapie@naver.com>
2024-08-26 12:57:51 +0900

    :arrow_right: 10991번 풀어놓음 (미제출)
    10990번 응용버전 ... 가운데 빈칸에서 홀수일 경우에만 별 찍기로 변경함.

---
+++ questions/src/main/java/baekjoon/processing/Main10991.java
@@ -0,0 +1,32 @@
+package baekjoon.processing;
+
+import java.io.*;
+
+public class Main10991 {
+    public static final String star = "*";
+    public static final String blank = " ";
+    public static final String newLine = "\n";
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++) {
+            //첫번째 별 찍기
+            for(int j=0; j<n-i-1; j++) //앞의 빈칸
+                sb.append(blank);
+            sb.append(star);
+            //두번째 ~ 별 찍기
+            for(int j=0; j<2*i-1; j++) //가운데 빈칸 + 별 패턴
+                if(j%2==0) sb.append(blank);
+                else sb.append(star);
+            if(i!=0)
+                sb.append(star);
+            sb.append(newLine);
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
