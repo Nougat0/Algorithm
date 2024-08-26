341a84ac45458a3892abd17cecee44c6ba81d25c
hmkim <elppapie@naver.com>
2024-08-26 12:37:38 +0900

    :arrow_right: 2522번 풀어놓음 (미제출)
    3
      *
     **
    ***
     **
      *

---
+++ questions/src/main/java/baekjoon/processing/Main2522.java
@@ -0,0 +1,37 @@
+package baekjoon.processing;
+
+import java.io.*;
+
+public class Main2522 {
+    public static final String star = "*";
+    public static final String blank = " ";
+    public static final String newLine = "\n";
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<n; i++){
+            for(int j=0; j<n-i; j++)
+                sb.append(blank);
+            for(int j=n-i; j<n; j++)
+                sb.append(star);
+            sb.append(newLine);
+        }
+        for(int i=1; i<=n; i++)
+            sb.append(star);
+        sb.append(newLine);
+        for(int i=n-1; i>0; i--){
+            for(int j=n-i; j>0; j--)
+                sb.append(blank);
+            for(int j=i; j>0; j--)
+                sb.append(star);
+            sb.append(newLine);
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
