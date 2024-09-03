c4f4beb8acbb77ab0f249c9764b208199a33b0e1
hmkim <elppapie@naver.com>
2024-09-04 08:03:19 +0900

    :arrow_right: 2475번 풀어놓음 (미제출)

---
+++ questions/src/main/java/baekjoon/processing/Main2475.java
@@ -0,0 +1,20 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main2475 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sum = 0;
+        while(st.hasMoreTokens())
+            sum += Math.pow(Integer.parseInt(st.nextToken()), 2);
+
+        bw.write(sum%10 + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
