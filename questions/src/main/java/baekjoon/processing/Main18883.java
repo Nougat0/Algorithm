46027ce68ea0dd81d51ccb5ce178595fe86ed165
hmkim <elppapie@naver.com>
2024-09-04 08:56:59 +0900

    :arrow_right: 18883번 풀어놓음 (미제출)
    시간제한에 맞출 수 있는 방법인지는 직접 제출해봐야 알 것...
    시간제한: 1초(1000ms) (평상시 1/2배), 메모리 제한: 512KB (평상시 2배)

---
+++ questions/src/main/java/baekjoon/processing/Main18883.java
@@ -0,0 +1,33 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main18883 {
+
+    public static final String star = "*";
+    public static final String blank = " ";
+    public static final String newLine = "\n";
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        //시간 제한이 타이트해지고, 메모리 제한이 넉넉해짐
+        int num = 1;
+
+        //출력
+        for(int i=0; i<n; i++)
+            for(int j=0; j<m; j++) {
+                sb.append(num++);
+                if(j == m-1) sb.append(newLine);
+                else sb.append(blank);
+            }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
