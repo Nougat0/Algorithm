aec682c4abb4dbf2243e3f58364c8e4752bb7f15
hmkim <elppapie@naver.com>
2024-09-10 09:15:07 +0900

    :arrow_right: 23802번 풀어놓음 (미제출)

---
+++ questions/src/main/java/baekjoon/processing/Main23802.java
@@ -0,0 +1,25 @@
+package baekjoon.processing;
+
+import java.io.*;
+
+public class Main23802 {
+    public static final String at = "@";
+    public static final String blank = " ";
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int cellSize = Integer.parseInt(br.readLine());
+        int cellCount = 5; //셀의 개수
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<cellSize; i++) //셀 맨 윗줄
+            sb.append(at.repeat(cellSize*cellCount) + "\n");
+        for(int i=cellSize; i<cellSize*cellCount; i++) //셀 세로줄 
+            sb.append(at.repeat(cellSize) + "\n");
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
