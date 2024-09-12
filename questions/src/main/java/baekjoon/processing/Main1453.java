87d2d12dc0451e1e279ceaf361efda49df679f55
hmkim <elppapie@naver.com>
2024-09-12 09:13:02 +0900

    :arrow_right: 1453번 풀어놓음
    피시방 자리 1~100 인 걸 까먹을 뻔 함
    결과적으로 for문으로 int i 지정하는 것보다는 while문 사용이 나을 것 같아서 처음에 받아놓은 totalCustomerCnt는 안 사용하게 되었는데... 어느 방법이 더 나은지 제출 시 한 번 테스트해볼 것

---
+++ questions/src/main/java/baekjoon/processing/Main1453.java
@@ -0,0 +1,25 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main1453 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int totalCustomerCnt = Integer.parseInt(br.readLine()); //미사용
+        int refusedCustomerCnt = 0;
+        //false: 비어있는 자리, true: 선점된 자리
+        boolean[] seatList = new boolean[100]; //피시방 자리 1~100
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while(st.hasMoreTokens()) {
+            int seatNo = Integer.parseInt(st.nextToken()) -1; //1번 -> 0번으로 시작
+            if(seatList[seatNo]) refusedCustomerCnt++;
+            else seatList[seatNo] = true;
+        }
+        bw.write(refusedCustomerCnt + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
