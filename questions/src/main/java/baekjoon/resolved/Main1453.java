60303252086f6a50a7cd8afcc2ed52775bbfdb2d
hmkim <elppapie@naver.com>
2024-09-18 21:08:57 +0900

    :+1: 1453번 해결
    제출해보니 for문 사용하는 것보다 while문 사용이 메모리와 시간상 모두 이득인 것을 확인함
    
    제출 번호	아이디	메모리	시간	언어		코드 길이
    84020438	bcdlife	15928	116	Java 11	996	-- for문 사용
    84020314	bcdlife	15748	112	Java 11	958	-- while문 사용
    
    잠깐 생각해봐도 당연한 게, for문은 비교문, 실행문까지 한 번 loop 돌 때마다 실행하는 것이 많음.
    while문은 비교문만 실행함.

--- questions/src/main/java/baekjoon/resolved/Main1453.java
+++ questions/src/main/java/baekjoon/resolved/Main1453.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.StringTokenizer;
@@ -7,7 +7,8 @@ public class Main1453 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int totalCustomerCnt = Integer.parseInt(br.readLine()); //미사용
+        //int totalCustomerCnt = Integer.parseInt(br.readLine()); //미사용
+        br.readLine(); //미사용 처리
         int refusedCustomerCnt = 0;
         //false: 비어있는 자리, true: 선점된 자리
         boolean[] seatList = new boolean[100]; //피시방 자리 1~100
