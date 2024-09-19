b89a4c4500b876e90a682e5eac74d461fc8d9311
hmkim <elppapie@naver.com>
2024-09-20 08:14:39 +0900

    :+1: 1333번 해결
    미사용 로직은 지움

--- questions/src/main/java/baekjoon/resolved/Main1333.java
+++ questions/src/main/java/baekjoon/resolved/Main1333.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
@@ -55,24 +55,5 @@ public class Main1333 {
         bw.flush();
         bw.close();
         br.close();
-
-        /*
-        int n = Integer.parseInt(st.nextToken());
-        int l = Integer.parseInt(st.nextToken());
-        int d = Integer.parseInt(st.nextToken());
-        boolean heardBell = false;
-
-       int disturbTime = n*l + (n-1)*5;
-       boolean[] arr = new boolean[disturbTime];
-        /*
-        노래 사이 5초 간격이 언제인지 계산,
-        전화벨이 울리는 시간이 언제인지 계산,
-        둘이 대조하기
-
-        for(int i=0; i<n; i++) {
-            for(int j=i*(l+5); j<(i+1)*(l+5)-1; j++) {
-                //if()
-            }
-        } */
     }
 }
