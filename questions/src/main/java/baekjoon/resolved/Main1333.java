fbaa4751b61537a6eda473c56d07459f8fa565be
hmkim <elppapie@naver.com>
2024-09-20 12:56:59 +0900

    :zap: 1333번 배열 미사용 시도
    그러나 기존보다 메모리와 시간 모두 사용량이 늘어남
    loop 문 내부에 if 문이 너무 많아져서 그런 듯...
    
    84085052	bcdlife	 1333	맞았습니다!!	15948	124	Java 11 / 수정	1994
    84077247	bcdlife	 1333	맞았습니다!!	15892	116	Java 11 / 수정	1964

--- questions/src/main/java/baekjoon/resolved/Main1333.java
+++ questions/src/main/java/baekjoon/resolved/Main1333.java
@@ -12,44 +12,53 @@ public class Main1333 {
         int songLength = Integer.parseInt(st.nextToken());
         int bellInterval = Integer.parseInt(st.nextToken());
         //강토가 듣는 것 (0:없음, 1:벨소리, 2:음악)
-        int[] listen = new int[songCnt*(songLength + 5) + bellInterval];
+        int listen = songCnt*(songLength + 5) + bellInterval;
         int songPlaying = 0, songInterval = 0;
         int songPlayed = 0;
         boolean playing = true;
         int firstHeard = -1;
-        for(int i=0; i<listen.length; i++) {
+        for(int i=0; i<listen; i++) {
+            boolean bellRing = false;
             //벨소리 울리기 [1]
-            if(i % bellInterval == 0) listen[i] = 1;
+            if(i % bellInterval == 0) bellRing = true;
             //노래 듣기 [2]
-            if(songPlayed == songCnt) continue;
+            if(songPlayed == songCnt) {
+                if(bellRing) {
+                    firstHeard = i;
+                    break;
+                }
+                continue;
+            }
             if(playing) {
                 if(songPlaying == songLength) {
                     playing = false;
                     songPlaying = 0;
                     songPlayed++;
                     songInterval++;
-                } else {
+                } else
                     songPlaying++;
-                    listen[i] = 2;
-                }
             } else {
                 if(songInterval == 5){
                     playing = true;
                     songInterval = 0;
                     songPlaying++;
-                    listen[i] = 2;
                 } else
                     songInterval++;
             }
-        }
-
-        //벨소리 들은 최초구간 찾기
-        for(int i=0; i<listen.length; i++) {
-            if(listen[i] == 1) {
+            //벨소리 들은 최초구간 찾기
+            if(bellRing && !playing) {
                 firstHeard = i;
                 break;
             }
         }
+
+//        //벨소리 들은 최초구간 찾기
+//        for(int i=0; i<listen.length; i++) {
+//            if(listen[i] == 1) {
+//                firstHeard = i;
+//                break;
+//            }
+//        }
         //출력
         bw.write(firstHeard + "\n");
         bw.flush();
