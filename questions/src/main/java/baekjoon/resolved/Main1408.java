433a977b7e2c37217478b583788b7ce94b0b6574
hmkim <elppapie@naver.com>
2024-09-22 09:43:21 +0900

    :zap: :arrow_right: 라이브러리 없이 로직만으로 풀어보기 진행중
    제출했다가 틀렸고... 초-분-시 순서대로 계산했어야 했다는 걸 깨달음
    [ 반례 ]
    input:
    13:52:30
    14:52:29
    output:
    01:-1:59
    answer:
    00:59:59

--- questions/src/main/java/baekjoon/resolved/Main1408.java
+++ questions/src/main/java/baekjoon/resolved/Main1408.java
@@ -1,36 +1,77 @@
 package baekjoon.resolved;
 
 import java.io.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
+import java.util.*;
 
 public class Main1408 {
-    public static void main(String[] args) throws IOException, ParseException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
-        Date nowTime = format.parse(br.readLine());
-        Date startTime = format.parse(br.readLine());
-        Date endTime = (Date) startTime.clone();
-        endTime.setTime(endTime.getTime() + TimeUnit.DAYS.toMillis(1));
-        long diff;
-
-        if(nowTime.before(startTime)) { //현재시간이 더 작을 경우 자정을 넘긴 것으로 간주
-            nowTime.setTime(nowTime.getTime() + TimeUnit.DAYS.toMillis(1));
+        //입력
+        StringTokenizer now = new StringTokenizer(br.readLine(), ":");
+        StringTokenizer start = new StringTokenizer(br.readLine(), ":");
+        int[] nowTime = new int[3];
+        int[] startTime = new int[3];
+        for(int i=0; i<3; i++) {
+            nowTime[i] = Integer.parseInt(now.nextToken());
+            startTime[i] = Integer.parseInt(start.nextToken());
         }
-
-        diff = endTime.getTime() - nowTime.getTime();
-        long hour = TimeUnit.MILLISECONDS.toHours(diff);
-        long min = TimeUnit.MILLISECONDS.toMinutes(diff) % 60;
-        long sec = TimeUnit.MILLISECONDS.toSeconds(diff) % 60;
-
-        bw.write(String.format("%02d:%02d:%02d", hour, min, sec));
-
+        //연산 + 출력
+        bw.write(getTimeDiff(startTime, nowTime));
+        bw.newLine();
         bw.flush();
         bw.close();
         br.close();
+    }
+
+    /**
+     * nowTime에서 endTime(startTime + 24시간)까지의 남은시간을 구한다.
+     * 다음날이면 단순히 시간차이 구하기, 임무시작 당일이라면 24시간에서 흐른시간 빼기
+     * @param startTime 임무 시작시간
+     * @param nowTime 현재시간
+     * @return 시간차이 문자열 (XX:XX:XX)
+     */
+    public static String getTimeDiff(int[] startTime, int[] nowTime) {
+        boolean nextDay; //다음날여부 (24시간에서 시간차를 빼야 하는지 여부)
+        int[] former, latter;
+        nextDay = checkIsNextDay(startTime, nowTime);
+        former = nextDay ? nowTime : startTime;
+        latter = nextDay ? startTime : nowTime;
+
+        int hour = latter[0] - former[0];
+        int minute = latter[1] - former[1];
+        if(minute < 0) {
+            hour--;
+            minute += 60;
+        }
+        int second = latter[2] - former[2];
+        if(second < 0) {
+            minute--;
+            second += 60;
+        }
+        if(!nextDay) { //임무시작 다음날이 아니라면 24시간에서 지나간 시간을 빼기
+            second = 60 - second;
+            minute = 59 - minute;
+            hour = 23 - hour;
+        }
+        return String.format("%02d:%02d:%02d", hour, minute, second);
+    }
+
+    /**
+     * 현재시간이 임무 시작 다음날인지 확인
+     * @param startTime 임무 시작시간
+     * @param nowTime 현재시간
+     * @return 현재시간이 임무 시작시간의 다음날인지 여부
+     */
+    public static boolean checkIsNextDay(int[] startTime, int[] nowTime) {
+        boolean nextDay = false; //다음날여부
+        int[] timeCompare = new int[3];
+        for(int i=0; i<3; i++) timeCompare[i] = Integer.compare(nowTime[i], startTime[i]);
+
+        if(timeCompare[0] < 0) nextDay = true; //시간에서 더 작을 경우
+        else if(timeCompare[0] == 0)
+            if(timeCompare[1] < 0) nextDay = true; //분에서 더 작을 경우
+            else if(timeCompare[1] == 0 && timeCompare[2] < 0) nextDay = true; //초에서 더 작을 경우
+        return nextDay;
     }
 }
