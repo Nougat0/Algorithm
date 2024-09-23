392e92df0356d06e81f9332de7dbe44b7f4e55f7
hmkim <elppapie@naver.com>
2024-09-23 23:47:58 +0900

    :arrow_right: 1672번 풀어놓음 (미제출)
    염기서열 해독하기... 메모리 제한 (기존 128MB, 이 문제는 16MB) 이 너무 작아서 최대한 메모리를 덜 먹을 듯한 int 로 전부 바꿔서 문제를 풀도록 구성함
    미리 제공된 해독용 표도 전부 int 로 미리 만들어서 하드코딩해놓고 사용함

---
+++ questions/src/main/java/baekjoon/processing/Main1672.java
@@ -0,0 +1,28 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+
+public class Main1672 {
+    static final int[][] baseMap = new int[][] {{0, 2, 0, 1}, {2, 1, 3, 0}, {0, 3, 2, 1}, {1, 0, 1, 3}}; //미리 제공된 해독용 표
+    static final List<Character> nucleicOrder = new ArrayList<>(List.of('A', 'G', 'C', 'T')); //염기서열 숫자 1:1 순서
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int length = Integer.parseInt(br.readLine());
+        //염기서열 입력받아서 int[] 로 변환
+        //미리 제공된 표에 나와있듯이 A(0), G(1), C(2), T(3) -> nucleicOrder 에 저장해놓기
+        char[] list = br.readLine().toCharArray();
+        int[] listToInt = new int[length];
+        for(int i=0; i<length; i++) listToInt[i] = nucleicOrder.indexOf(list[i]);
+
+        //염기서열 해독
+        for(int i=length-1; i>0; i--) {
+            listToInt[i-1] = baseMap[listToInt[i-1]][listToInt[i]];
+        }
+        bw.write(String.format("%c%n", nucleicOrder.get(listToInt[0])));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
