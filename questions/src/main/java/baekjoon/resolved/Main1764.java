c8f67b1b83f9413d8bcd59c5ef7009d5bd4d4550
hmkim <elppapie@naver.com>
2024-09-29 10:56:53 +0900

    :+1: 1764번 해결 (solved.ac 3단계)
    \출력하는 목록을 HashSet으로 했다가 틀림...(제한된 정렬) TreeSet으로 고쳐서 해결

--- questions/src/main/java/baekjoon/resolved/Main1764.java
+++ questions/src/main/java/baekjoon/resolved/Main1764.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
@@ -10,29 +10,19 @@ public class Main1764 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int neverHeard = Integer.parseInt(st.nextToken()); //듣도 못한 사람 수
         int neverSeen = Integer.parseInt(st.nextToken()); //보도 못한 사람 수
-        List<String> neverKnown = new ArrayList<>(); //듣보잡 목록
+        TreeSet<String> neverKnown = new TreeSet<>(); //듣보잡 목록
 
-        Map<Character, List<String>> neverHeardMap = new HashMap<>(); //듣도 못한 사람 목록
-        for(int i=0; i<neverHeard; i++) { //주소록처럼 입력
-            String name = br.readLine();
-            char firstChar = name.charAt(0);
-            if(neverHeardMap.containsKey(firstChar)) { //기존 List에 추가
-                neverHeardMap.get(firstChar).add(name);
-            } else { //해당 알파벳의 첫 등록
-                List<String> list = new ArrayList<>();
-                list.add(name);
-                neverHeardMap.put(firstChar, list);
-            }
-        }
+        TreeSet<String> neverHeardMap = new TreeSet<>(); //듣도 못한 사람 목록
+        for(int i=0; i<neverHeard; i++)
+            neverHeardMap.add(br.readLine());
 
         for(int i=0; i<neverSeen; i++) { //듣도 못한 사람들 보도 못한 사람들과 대조
             String name = br.readLine();
-            char firstChar = name.charAt(0);
-            if(neverHeardMap.containsKey(firstChar) && neverHeardMap.get(firstChar).contains(name))
+            if(neverHeardMap.contains(name))
                 neverKnown.add(name);
         }
 
-        neverKnown.sort(Comparator.naturalOrder()); //알파벳순 정렬
+        //neverKnown.sort(Comparator.naturalOrder()); //알파벳순 정렬
         bw.write(neverKnown.size() + "\n"); //듣보잡 개수 출력
         for(String name : neverKnown) { //듣보잡 하나씩 출력
             bw.write(name);
