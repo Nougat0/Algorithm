09991270b7a42797fa162de3781e34288720022a
hmkim <elppapie@naver.com>
2024-10-11 12:43:14 +0900

    :arrow_right: :athletic_shoe: 6780번 풀어놓음 (미제출)

---
+++ questions/src/main/java/baekjoon/processing/Main6780.java
@@ -0,0 +1,24 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+
+public class Main6780 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t1 = Integer.parseInt(br.readLine());
+        int t2 = Integer.parseInt(br.readLine());
+        int sequenceSize = 2; //크기만 확인 - 굳이 배열일 필요가 없음
+        int nextSequence;
+        while((nextSequence = t1-t2) >= 0) { //새 시퀀스 값 도출 가능할 때만
+            t1 = t2; //한 칸씩 밀어넣기
+            t2 = nextSequence; //새로운 sequence 값 도출
+            sequenceSize++;
+        }
+        bw.write(sequenceSize + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
