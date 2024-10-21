df7408d583a92800d7dbf94637a777cbda5b0d7f
hmkim <elppapie@naver.com>
2024-10-22 08:26:24 +0900

    :+1: :zap: :athletic_shoe: 32305번 해결 (마라톤:코스 20 완주)
    1. 3항 연산자(if문) 사용보다는 나머지 올림이 되도록 값을 더해주는 게 좋다...
    특히 이번에는 나눌 숫자가 12로 미리 제시되어 있는 고정값이었으므로 충분히 가능한 방법
    
    2. 메모리 + 시간복잡도 소요는 bufferedwriter 선언 및 사용 시 상당수 지분을 차지하는 듯
    채점용 테스트케이스가 그렇게 많지 않은 이번 문제의 경우? sout 사용이 오히려 시간이나 메모리 측면에서 더 효율적이었다...

--- questions/src/main/java/baekjoon/resolved/Main32305.java
+++ questions/src/main/java/baekjoon/resolved/Main32305.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
@@ -6,15 +6,12 @@ import java.util.*;
 public class Main32305 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         //입력
         StringTokenizer st = new StringTokenizer(br.readLine());
         int totalApples = Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken());
         int costOfDozen = Integer.parseInt(br.readLine());
         //연산 + 출력 (dozen 의 나머지가 있을 경우 dozen 으로 취급(올림))
-        bw.write((totalApples/12 + (totalApples%12 == 0 ? 0 : 1)) * costOfDozen + "\n");
-        bw.flush();
-        bw.close();
+        System.out.println((totalApples + 11)/12 * costOfDozen);
         br.close();
     }
 }
