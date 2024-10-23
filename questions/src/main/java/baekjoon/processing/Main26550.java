fc6ee9089692dd0254d7192221663e01c8915015
hmkim <elppapie@naver.com>
2024-10-24 08:59:53 +0900

    :arrow_right: :alarm_clock: 26550번 해결중
    시도1 : 무조건 2중 for문 돌려야해서 그런지 초장부터 시간초과 뜸
    시도2 : 레이어별로 ornament가 몇 개씩 있는지 값을 저장해놓고 사용하려고 나름의 DP를 적용해보았으나 25%까지 채점하다가 시간초과가 뜸
    아무래도 갑자기 큰 layer 값이 주어지면 대부분의 layer에서 이중 for문을 돌려야 해서 그런 듯...

--- questions/src/main/java/baekjoon/processing/Main26550.java
+++ questions/src/main/java/baekjoon/processing/Main26550.java
@@ -1,19 +1,28 @@
 package baekjoon.processing;
 
 import java.io.*;
+import java.util.*;
 
 public class Main26550 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Map<Integer, Integer> layerOrnaments = new HashMap<>();
         int testCase = Integer.parseInt(br.readLine());
         for(int t=0; t<testCase; t++) {
             int layers = Integer.parseInt(br.readLine());
             int ornaments = 0;
             //레이어별 ornament 더하기
             for(int layer=1; layer<=layers; layer++) {
-                //각 레이어의 ornament 구하기
-                for(int o=1; o<=layer; o++) ornaments += o;
+                if(layerOrnaments.containsKey(layer)) {
+                    ornaments += layerOrnaments.get(layer);
+                } else {
+                    int oneLayer = 0;
+                    //각 레이어의 ornament 구하기
+                    for(int o=1; o<=layer; o++) oneLayer += o;
+                    layerOrnaments.put(layer, oneLayer);
+                    ornaments += oneLayer;
+                }
             }
             bw.write(ornaments + "\n");
         }
