b934ece019bac75c16928e11a193c8d97f22d268
hmkim <elppapie@naver.com>
2024-10-24 09:11:31 +0900

    :+1: :athletic_shoe: 26550번 해결
    - 이중 for문을 사칙연산을 활용해서 단일 for문으로 변경,
    - N 의 제한이 없어서 작게만 생각했다가 질문게시판에서 반례를 찾아보니 연산값이 long 의 범위라... int 끼리의 연산에 long 변수를 추가하여 특정 layer 연산 시 int 범위 때문에 - 값이 나오는 현상 제어함

--- questions/src/main/java/baekjoon/resolved/Main26550.java
+++ questions/src/main/java/baekjoon/resolved/Main26550.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
@@ -7,22 +7,14 @@ public class Main26550 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        Map<Integer, Integer> layerOrnaments = new HashMap<>();
         int testCase = Integer.parseInt(br.readLine());
         for(int t=0; t<testCase; t++) {
             int layers = Integer.parseInt(br.readLine());
-            int ornaments = 0;
+            long ornaments = 0;
             //레이어별 ornament 더하기
-            for(int layer=1; layer<=layers; layer++) {
-                if(layerOrnaments.containsKey(layer)) {
-                    ornaments += layerOrnaments.get(layer);
-                } else {
-                    int oneLayer = 0;
-                    //각 레이어의 ornament 구하기
-                    for(int o=1; o<=layer; o++) oneLayer += o;
-                    layerOrnaments.put(layer, oneLayer);
-                    ornaments += oneLayer;
-                }
+            for(long layer=1; layer<=layers; layer++) {
+                //int 범위를 넘어가는 연산이 존재, long 타입의 index 변수 사용
+                ornaments += (layers - (layer - 1)) * layer;
             }
             bw.write(ornaments + "\n");
         }
