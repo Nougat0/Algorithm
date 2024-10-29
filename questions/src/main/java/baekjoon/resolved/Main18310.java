5ef50eaf8af57039d94d77457a81c5321651ec3b
hmkim <elppapie@naver.com>
2024-10-29 13:33:38 +0900

    :+1: :athletic_shoe: 18310번 해결 (마라톤:코스 21 완주)
    중앙값으로 해결...!

--- questions/src/main/java/baekjoon/resolved/Main18310.java
+++ questions/src/main/java/baekjoon/resolved/Main18310.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
@@ -13,19 +13,22 @@ public class Main18310 {
         int[] houses = new int[houseCnt];
         st = new StringTokenizer(br.readLine());
         for(int h=0; h<houseCnt; h++) houses[h] = Integer.parseInt(st.nextToken());
+        Arrays.sort(houses); //오름차순 정렬
+
         //적절한 안테나의 위치 선정
-        long minDistance = Long.MAX_VALUE;
-        int houseNo = 0;
-        for(int h=0; h<houseCnt; h++) {
-            long distance = 0;
-            for(int i=0; i<houseCnt; i++) {
-                if(h==i) continue;
-                distance += Math.abs(houses[h] - houses[i]);
+        int houseNo;
+        if(houseCnt%2 != 0) //중앙값이 1개일 경우 그대로 사용
+            houseNo = houseCnt/2;
+        else { //중앙값이 2개일 경우 비교하기
+            int case1 = houseCnt/2 - 1, case2 = houseCnt/2;
+            long case1Distance = 0, case2Distance = 0;
+            for(int h=0; h<houseCnt; h++) {
+                case1Distance += Math.abs(houses[h] - houses[case1]);
+                case2Distance += Math.abs(houses[h] - houses[case2]);
             }
-            if(distance < minDistance) {
-                minDistance = distance;
-                houseNo = h;
-            }
+            //거리가 같을 경우에도 더 작은 값 선택
+            if(case1Distance <= case2Distance) houseNo = case1;
+            else houseNo = case2;
         }
         //출력
         bw.write(houses[houseNo] + "\n");
