3af80ccd9fcb9eae672ed6fe346848be2ac05738
hmkim <elppapie@naver.com>
2024-11-05 20:15:21 +0900

    :zap: 6463번 BigInteger -> long 사용
    기존 규칙 (number를 n 최대값보다 크게 유지) 을 지켰더니 맞음 + 훨씬 빠르고 메모리 적게 사용

--- questions/src/main/java/baekjoon/resolved/Main6463.java
+++ questions/src/main/java/baekjoon/resolved/Main6463.java
@@ -12,14 +12,13 @@ public class Main6463 {
         while((line = br.readLine()) != null) {
             int n = Integer.parseInt(line);
             int length = String.valueOf(n).length();
-            BigInteger number = BigInteger.ONE;
+            long number = 1;
             for(int i=1; i<=n; i++) {//팩토리얼 계산
-                number = number.multiply(BigInteger.valueOf(i));
-                while(number.remainder(BigInteger.TEN).compareTo(BigInteger.ZERO) == 0)
-                    number = number.divide(BigInteger.TEN); //뒤에 붙은 0 제거
-                number = number.remainder(BigInteger.valueOf(100000)); //10만까지 자르기
+                number *= i;
+                while(number%10 == 0) number /= 10; //뒤에 붙은 0 제거
+                number %= 100000; //10만까지 자르기
             }
-            sb.append(" ".repeat(5-length)).append(n).append(" -> ").append(number.remainder(BigInteger.TEN)).append("\n");
+            sb.append(" ".repeat(5-length)).append(n).append(" -> ").append(number%10).append("\n");
         }
         bw.write(sb.toString());
         bw.flush();
