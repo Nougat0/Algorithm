9353492196bc87db634bc6471e011187959bab8a
hmkim <elppapie@naver.com>
2024-11-05 23:35:20 +0900

    :arrow_right: 32373번 해결중... (2)
    문제를 다시 읽고 i+k 만 체크해봤는데,,, (i 에서 미리 윗부분이랑 교환해서 정렬되는지 확인) 틀렸다!
    설마 distance 칸마다 있는 값들을 서로 돌려돌려 교환하는 느낌인가?
    distance가 2일 경우, 2=>4랑 교환도 가능하지만 4=>6도 교환이 가능하니까 ... 문제에 교환 횟수 제한 명시가 없다는 게 마음에 걸린다

--- questions/src/main/java/baekjoon/processing/Main32373.java
+++ questions/src/main/java/baekjoon/processing/Main32373.java
@@ -7,13 +7,14 @@ public class Main32373 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st; 
         //입력
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
+        int distance = Integer.parseInt(st.nextToken());
         int[] lock = new int[n]; //자물쇠 수열
         boolean[] itemsAligned = new boolean[n]; //각 숫자가 오름차순 정렬되었는지 확인
         boolean aligned = true; //자물쇠 불량 여부
-        int distance = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++) {
             lock[i] = Integer.parseInt(st.nextToken());
@@ -21,9 +22,11 @@ public class Main32373 {
         }
         //연산
         for(int i=0; i<n; i++)
-            if(!itemsAligned[i])
-                //윗 번호나 아래 번호와 바꿔서 정렬이 되지 않을 경우
-                if(!(i+distance < n && lock[i+distance] == i || i-distance >= 0 && lock[i-distance] == i)) {
+            if(!itemsAligned[i]) //정렬이 안된 수열값에 대해서 정렬 가능한지 확인
+                if(i+distance < n && !itemsAligned[i+distance] && lock[i+distance] == i) {
+                    itemsAligned[i] = true;
+                    itemsAligned[i+distance] = true;
+                } else { //윗 번호와 바꿔서 정렬이 되지 않을 경우
                     aligned = false;
                     break;
                 }
