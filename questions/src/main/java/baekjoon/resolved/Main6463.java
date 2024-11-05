a6145d9d80fd3f6c6adfab5307434e56b6871ec0
hmkim <elppapie@naver.com>
2024-11-05 20:01:32 +0900

    :+1: :athletic_shoe: 6463번 해결
    이전에 확인한 대로 10만처럼 큰 숫자로 자르니 메모리 초과나 오류 없이 맞음

--- questions/src/main/java/baekjoon/resolved/Main6463.java
+++ questions/src/main/java/baekjoon/resolved/Main6463.java
@@ -14,11 +14,11 @@ public class Main6463 {
             int length = String.valueOf(n).length();
             BigInteger number = BigInteger.ONE;
             for(int i=1; i<=n; i++) {//팩토리얼 계산
-                System.out.println(i+" :: "+number);
                 number = number.multiply(BigInteger.valueOf(i));
+                while(number.remainder(BigInteger.TEN).compareTo(BigInteger.ZERO) == 0)
+                    number = number.divide(BigInteger.TEN); //뒤에 붙은 0 제거
+                number = number.remainder(BigInteger.valueOf(100000)); //10만까지 자르기
             }
-            while(number.remainder(BigInteger.TEN).compareTo(BigInteger.ZERO) == 0)
-                number = number.divide(BigInteger.TEN); //뒤에 붙은 0 제거
             sb.append(" ".repeat(5-length)).append(n).append(" -> ").append(number.remainder(BigInteger.TEN)).append("\n");
         }
         bw.write(sb.toString());
