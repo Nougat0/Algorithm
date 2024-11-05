ba61a09e53d099719f605784ac74ae83c888560e
hmkim <elppapie@naver.com>
2024-11-05 19:01:15 +0900

    :arrow_right: :floppy_disk: 6463번 해결중 (메모리초과)
    - 1의 자리만 남겨서 계산했더니 1의 자리가 0이 되면서 10이상의 자리를 가져올 때 숫자가 다른 오류 발생
    - 그래서 BigInteger 로 전부 다 남기고 사용했더니 메모리 초과가 뜸
    - 결국 매번 곱할 때마다 곱할 숫자의 자릿수만큼은 최소한으로 숫자를 남겨서 계산해야 함
    
    - 26의 예시 - 25 곱할 때) 6*25 = 150 // 3936*25 = 98400
    - 위처럼 1의 자리가 다르게 나오는 이유와 같다. 기존 윗부분 남겨뒀으면 6*25로 나온 10 이상의 자릿수와 더해서 0이 됐을 텐데 없애버려서 다른 결과가 나옴

---
+++ questions/src/main/java/baekjoon/processing/Main6463.java
@@ -0,0 +1,29 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main6463 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while((line = br.readLine()) != null) {
+            int n = Integer.parseInt(line);
+            int length = String.valueOf(n).length();
+            BigInteger number = BigInteger.ONE;
+            for(int i=1; i<=n; i++) {//팩토리얼 계산
+                System.out.println(i+" :: "+number);
+                number = number.multiply(BigInteger.valueOf(i));
+            }
+            while(number.remainder(BigInteger.TEN).compareTo(BigInteger.ZERO) == 0)
+                number = number.divide(BigInteger.TEN); //뒤에 붙은 0 제거
+            sb.append(" ".repeat(5-length)).append(n).append(" -> ").append(number.remainder(BigInteger.TEN)).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
