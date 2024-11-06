526604a1fd724a3f3f28dcc5c3911ba031768080
hmkim <elppapie@naver.com>
2024-11-06 18:38:43 +0900

    :+1: 32373번 해결 !! (5번 시도)
    - 중복입력 체크로직과 ArrayList.indexOf 가 남은 코드 중 가장 오래 걸릴 것 같아서 ArrayList -> HashSet 으로 변경함
    - index 에 몫 말고 깡 index를 넣도록 코드 바꿈 (index * 거리 + 나머지) 로직을 여러번 진행하는 것이 아주 크진 않더라도 시간 소요 잡아먹을 테니까 조금이라도 더 줄여보려고...
    
    ArrayList 가 느린 (시간복잡도 소요량 큰) 것은 처음 체감함... 향후 문제풀 때 참고할 것

--- questions/src/main/java/baekjoon/resolved/Main32373.java
+++ questions/src/main/java/baekjoon/resolved/Main32373.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
@@ -14,28 +14,27 @@ public class Main32373 {
         int distance = Integer.parseInt(st.nextToken());
         int[] lock = new int[n]; //자물쇠 수열
         boolean aligned = true; //자물쇠 불량 여부
-        List<Integer> notAlignedItems = new ArrayList<>(); //정렬되지 않은 시작점들
         st = new StringTokenizer(br.readLine());
+        Set<Integer> notAlignedItems = new HashSet<>(); //정렬되지 않은 시작점들
         for(int i=0; i<n; i++) {
             lock[i] = Integer.parseInt(st.nextToken());
             //정렬되지 않은 값들의 시작점 (교환가능 거리의 나머지값들)
-            if(lock[i] != i && notAlignedItems.indexOf(i%distance) == -1)
-                notAlignedItems.add(i%distance);
+            if(lock[i] != i) notAlignedItems.add(i%distance);
         }
         //연산
         if(notAlignedItems.size() > 0) { //정렬되지 않은 요소 존재 시
             EXIT_FOR:
             for(int notAlignedItem : notAlignedItems) {
                 //교환가능 거리 간격의 값들(같은 나머지값) 정말 나머지값이 같은지 검사
-                int index = 0;
-                while(index*distance + notAlignedItem < n) {
+                int index = notAlignedItem; //초기값 나머지
+                while(index < n) {
                     //나머지값이 다를 시 불량 자물쇠
-                    if(lock[index*distance + notAlignedItem] % distance != notAlignedItem) {
+                    if(lock[index] % distance != notAlignedItem) {
                         aligned = false;
                         break EXIT_FOR;
                     }
                     //다음 검사할 index
-                    index++;
+                    index += distance;
                 }
             }
         }
