b5761687487d917b3cafa69feb97feabb34e770e
hmkim <elppapie@naver.com>
2024-11-06 17:38:47 +0900

    :arrow_right: :alarm_clock: 32373번 해결중
    이제는 채점은 되는데 5%에서 시간초과가 뜸...
    접근 방식은 맞으나 시간복잡도가 덜한 방향으로 수정 필요

--- questions/src/main/java/baekjoon/processing/Main32373.java
+++ questions/src/main/java/baekjoon/processing/Main32373.java
@@ -1,7 +1,7 @@
 package baekjoon.processing;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main32373 {
     public static void main(String[] args) throws IOException {
@@ -13,23 +13,36 @@ public class Main32373 {
         int n = Integer.parseInt(st.nextToken());
         int distance = Integer.parseInt(st.nextToken());
         int[] lock = new int[n]; //자물쇠 수열
-        boolean[] itemsAligned = new boolean[n]; //각 숫자가 오름차순 정렬되었는지 확인
         boolean aligned = true; //자물쇠 불량 여부
+        List<Integer> notAlignedItems = new ArrayList<>(); //정렬되지 않은 시작점들
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++) {
             lock[i] = Integer.parseInt(st.nextToken());
-            itemsAligned[i] = lock[i] == i;
+            //정렬되지 않은 값들의 시작점 (교환가능 거리의 나머지값들)
+            if(lock[i] != i && notAlignedItems.indexOf(i%distance) == -1)
+                notAlignedItems.add(i%distance);
         }
-        //연산
-        for(int i=0; i<n; i++)
-            if(!itemsAligned[i]) //정렬이 안된 수열값에 대해서 정렬 가능한지 확인
-                if(i+distance < n && !itemsAligned[i+distance] && lock[i+distance] == i) {
-                    itemsAligned[i] = true;
-                    itemsAligned[i+distance] = true;
-                } else { //윗 번호와 바꿔서 정렬이 되지 않을 경우
-                    aligned = false;
-                    break;
+
+        if(notAlignedItems.size() > 0) { //정렬되지 않은 요소 존재 시
+            List<Integer> newLockArr;
+            EXIT_FOR:
+            for(int notAlignedItem : notAlignedItems) {
+                //교환가능 거리 간격의 값들(같은 나머지값)을 새로운 수열로 가져오기
+                newLockArr = new ArrayList<>();
+                for(int i=0; i<=n/distance && i*distance + notAlignedItem <n; i++)
+                    newLockArr.add(lock[i*distance + notAlignedItem]);
+                Collections.sort(newLockArr); //오름차순 정렬
+                //새로운 수열의 element 값들 간의 간격이 교환가능 거리와 일치하는지 확인
+                for(int i=0; i<n/distance-1; i++) {
+                    if(newLockArr.get(i+1) - newLockArr.get(i) != distance) {
+                        //일치하지 않을 경우 바로 불량품 판정
+                        aligned = false;
+                        break EXIT_FOR;
+                    }
                 }
+            }
+        }
+
         //출력
         bw.write(aligned ? "Yes" : "No");
         bw.newLine();
