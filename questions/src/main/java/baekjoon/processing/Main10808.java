6543bd099cf50486a11e20d2b9464f501fbde0a8
hmkim <elppapie@naver.com>
2024-11-06 18:57:41 +0900

    :arrow_right: :athletic_shoe: 10808번 풀어놓음 (미제출) -- byte
    전체 문자열의 길이나 소문자 ascii 코드번호 개수나 전부 100 이하라서 byte 자료형을 사용해봄

---
+++ questions/src/main/java/baekjoon/processing/Main10808.java
@@ -0,0 +1,24 @@
+package baekjoon.processing;
+
+import java.io.*;
+
+public class Main10808 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        //입력, 연산
+        char[] word = br.readLine().toCharArray(); //97~122
+        byte[] alphabet = new byte[26]; //최대 100개라서 byte 사용
+        byte length = (byte) word.length;
+        for(byte a=0; a<length; a++)
+            alphabet[word[a]-97]++;
+        //출력
+        for(byte a=0; a<26; a++)
+            sb.append(alphabet[a]).append(" ");
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
