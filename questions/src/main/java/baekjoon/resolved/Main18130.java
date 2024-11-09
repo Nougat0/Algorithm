2fc40c6f59be09e9f4ac6f0ed49edda5d2530c98
hmkim <elppapie@naver.com>
2024-11-09 21:50:49 +0900

    :+1: :athletic_shoe: 18130번 해결
    - 시간복잡도를 줄이기 위해 while문을 제외, 수학식으로 추가비용 계산
    - 마지막 도착할 때의 추가비용은 무시하도록 수학식 수정 (time-1)... 어차피 비용은 무조건 정수(시간) 으로 계산되니까 -1을 해도 계산에는 문제가 없음

--- questions/src/main/java/baekjoon/resolved/Main18130.java
+++ questions/src/main/java/baekjoon/resolved/Main18130.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
@@ -12,25 +12,19 @@ public class Main18130 {
         //입력, 연산
         short n = Short.parseShort(st.nextToken());
         int time = Integer.parseInt(st.nextToken());
-        int[] fanInfo;
+        long[] fanInfo;
         
         long leastExpense = Long.MAX_VALUE;
         short leastFan = -1;
         //손풍기 기기별 비용 예상
         for(short i=0; i<n; i++) {
-            fanInfo = new int[3]; //저장: [0]손풍기 가격, [1]추가비용 부과시간, [2]추가비용
+            fanInfo = new long[3]; //저장: [0]손풍기 가격, [1]추가비용 부과시간, [2]추가비용
             st = new StringTokenizer(br.readLine());
-            for(short j=0; j<3; j++) fanInfo[j] = Integer.parseInt(st.nextToken());
+            for(short j=0; j<3; j++) fanInfo[j] = Long.parseLong(st.nextToken());
             //비용 계산
             long expense = fanInfo[0]; //손풍기 가격
-            int timeflow = fanInfo[1], section = 1; //추가비용 첫 구간으로 초기화
-            while(timeflow < time) {
-                //추가비용 부과
-                expense += (long) section*fanInfo[2];
-                //다음 추가비용 부과구간
-                timeflow += fanInfo[1];
-                section++;
-            }
+            long quotient = (time-1)/fanInfo[1]; //도착시간과 비용내는 시간이 겹칠 때의 비용은 제외하기 위해 time-1
+            expense += fanInfo[2] * ((quotient + 1) * (quotient/2) + (quotient%2 != 0 ? quotient/2+1 : 0));
             //최소비용인지 확인
             if(expense < leastExpense) {
                 leastFan = (short) (i+1);
