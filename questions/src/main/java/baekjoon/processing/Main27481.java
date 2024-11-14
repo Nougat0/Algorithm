49be8a5132d1f114f4d9a0d592e4fe4e0537e858
hmkim <elppapie@naver.com>
2024-11-14 22:36:26 +0900

    :arrow_right: :athletic_shoe: 27481번 풀어놓음 (미제출)
    1초 시간제한 떄문에 for문은 안하고 싶었는데... 그나마 제일 간결한 방법 같아서 일단 사용해봄

---
+++ questions/src/main/java/baekjoon/processing/Main27481.java
@@ -0,0 +1,40 @@
+package baekjoon.processing;
+
+import java.io.*;
+
+public class Main27481 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        //입력
+        int n = Integer.parseInt(br.readLine());
+        boolean[] rooms = new boolean[10];
+        char[] customers = br.readLine().toCharArray();
+        //연산
+        for(int c=0; c<n; c++)
+            //왼쪽부터 들어가기
+            if(customers[c] == 'L') {
+                for(int i=0; i<10; i++)
+                    if(!rooms[i]) {
+                        rooms[i] = true;
+                        break;
+                    }
+            } //오른쪽부터 들어가기
+            else if(customers[c] == 'R') {
+                for(int i=9; i>0; i--)
+                    if(!rooms[i]) {
+                        rooms[i] = true;
+                        break;
+                    }
+            } //방 번호의 손님 나감
+            else rooms[Character.getNumericValue(customers[c])] = false;
+        //출력
+        for(int i=0; i<10; i++) sb.append(rooms[i] ? 1 : 0);
+        bw.write(sb.toString());
+        bw.newLine();
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
