6ca4c70fcd831410306ef680ffee4b87b2021696
hmkim <elppapie@naver.com>
2024-12-07 11:10:18 +0900

    :zap: 👟 13776번 2번째 loop제거
    시간을 더 줄일 수 있을 줄 알고 두번째 loop를 제거하고 첫 번째 loop에서 모든 작업을 진행했으나... 어째 시간은 똑같고 메모리만 더 소요되는 것으로 나타남...

--- questions/src/main/java/baekjoon/resolved/Main13776.java
+++ questions/src/main/java/baekjoon/resolved/Main13776.java
@@ -12,14 +12,13 @@ public class Main13776 {
         int assignNo = 1; //배정번호
         char[] line;
         int[] alphabet = new int[26]; //65~90 알파벳 값 배정(!=0) 확인용
-        char[] order = new char[26]; //65~90 배정순서 미리저장용
         //입력 및 연산
         allAssigned:
         for(int i=0; i<size; i++) {
             line = br.readLine().toCharArray();
             for(int j=0; j<line.length; j++) {
                 if(line[j] != ' ' && alphabet[line[j]-65] == 0) {
-                    order[assignNo-1] = line[j];
+                    sb.append(line[j]);
                     alphabet[line[j]-65] = assignNo++;
                 }
                 //알파벳 전부 등장 시 loop 중단
@@ -27,7 +26,6 @@ public class Main13776 {
             }
         }
         //출력
-        for(char c: order) sb.append(c);
         bw.write(sb.toString());
         bw.flush();
         bw.close();
