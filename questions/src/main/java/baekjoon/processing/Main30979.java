229e06dcc844b81b557101d32ba66591850d7c90
hmkim <elppapie@naver.com>
2024-12-11 12:51:15 +0900

    :arrow_right: 👟 30979번 풀어놓음 (미제출)
    파댕이 돌보기 (사탕주기)

---
+++ questions/src/main/java/baekjoon/processing/Main30979.java
@@ -0,0 +1,25 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+public class Main30979 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder("Padaeng_i ");
+        StringTokenizer st;
+        //입력
+        int babysit = Integer.parseInt(br.readLine());
+        int candy = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        //연산
+        int eating = 0;
+        for(int i=0; i<candy; i++) eating += Integer.parseInt(st.nextToken());
+        //출력
+        sb.append(eating >= babysit ? "Happy" : "Cry");
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
