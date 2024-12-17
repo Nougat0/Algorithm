f8f1ac7e2042755d6f0935f2c359425a9aea6391
hmkim <elppapie@naver.com>
2024-12-18 08:01:53 +0900

    :arrow_right: 👟 29751번 풀어놓음 (미제출)

---
+++ questions/src/main/java/baekjoon/processing/Main29751.java
@@ -0,0 +1,19 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+
+public class Main29751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        //가로길이와 높이 입력
+        double w = Double.parseDouble(st.nextToken());
+        double h = Double.parseDouble(st.nextToken());
+        bw.write(String.format("%.1f", w*h/2));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
