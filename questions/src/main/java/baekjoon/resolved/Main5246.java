a4b1a09ca6b76f48adffaa65857d2fc9e52ce3fa
hmkim <elppapie@naver.com>
2024-12-19 18:53:08 +0900

    👍 👟 5246번 해결
    ㅋㅋㅋㅋ 문제를 제대로 안 읽고 제멋대로 colMax 와 rowMax를 동시에 구하려 들었음
    문제에서 요구하는 것은 가로줄(row) 만 놓고 봤을 때 최대값이었다...

--- questions/src/main/java/baekjoon/resolved/Main5246.java
+++ questions/src/main/java/baekjoon/resolved/Main5246.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
@@ -10,30 +10,23 @@ public class Main5246 {
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         int boardCnt, pieceCnt;
-        boolean[][] board;
+        int[] rowCount;
         //보드별 정보
         boardCnt = Integer.parseInt(br.readLine());
         for(int i=0; i<boardCnt; i++) {
-            board = new boolean[8][8];
+            rowCount = new int[8];
             st = new StringTokenizer(br.readLine());
             //보드 위 구조물 위치 정보
             pieceCnt = Integer.parseInt(st.nextToken());
             for(int j=0; j<pieceCnt; j++) {
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
-                board[y-1][x-1] = true;
+                rowCount[y-1]++;
             }
             //가장 많은 구조물을 가진 줄 확인
             int maxLine = 0;
             for(int j=0; j<8; j++) {
-                int vertical = 0;
-                int horizontal = 0;
-                for(int k=0; k<8; k++) {
-                    if(board[j][k]) horizontal++;
-                    if(board[k][j]) vertical++;
-                }
-                if(horizontal > maxLine) maxLine = horizontal;
-                if(vertical > maxLine) maxLine = vertical;
+                if(rowCount[j] > maxLine) maxLine = rowCount[j];
             }
             sb.append(maxLine).append("\n");
         }
