2bfca915dd79198b37221c3f52d9e808866569cc
hmkim <elppapie@naver.com>
2024-12-20 18:45:25 +0900

    👍 👟 27487번 해결
    2가 등장하지 않을 때 인덱스 최소값은 무조건 1이다... 이걸 간과하고 가운데 인덱스를 출력하게 해서 틀림

--- questions/src/main/java/baekjoon/resolved/Main27487.java
+++ questions/src/main/java/baekjoon/resolved/Main27487.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
@@ -12,9 +12,9 @@ public class Main27487 {
         int testCase = Integer.parseInt(br.readLine());
         /**
          * 2와 1의 수열의 양측 곱이 일치하려면 양측 등장하는 2의 개수가 동일해야 함
-         * 1) 동일하지 못할 경우 -1 출력
+         * 1) 동일하지 못할 경우(2가 홀수번 등장) -1 출력
          * 2) 동일할 경우 좌측 마지막 2의 인덱스 출력 (인덱스 최소값)
-         * 3) 2가 등장하지 않을 경우 전체 수열의 가운데 인덱스 출력
+         * 3) 2가 등장하지 않을 경우 1 출력 (인덱스 최소값)
          */
         for(int t=0; t<testCase; t++) {
             int length = Integer.parseInt(br.readLine());
@@ -29,7 +29,7 @@ public class Main27487 {
                 }
             //출력
             if(twoListSize == 0) { //2가 등장하지 않음
-                sb.append(length/2 - 1);
+                sb.append(1);
             } else if(twoListSize%2 == 0) { //2의 개수가 짝수
                 sb.append(twoList.get(twoListSize/2 - 1) + 1);
             } else { //2의 개수가 홀수
