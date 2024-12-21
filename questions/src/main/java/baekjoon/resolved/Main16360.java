1b7fa04646f4dcd06bdfe113dbdaccb602455abb
hmkim <elppapie@naver.com>
2024-12-21 10:56:31 +0900

    👍 👟 16360번 해결

--- questions/src/main/java/baekjoon/resolved/Main16360.java
+++ questions/src/main/java/baekjoon/resolved/Main16360.java
@@ -1,9 +1,10 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
 
-public class Main16360 {
+public class
+Main16360 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
