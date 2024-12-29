001ece76515261f238383a1accf0eded34643a5b
hmkim <elppapie@naver.com>
2024-12-29 16:04:49 +0900

    :arrow_right: (레벨) 2744번 풀어놓음 (미제출)

---
+++ questions/src/main/java/baekjoon/processing/Main2744.java
@@ -0,0 +1,24 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main2744 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        char[] word = br.readLine().toCharArray();
+        for(char c : word) {
+            if(c/97 > 0) { //소문자
+                sb.append((char) (c-32));
+            } else { //대문자
+                sb.append((char) (c+32));
+            }
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
