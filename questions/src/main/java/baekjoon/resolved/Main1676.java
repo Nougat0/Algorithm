f9edb27f83b88e2e7114a835ccd0d0b010278d65
hmkim <elppapie@naver.com>
2025-01-07 19:36:16 +0900

    👍 👟 1676번 해결
    예상대로 25의 배수 125의 배수를 안 구해서 틀렸던 것

--- questions/src/main/java/baekjoon/resolved/Main1676.java
+++ questions/src/main/java/baekjoon/resolved/Main1676.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
@@ -11,8 +11,11 @@ public class Main1676 {
         /*
         0을 만드는 것은 5*2 -> 즉, 5의 배수의 등장횟수 구하기
         1,2,3...n까지 5의 배수 개수 구하기
+        +)
+        5의 배수라고 무조건 5가 1번만 곱해진 것은 아님
+        25의 배수,125의 배수는 5가 2,3번 곱해짐 (0<=n<=500)
          */
-        bw.write(n/5 + "\n");
+        bw.write((n/5 + n/25 + n/125) + "\n");
         bw.flush();
         bw.close();
         br.close();
