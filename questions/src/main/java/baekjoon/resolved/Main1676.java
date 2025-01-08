e092578b5e4f7eb82be85a579eb732b670b3e854
hmkim <elppapie@naver.com>
2025-01-08 12:52:17 +0900

    :zap: 1676번 수정 시도 (목적 달성 실패)
    단순 숫자 연산값인데 (반복문 없이) 시간이랑 메모리가 너무 많이 소요되는 것 같아서 BufferedReader 와 BufferedWriter 를 Scanner, Sout 으로 바꿔봤는데 소용없었음 (오히려 폭발적으로 늘어남)

--- questions/src/main/java/baekjoon/resolved/Main1676.java
+++ questions/src/main/java/baekjoon/resolved/Main1676.java
@@ -5,9 +5,8 @@ import java.util.*;
 
 public class Main1676 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         /*
         0을 만드는 것은 5*2 -> 즉, 5의 배수의 등장횟수 구하기
         1,2,3...n까지 5의 배수 개수 구하기
@@ -15,9 +14,6 @@ public class Main1676 {
         5의 배수라고 무조건 5가 1번만 곱해진 것은 아님
         25의 배수,125의 배수는 5가 2,3번 곱해짐 (0<=n<=500)
          */
-        bw.write((n/5 + n/25 + n/125) + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(n/5 + n/25 + n/125);
     }
 }
