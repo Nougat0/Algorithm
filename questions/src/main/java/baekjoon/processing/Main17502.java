80389bf0ad82889b00d094d992a3d0f76b0f30b5
hmkim <elppapie@naver.com>
2025-01-16 16:10:03 +0900

    :arrow_right: 👟 17502번 풀어놓음 (미제출)
    팰린드롬 문자열 만들기 (영단어가 아니어도 됨)
    홀수일 경우 한가운데 문자 채워넣는 게 중복 채워넣기 동작이 되는 문제 빼고는 괜찮을 듯...?
    (굳이 if문 하나 더 추가하고 싶지 않음)

---
+++ questions/src/main/java/baekjoon/processing/Main17502.java
@@ -0,0 +1,34 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+
+public class Main17502 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int length = Integer.parseInt(br.readLine());
+        char[] str = br.readLine().toCharArray();
+        char letter = 'a'; //채워넣을 때 사용할 임의의 알파벳 소문자
+        for(int i=0; i<length; i++) {
+            //임의의 알파벳 소문자 초기화
+            if(letter > 'z') letter = 'a';
+            //빈자리 채워넣기
+            if(str[i] == '?') {
+                if(str[length - 1 - i] == '?') {
+                    str[i] = letter;
+                    str[length - 1 - i] = letter++;
+                } else {
+                    str[i] = str[length - 1 -i];
+                }
+            }
+            //출력
+            sb.append(str[i]);
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
