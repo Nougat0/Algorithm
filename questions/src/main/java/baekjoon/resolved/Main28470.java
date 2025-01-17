fe4cc79680fbf325b9ff46d192f7807bf56841b6
hmkim <elppapie@naver.com>
2025-01-17 17:43:33 +0900

    👍 👟 28470번 해결 ...
    알고보니 출력하는 아드레날린 변수범위가 너무 작아서 틀림 (int 범위로 자동 형변환 돼서)
    int -> long 으로 변경하고 해결함
    계속 부동소수점 때문인 줄 알고 이것저것 코드 고쳤는데 진작에 범위 체크 해볼걸 ㅠㅠ

--- questions/src/main/java/baekjoon/resolved/Main28470.java
+++ questions/src/main/java/baekjoon/resolved/Main28470.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
@@ -27,14 +27,14 @@ public class Main28470 {
             }
         }
         //연산
-        int adrenaline = 0; //아드레날린은 항상 정수... 증/감량은 바닥 함수 사용하기 --> ⌊3.7⌋ --> 3
+        long adrenaline = 0; //아드레날린은 항상 정수... 증/감량은 바닥 함수 사용하기 --> ⌊3.7⌋ --> 3
         for(int i=0; i<repeat; i++) {
             //각 동작 시 순서 정하기
             //먼저 하는 동작이 정해진 값에서 RATE만큼 변함
             //증가량 vs 감소량 비교해서 아드레날린 보존가능한 방향으로 정하기
-            double hitFirst = Math.floor(input[INCREASE][i]*input[RATE][i]/10.0) - input[DECREASE][i];
-            double duckFirst = input[INCREASE][i] - Math.floor(input[DECREASE][i]*input[RATE][i]/10.0);
-            adrenaline += (int) Math.max(hitFirst, duckFirst);
+            int hitFirst =  input[INCREASE][i]*input[RATE][i]/10 - input[DECREASE][i];
+            int duckFirst = input[INCREASE][i] - input[DECREASE][i]*input[RATE][i]/10;
+            adrenaline += Math.max(hitFirst, duckFirst);
         }
         bw.write(adrenaline + "\n");
         bw.flush();
