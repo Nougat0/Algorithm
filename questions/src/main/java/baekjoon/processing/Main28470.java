2602d95e4b2d095e696fa7bf2720c893d48d506e
hmkim <elppapie@naver.com>
2025-01-17 11:34:05 +0900

    :arrow_right: 👟 28470번 해결중.........
    바닥 함수 사용하라는 곳을 고려해서 연산값 말고 증/감 변화량에 Math.floor 를 씌워서 계산했음
    기존에는 채점 시작도 전에 틀렸으나 이번에는 1%까지는 갔고 또 틀림
    왜... 틀렸을까...

--- questions/src/main/java/baekjoon/processing/Main28470.java
+++ questions/src/main/java/baekjoon/processing/Main28470.java
@@ -21,13 +21,13 @@ public class Main28470 {
             }
         }
         //연산
-        int adrenaline = 0; //아드레날린은 항상 정수... 바닥 함수 사용하기 --> ⌊3.7⌋ --> 3
+        int adrenaline = 0; //아드레날린은 항상 정수... 증/감량은 바닥 함수 사용하기 --> ⌊3.7⌋ --> 3
         for(int i=0; i<repeat; i++) {
             //각 동작 시 순서 정하기
             //먼저 하는 동작이 정해진 값에서 RATE만큼 변함
             //증가량 vs 감소량 비교해서 아드레날린 보존가능한 방향으로 정하기
-            double hitFirst = Math.floor(input[INCREASE][i]*input[RATE][i] - input[DECREASE][i]);
-            double duckFirst = Math.floor(input[INCREASE][i] - input[DECREASE][i]*input[RATE][i]);
+            double hitFirst = Math.floor(input[INCREASE][i]*input[RATE][i]) - input[DECREASE][i];
+            double duckFirst = input[INCREASE][i] - Math.floor(input[DECREASE][i]*input[RATE][i]);
             adrenaline += Math.max(hitFirst, duckFirst);
         }
         bw.write(adrenaline + "\n");
