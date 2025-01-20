53050d5ae5f0e1eaa1c370242ee21ea71e7d1e3a
hmkim <elppapie@naver.com>
2025-01-21 08:12:15 +0900

    ⚡ 👟 26069번 수정 (Map -> Set)
    굳이 Map으로 불필요한 value를 갖고 있을 필요가 없어서... Set으로 변경함
    여전히 과한 로직이 있는 것 같지만... 이렇게 바꿔서 조금 개선됨

--- questions/src/main/java/baekjoon/resolved/Main26069.java
+++ questions/src/main/java/baekjoon/resolved/Main26069.java
@@ -12,7 +12,7 @@ public class Main26069 {
         String[] meet;
         boolean infectionStart = false; //춤 바이러스 감염 시작여부
         int danceCnt = 1; //최초 춤추는 사람(토끼): 총총이 1명
-        Map<String, Boolean> danceList = new HashMap<>(); //춤추는 사람 목록
+        Set<String> danceList = new HashSet<>(); //춤추는 사람 목록
         int count = Integer.parseInt(br.readLine());
         for(int i=0; i<count; i++) {
             st = new StringTokenizer(br.readLine());
@@ -23,7 +23,7 @@ public class Main26069 {
                 if(infectionStart) {
                     //총총이를 만났으므로 춤추는 목록에 추가
                     for(int j=0; j<2; j++) {
-                        danceList.put(meet[j], true);
+                        danceList.add(meet[j]);
                     }
                     danceCnt++;
                 }
@@ -31,14 +31,14 @@ public class Main26069 {
                 int dancing = 0;
                 //앞사람이나 뒷사람이 춤을 추고 있는지 확인
                 for(int j=0; j<2; j++) {
-                    if(danceList.containsKey(meet[j])) {
+                    if(danceList.contains(meet[j])) {
                         dancing++;
                     }
                 }
                 //한 명만 춤추고 있다면 둘 다 춤추게 만들기
                 if(dancing == 1) {
                     for(int j=0; j<2; j++) {
-                        danceList.put(meet[j], true);
+                        danceList.add(meet[j]);
                     }
                     danceCnt++;
                 }
