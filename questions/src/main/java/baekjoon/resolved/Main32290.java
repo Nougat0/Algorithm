9ad38d3582b13275c59014c430f951f554c52d65
hmkim <elppapie@naver.com>
2025-01-22 00:31:27 +0900

    👍 👟 32290번 해결
    Array length 최대값 고려 안하고 임의로 정했다가 0 1000 1000 에서 IndexOutOfBounds 오류가 뜸... 다시 이진법으로 확인해보고 MAX LENGTH =1024로 지정함
    
    문제가 거진 0이 정답이라 조금 싱거움 ... 반례를 못 찾아서 그런 것일지도 모르겠지만

--- questions/src/main/java/baekjoon/resolved/Main32290.java
+++ questions/src/main/java/baekjoon/resolved/Main32290.java
@@ -1,10 +1,16 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
-import javax.swing.*;
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main32290 {
+
+    /*
+    비트OR 연산하는 l, r 모두 1000 이하의 숫자
+    1000은 이진법으로 1111101000 (10자리) 이며, 10자리 이진수의 최대값은 1023
+    따라서 값 체크할 배열의 최대길이는 1024로 지정하면 된다 (0~1023)
+     */
+    public static final int MAXLENGTH = 1024;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -33,13 +39,16 @@ public class Main32290 {
      */
     public static int mex(int[] array) {
         int length = array.length;
-        boolean[] unavailable = new boolean[1001];
+        boolean[] unavailable = new boolean[MAXLENGTH];
+        //등장한 숫자들 기록
         for(int i=0; i<length; i++) {
             unavailable[array[i]] = true;
         }
-        for(int i=0; i<=1001; i++) {
+        //등장하지 않은 숫자 0부터 확인, 확인될 시 바로 반환
+        for(int i=0; i<MAXLENGTH; i++) {
             if(!unavailable[i]) return i;
         }
-        return 1001;
+        //전부 등장했으면 1024가 최소값
+        return MAXLENGTH;
     }
 }
