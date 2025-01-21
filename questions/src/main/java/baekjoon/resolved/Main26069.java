284f1f6aa82ed2018921e7f2ba11624f530dedba
hmkim <elppapie@naver.com>
2025-01-21 09:01:06 +0900

    :zap: 👟 26069번 수정 (총총이 만나기 전/후 로직 통합...)
    근데 결과적으로 시간/메모리 사용 모두 늘어났음
    비추천 방식...

--- questions/src/main/java/baekjoon/resolved/Main26069.java
+++ questions/src/main/java/baekjoon/resolved/Main26069.java
@@ -4,38 +4,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main26069 {
-    public static final String Chong = "ChongChong";
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
         String[] meet;
-        boolean infectionStart = false; //춤 바이러스 감염 시작여부
+        //초기화
         int danceCnt = 1; //최초 춤추는 사람(토끼): 총총이 1명
         Set<String> danceList = new HashSet<>(); //춤추는 사람 목록
+        danceList.add("ChongChong");
+        //입력
         int count = Integer.parseInt(br.readLine());
         for(int i=0; i<count; i++) {
             st = new StringTokenizer(br.readLine());
             meet = new String[] {st.nextToken(), st.nextToken()};
-            //총총이를 만났는지 여부 확인
-            if(!infectionStart) {
-                infectionStart = (meet[0].charAt(0) == 'C' && meet[0].equals(Chong) || meet[1].charAt(0) == 'C' && meet[1].equals(Chong));
-                if(infectionStart) {
-                    //총총이를 만났으므로 춤추는 목록에 추가
-                    danceList.addAll(List.of(meet));
-                    danceCnt++;
-                }
-            } else {
-                boolean dancing1 = danceList.contains(meet[0]);
-                boolean dancing2 = danceList.contains(meet[1]);
-                //앞사람이나 뒷사람이 춤을 추고 있는지 확인
-                if(dancing1 ^ dancing2) {
-                    //한 명만 춤추고 있다면 둘 다 춤추게 만들기
-                    danceList.addAll(List.of(meet));
-                    danceCnt++;
-                }
+            boolean dancing1 = danceList.contains(meet[0]);
+            boolean dancing2 = danceList.contains(meet[1]);
+            //앞사람이나 뒷사람이 춤을 추고 있는지 확인
+            if(dancing1 ^ dancing2) {
+                //한 명만 춤추고 있다면 둘 다 춤추게 만들기
+                danceList.addAll(List.of(meet));
+                danceCnt++;
             }
         }
+        //출력
         bw.write(danceCnt + "\n");
         bw.flush();
         bw.close();
