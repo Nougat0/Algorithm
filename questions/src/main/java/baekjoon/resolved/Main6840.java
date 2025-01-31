0f96aac6c82afbd2db147bb0b653801d93817c79
hmkim <elppapie@naver.com>
2025-01-31 22:21:40 +0900

    👍 :athletic_shoe: 6840번 해결
    단순 정렬로 하려다가 3개뿐이라... 값 밀어넣는 식으로 구현해봄
    다시보니 굳이 for문 2개가 아니라 입력받으면서 바로 진행해도 될 듯?

---
+++ questions/src/main/java/baekjoon/resolved/Main6840.java
@@ -0,0 +1,42 @@
+package baekjoon.resolved;
+
+import java.io.*;
+import java.util.*;
+
+public class Main6840 {
+    /*
+    엄마곰의 그릇(중앙값) 찾기
+     */
+    public static final int BOWLS = 3;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        //입력
+        int[] bowls = new int[BOWLS];
+        for(int i=0; i<BOWLS; i++) {
+            bowls[i] = Integer.parseInt(br.readLine());
+        }
+        //단순 정렬보다 재밌어 보여서 해봤는데... 실효성은 없는 듯?
+        int biggest = 0, smallest = 0, middle = 0;
+        for(int i=0; i<BOWLS; i++) {
+            if(biggest == 0 && smallest == 0) {
+                //초기값 지정
+                biggest = bowls[i];
+                middle = bowls[i];
+                smallest = bowls[i];
+            } else if(bowls[i] > biggest) {
+                if(middle != biggest) middle = biggest; //밀기
+                biggest = bowls[i];
+            } else if(bowls[i] < smallest) {
+                if(middle != smallest) middle = smallest; //밀기
+                smallest = bowls[i];
+            } else {
+                middle = bowls[i];
+            }
+        }
+        bw.write(middle + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
