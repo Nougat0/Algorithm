1002f808de99ad5d725bd97483b3b408c76b2eeb
hmkim <elppapie@naver.com>
2025-02-05 18:47:58 +0900

    :arrow_right: 👟 10156번 풀어놓음 (미제출)

---
+++ questions/src/main/java/baekjoon/processing/Main10156.java
@@ -0,0 +1,25 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main10156 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int price = Integer.parseInt(st.nextToken());
+        int cnt = Integer.parseInt(st.nextToken());
+        int asset = Integer.parseInt(st.nextToken());
+        //과자 사먹기 위해 받아야 하는 금액
+        int loan = price*cnt - asset;
+        if(loan > 0) {
+            bw.write(loan + "\n");
+        } else {
+            bw.write(0 + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
