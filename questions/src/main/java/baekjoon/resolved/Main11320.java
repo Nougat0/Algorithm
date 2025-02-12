15a700387f9d93cc3639e08118716e4d87f54cb3
hmkim <elppapie@naver.com>
2025-02-12 19:03:18 +0900

    👍 👟 11320번 해결 ㅋㅋ;
    아니 이게 뭐라고 3번이나 틀리니...
    1~n 까지의 합도 틀렸지만, 결과적으로는 삼항연산자에 괄호 안 씌우고 써서 틀림
    결론: 삼항연산자를 if문으로 구현했으면 안 틀렸다

--- questions/src/main/java/baekjoon/resolved/Main11320.java
+++ questions/src/main/java/baekjoon/resolved/Main11320.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
@@ -15,8 +15,10 @@ public class Main11320 {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int lines = a/b;
+            //0 ~ (lines-1) 까지니 lines-1 에 대한 값 구하기
+            int n = lines-1;
             //각 줄마다 삼각형의 개수는 1+(line-1)*2 개,,, 1~N까지의 합 구하는 공식 사용
-            int count = lines + (lines == 1 ? 0 : ((lines)*(lines-1)/2 + lines%2 == 0 ? 0 : 1) * 2);
+            int count = lines + (lines == 1 ? 0 : ((n+1)*(n/2) + (n%2 == 0 ? 0 : (n+1)/2)) * 2);
             sb.append(count).append("\n");
         }
         bw.write(sb.toString());
