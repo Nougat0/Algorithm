330c02bf9d51bd69c457738c4718e0c526c4a494
hmkim <elppapie@naver.com>
2025-02-16 23:05:25 +0900

    :zap: 16524번 List -> Set 수정해보았으나 큰 변화 없음
    시간은 다소 줄었으나 오히려 메모리는 늘어남

--- questions/src/main/java/baekjoon/resolved/Main16524.java
+++ questions/src/main/java/baekjoon/resolved/Main16524.java
@@ -8,15 +8,13 @@ public class Main16524 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-        List<String> mailList = new ArrayList<>();
+        Set<String> mailList = new HashSet<>();
         int mailCnt = Integer.parseInt(br.readLine());
         for(int m=0; m<mailCnt; m++) {
             st = new StringTokenizer(br.readLine(), "@");
             //정규식으로 @ 앞의 문자에 있는 . 과 +(뒤에 따라오는 문자 포함) 을 제거하고 비교
             String uniqueUser = st.nextToken().replaceAll("\\.|\\+.*", "") + "@" + st.nextToken();
-            if(mailList.indexOf(uniqueUser) == -1) {
-                mailList.add(uniqueUser);
-            }
+            mailList.add(uniqueUser);
         }
         bw.write(mailList.size() + "\n");
         bw.flush();
