a5c65142c2eaa48802589b65f075485bf8a9c22e
hmkim <elppapie@naver.com>
2025-02-17 12:42:03 +0900

    :arrow_right: :two: 1920번 해결중
    68%까지 채점되다가 틀렸다고 뜬다 (시간 초과도 아니고)

---
+++ questions/src/main/java/baekjoon/processing/Main1920.java
@@ -0,0 +1,31 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+
+public class Main1920 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        HashSet<Integer> nSet = new HashSet<>();
+        //기준대상 숫자들 목록
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+            nSet.add(Integer.parseInt(st.nextToken()));
+        }
+        //비교대상 숫자들 목록 - 존재여부 확인
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+            boolean inList = nSet.contains(Integer.parseInt(st.nextToken()));
+            sb.append(inList ? 1 : 0).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
