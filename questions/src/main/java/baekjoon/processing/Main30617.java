26d30056210c39c845c16a3f27c32915ca2177da
hmkim <elppapie@naver.com>
2025-02-19 23:42:30 +0900

    :arrow_right: :athletic_shoe: 30617번 풀어놓음 (미제출)
    처음에는 int[length][2] 를 사용했는데... 바로 전 노드만 알고 있으면 되고, 호출시간이 오히려 많이 걸리지 않을까 싶어서 일반 int 변수로 대체함...... 과연....

---
+++ questions/src/main/java/baekjoon/processing/Main30617.java
@@ -0,0 +1,37 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+
+public class Main30617 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int length = Integer.parseInt(br.readLine());
+        int currL, currR, lastL=0, lastR=0;
+
+        int fun = 0; //재미
+        for(int t=0; t<length; t++) {
+            st = new StringTokenizer(br.readLine());
+            currL = Integer.parseInt(st.nextToken());
+            currR = Integer.parseInt(st.nextToken());
+            boolean moveL = (currL != 0);
+            boolean moveR = (currR != 0);
+            if(t > 0) { //[1] 이전과 같은 방향 노드
+                if(moveL && lastL == currL) fun++;
+                if(moveR && lastR == currR) fun++;
+            }
+            //[2] 양쪽 노드 같은 방향
+            if(moveL && moveR && currL == currR) fun++;
+            //현재 노드를 이전 노드 값으로 저장
+            //int[length][2] 2중배열 대신 일반 int 변수 사용함
+            lastL = currL;
+            lastR = currR;
+        }
+        bw.write(fun + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
