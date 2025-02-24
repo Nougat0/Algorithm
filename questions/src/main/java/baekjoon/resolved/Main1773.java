249f9ccbac6dacb50cb01c284802320a0c496676
hmkim <elppapie@naver.com>
2025-02-24 20:56:39 +0900

    :zap: 1773번 시간 단축 시도
    다른 분 소스 코드 참고하여 시간 단축 시도
    내가 기존에 제출한 코드는 full scan 을 하는 방식이었고... (매우 비효율적)
    제아무리 브루트 포스 문제더라도 굳이 full scan을 하지 않아도 되는 방식이 존재함!
    일단 inner - outer loop 순서를 바꾸고 나서부터 그게 보이는 듯
    
    마지막 주석 질문의 답은
    후자가 시간이 덜 걸린다 (현재 코드보다 10ms 정도 시간이 덜 걸림)
    
    참고한 소스코드들 링크
    https://www.acmicpc.net/source/87155731
    https://www.acmicpc.net/source/86454674

--- questions/src/main/java/baekjoon/resolved/Main1773.java
+++ questions/src/main/java/baekjoon/resolved/Main1773.java
@@ -11,29 +11,40 @@ public class Main1773 {
 
         근데 굳이 for문을 안 돌고 최소공배수 의 배수들을 빼면 되는 게 아닌가?
         → N의 크기가 커질수록 찾아야 하는 최소공배수도 중첩되어 많아짐...
+
+        ----- 제출 후 비교 --------------------------------------------------------------
+        + 내 방식 : studentCnt * endTime 번 loop (전체 탐색ㅎ...)
+        + 다른 분 방식 (1) : studentCnt * 학생의 rate 배수 초 번 loop
+        + 다른 분 방식 (2) : studentCnt * 학생의 rate 배수 초 번 loop (이미 등장한 수는 제외)
+        * 참고한 백준 소스 :  (1) 87155731	dmsgml9903
+                            (2) 86454674	hy8923
     */
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int studentCnt = Integer.parseInt(st.nextToken());
         int endTime = Integer.parseInt(st.nextToken());
-        int[] list = new int[studentCnt];
-        for(int i=0; i<studentCnt; i++) {
-            list[i] = Integer.parseInt(br.readLine());
-        }
-        //폭죽 터뜨리기
         int fireworksShown = 0;
-        for(int t=1; t<=endTime; t++) {
-            //폭죽 터뜨린 사람 있는지 확인
-            //있을 시, 체크 후 해당 시간 확인 중단
-            for(int i=0; i<studentCnt; i++) {
-                if(t % list[i] == 0) {
-                    fireworksShown++;
-                    break;
+        boolean[] time = new boolean[endTime+1];
+        for(int i=0; i<studentCnt; i++) {
+            int rate = Integer.parseInt(br.readLine());
+            if(!time[rate]) { //첫번째 배수에서 이미 폭죽 터진 경우 제외
+                int t = rate;
+                while(t <= endTime) {
+                    if(!time[t]) {
+                        time[t] = true;
+                        fireworksShown++; //새로운 표시마다 즉석 카운트
+                    }
+                    t += rate;
                 }
             }
         }
+        /*
+            매 loop 에 추가되는 if문에 걸리는 시간 
+            VS
+            마지막 최종 endTime 횟수만큼의 추가적인 loop 1회에 걸리는 시간
+        */
         bw.write(fireworksShown + "\n");
         bw.flush();
         bw.close();
