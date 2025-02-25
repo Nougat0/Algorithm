9ced189e8daedf53559bd1f5c7e373854b7eaa94
hmkim <elppapie@naver.com>
2025-02-25 18:43:24 +0900

    👍 👟 10914번 해결
    역시나 범위 때문에 틀린 것... 범위 내로 맞춰서 제출해서 맞았음

--- questions/src/main/java/baekjoon/resolved/Main10914.java
+++ questions/src/main/java/baekjoon/resolved/Main10914.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
@@ -20,13 +20,15 @@ public class Main10914 {
             //2개 글자 단위로 확인, 여분 버리기
             while(index+1 < length) { 
                 // 'a' -> 0 으로 환산하고, 최종 결과(x)는 'a' 다시 추가
-                char letter = (char) ((word[index] + word[index+1] - n - 'a'*2) % 26 + 'a');
+                // n을 빼기 때문에 최종 x 값이 영하가 될 수도 있음 = 'a'-'z' 범위 벗어남
+                // -> n의 최대값 이상 + 결과값에 영향을 안 미치는 26을 더함
+                char letter = (char) ((word[index] + word[index+1] - n + 26 - 'a'*2) % 26 + 'a');
                 sb.append(letter);
                 index += 2;
             }
             sb.append(' ');
         }
-        bw.write(sb.toString());
+        bw.write(sb.toString().trim());
         bw.flush();
         bw.close();
         br.close();
