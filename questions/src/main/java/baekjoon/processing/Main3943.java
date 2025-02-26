82a72c77f1d1e224e0812bd684640717f5e65333
hmkim <elppapie@naver.com>
2025-02-26 13:05:54 +0900

    :arrow_right: :athletic_shoe: 3943번 풀어놓음 (미제출)

---
+++ questions/src/main/java/baekjoon/processing/Main3943.java
@@ -0,0 +1,44 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+
+public class Main3943 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int testCase = Integer.parseInt(br.readLine());
+        for(int i=0; i<testCase; i++) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(getBiggestNum(n)).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    /**
+     * 헤일스톤 수열 진행,
+     * 수열 중 가장 큰 수 반환
+     * @param n 헤일스톤 수열의 시작값
+     * @return max
+     */
+    public static int getBiggestNum(int n) {
+        int max = n;
+        //hailstone sequence
+        while(n != 1) {
+            if(n % 2 == 0) {
+                n /= 2;
+            } else {
+                n *= 3;
+                n++;
+            }
+            if(n > max) {
+                max = n;
+            }
+        }
+        return max;
+    }
+}
