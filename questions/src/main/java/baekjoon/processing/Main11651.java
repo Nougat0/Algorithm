5b631fbe0d7b9f6e1627f25b49c0b2f908ab5a70
hmkim <elppapie@naver.com>
2025-03-01 18:45:37 +0900

    :arrow_right: :two: 11651번 풀어놓음 (미제출)
    11650 번과 유사한 문제인데 x와 y만 바뀜
    따라서 dots 이중배열에 x와 y만 바꿔서 넣어줘도 맞았을 테지만...
    굳이 x와 y 좌표 넣는 순서를 꼬기 싫어서 Comparator 를 수정함,,,

---
+++ questions/src/main/java/baekjoon/processing/Main11651.java
@@ -0,0 +1,43 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main11651 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int dotCnt = Integer.parseInt(br.readLine());
+        int[][] dots = new int[dotCnt][2];
+        for(int i=0; i<dotCnt; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            dots[i][0] = x;
+            dots[i][1] = y;
+        }
+        //정렬 (Comparator 커스텀 정의)
+        Collections.sort(Arrays.asList(dots), (o1, o2) -> {
+            //return o1-o2 오름차순 (양수: o2부터, 음수: o1부터)
+            if(o1[1] > o2[1]) { //o1가 y좌표가 더 큰 상황
+                return 1;
+            } else if(o1[1] == o2[1] && o1[0] > o2[0]) { //y는 동일하고 o1이 x만 더 큰 상황
+                return 1;
+            } else {
+                return -1;
+            }
+        });
+        //출력
+        for(int i=0; i<dotCnt; i++) {
+            sb.append(dots[i][0]).append(' ').append(dots[i][1]).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
