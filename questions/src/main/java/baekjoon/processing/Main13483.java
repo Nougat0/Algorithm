1cee819d404dc5b9f4846616c1907a87ee9adcec
hmkim <elppapie@naver.com>
2025-03-06 23:17:34 +0900

    :arrow_right: 👟 13483번 풀어놓음
    문제 접근 시 아래 2가지 방식 사이에서 고민했음 (현재 커밋은 2)임)
    
    1) 생일 오름차순으로 아들들 정렬 후 하나씩 확인
    - 맏이가 만 18살이 안될경우 -1 출력
    - 막내가 만 18살이 될 경우 막내 출력
    - 맏이가 만 18살 이상, 막내가 만 18살 미만일 경우 막내부터 거슬러 올라가며 만 18이상인 최초의 아들 출력
    - 따로 아들 클래스 구현하여 메소드, 아들 번호 변수 사용
    - 정렬 시 Collections.sort(람다함수) 방식 사용 예정
    
    2) 입력받은 순서대로 적격자를 가리는 로직 실행
    - 전체 탐색하여 탐색 종료 후 남은 적격자의 번호 출력
    - 매 순회마다 만 18살 이상 여부 + 가장 어린 적격자보다 어린지 여부 확인함
    
    1)을 하고 싶었으나... 나이순서대로 정렬하면서 비교할 테고, 그 후에 또 비교하는 거라서 과연 더 빠를 수 있을까? 싶었다 (전체탐색 후 추가탐색처럼 느껴졌음) 따라서 현재 구현한 방식은 2) 전체탐색 1회 진행하면서 체크하기...
    
    다만 예제를 풀었을 때 생각한 만큼 빠르지는 못하다...
    속도가 다소 느린 것 같은데... 현재 2) 방식으로 먼저 제출해보고 1) 방식도 구현해서 제출해볼 것.

---
+++ questions/src/main/java/baekjoon/processing/Main13483.java
@@ -0,0 +1,65 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+
+public class Main13483 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st;
+        //사망일 입력 - 연월일 입력
+        st = new StringTokenizer(br.readLine());
+        int[] dateOfDeath = new int[3];
+        for(int i=2; i>=0; i--) {
+            dateOfDeath[i] = Integer.parseInt(st.nextToken());
+        }
+        dateOfDeath[0] -= 18; //만 18살이 될 수 있는 기준
+        //아들들 생일 입력 - 연월일 입력
+        int sons = Integer.parseInt(br.readLine());
+        int[][] dateOfBirth = new int[sons][3];
+        for(int s=0; s<sons; s++) {
+            st = new StringTokenizer(br.readLine());
+            for(int i=2; i>=0; i--) {
+                dateOfBirth[s][i] = Integer.parseInt(st.nextToken());
+            }
+        }
+        //적격자 선별
+        int sonNumber = -1; //적격자 번호
+        int[] birthday = new int[] {1,1,1}; //적격자의 생일, 가장 작은(오래된) 날짜로 초기화
+        for(int s=0; s<sons; s++) {
+            if(olderOrEqualThan(dateOfBirth[s], dateOfDeath) && olderOrEqualThan(birthday, dateOfBirth[s])) {
+                //만 18살 이상 && 현재 가장 어린 적격자보다 어린지 확인
+                sonNumber = s+1;
+                birthday = dateOfBirth[s];
+            }
+        }
+        bw.write(sonNumber + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    /**
+     *
+     * @param date1
+     * @param date2
+     * @return date1 날짜가 date2보다 이르거나 같을 경우 true, 늦을의 경우 false
+     */
+    public static boolean olderOrEqualThan(int[] date1, int[] date2) {
+        boolean result = false;
+        if(date1[0] < date2[0]) {
+            //[1] 연도가 앞설 경우
+            result = true;
+        } else if(date1[0] == date2[0]) {
+            if(date1[1] < date2[1]) {
+                //[2] 월이 앞설 경우
+                result = true;
+            } else if(date1[1] == date2[1] && date1[2] <= date2[2]) {
+                //[3] 일이 앞서거나 같은 날짜일 경우
+                result = true;
+            }
+        }
+        return result;
+    }
+}
