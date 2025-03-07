3be2345fb1fc3578ad672f8f798046efb14eb83d
hmkim <elppapie@naver.com>
2025-03-07 19:09:53 +0900

    :zap: 13483번 객체범벅 방식으로 구현
    의외로 첫 번째 방식이 더 빠르고 메모리도 절약됨
    이번 방식이 객체를 많이 활용해서 메모리 소모가 더 큰 것은 납득함
    하지만 시간상 우위에 있지 않은 게 의외... 정렬을 활용한 더 좋은 구현 방식이 있는 걸 수도

--- questions/src/main/java/baekjoon/resolved/Main13483.java
+++ questions/src/main/java/baekjoon/resolved/Main13483.java
@@ -8,58 +8,95 @@ public class Main13483 {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws IOException {
         StringTokenizer st;
+        /** 입력 **/
         //사망일 입력 - 연월일 입력
         st = new StringTokenizer(br.readLine());
-        int[] dateOfDeath = new int[3];
-        for(int i=2; i>=0; i--) {
-            dateOfDeath[i] = Integer.parseInt(st.nextToken());
-        }
-        dateOfDeath[0] -= 18; //만 18살이 될 수 있는 기준
+        int qualifiedDay = Integer.parseInt(st.nextToken());
+        int qualifiedMonth = Integer.parseInt(st.nextToken());
+        int qualifiedYear = Integer.parseInt(st.nextToken()) - 18; //만 18살이 될 수 있는 기준
+        Birthday qualified = new Birthday(0, qualifiedYear, qualifiedMonth, qualifiedDay);
         //아들들 생일 입력 - 연월일 입력
         int sons = Integer.parseInt(br.readLine());
-        int[][] dateOfBirth = new int[sons][3];
+        List<Birthday> sonList = new ArrayList<>();
         for(int s=0; s<sons; s++) {
             st = new StringTokenizer(br.readLine());
-            for(int i=2; i>=0; i--) {
-                dateOfBirth[s][i] = Integer.parseInt(st.nextToken());
-            }
+            int day = Integer.parseInt(st.nextToken());
+            int month = Integer.parseInt(st.nextToken());
+            int year = Integer.parseInt(st.nextToken());
+            sonList.add(new Birthday(s+1, year, month, day));
         }
-        //적격자 선별
-        int sonNumber = -1; //적격자 번호
-        int[] birthday = new int[] {1,1,1}; //적격자의 생일, 가장 작은(오래된) 날짜로 초기화
-        for(int s=0; s<sons; s++) {
-            if(olderOrEqualThan(dateOfBirth[s], dateOfDeath) && olderOrEqualThan(birthday, dateOfBirth[s])) {
-                //만 18살 이상 && 현재 가장 어린 적격자보다 어린지 확인
-                sonNumber = s+1;
-                birthday = dateOfBirth[s];
-            }
-        }
+        /** 연산 **/
+        SucceedJudgement judgement = new SucceedJudgement(sons, sonList, qualified);
+        int sonNumber = judgement.getQualified();
         bw.write(sonNumber + "\n");
         bw.flush();
         bw.close();
         br.close();
     }
 
-    /**
-     *
-     * @param date1
-     * @param date2
-     * @return date1 날짜가 date2보다 이르거나 같을 경우 true, 늦을의 경우 false
-     */
-    public static boolean olderOrEqualThan(int[] date1, int[] date2) {
-        boolean result = false;
-        if(date1[0] < date2[0]) {
-            //[1] 연도가 앞설 경우
-            result = true;
-        } else if(date1[0] == date2[0]) {
-            if(date1[1] < date2[1]) {
-                //[2] 월이 앞설 경우
-                result = true;
-            } else if(date1[1] == date2[1] && date1[2] <= date2[2]) {
-                //[3] 일이 앞서거나 같은 날짜일 경우
-                result = true;
-            }
+    public static class SucceedJudgement {
+        int sons;
+        List<Birthday> sonList;
+        Birthday qualifiedDate;
+
+        public SucceedJudgement(int sons, List<Birthday> sonList, Birthday qualifiedDate) {
+            this.sons = sons;
+            this.sonList = sonList;
+            this.qualifiedDate = qualifiedDate;
         }
-        return result;
+
+        public int birthdayComparator(Birthday o1, Birthday o2) {
+            //return o1-o2 오름차순 (양수: o2부터, 음수: o1부터)
+            if(o1.year > o2.year) return -1;
+            else if(o1.year == o2.year)
+                if(o1.month > o2.month) return -1;
+                else if(o1.month == o2.month)
+                    if(o1.day > o2.day) return -1;
+                    else if(o1.day == o2.day) return 0;
+            return 1;
+        }
+
+        public int getQualified() {
+            //나이순 오름차순 정렬
+            Collections.sort(sonList, (o1, o2) -> birthdayComparator(o1, o2));
+            //적격자 선별
+            int sonNumber = -1; //적격자 번호 초기화
+            Birthday oldest = sonList.get(sons-1);
+            Birthday youngest = sonList.get(0);
+            boolean oldestQualified = birthdayComparator(oldest, qualifiedDate) >= 0;
+            boolean youngestQualified = birthdayComparator(youngest, qualifiedDate) >= 0;
+            //[1] 막내가 적격자일 경우 막내 선택
+            if(youngestQualified) {
+                sonNumber = youngest.number;
+            }
+            //[2] 막내가 부적격자, 맏이가 적격자일 경우
+            //    가장 어린 적격자 찾기
+            else if(oldestQualified) { //!youngestQualified
+                for(int i=1; i<sons; i++) {
+                    Birthday son = sonList.get(i);
+                    if(birthdayComparator(son, qualifiedDate) >= 0) {
+                        sonNumber = sonList.get(i).number;
+                        break;
+                    }
+                }
+            }
+            //[3] 맏이가 부적격자일 경우 -1
+            return sonNumber;
+        }
+    }
+
+    public static class Birthday {
+        int number;
+        //생년월일
+        int year;
+        int month;
+        int day;
+
+        public Birthday(int number, int year, int month, int day) {
+            this.number = number;
+            this.year = year;
+            this.month = month;
+            this.day = day;
+        }
     }
 }
