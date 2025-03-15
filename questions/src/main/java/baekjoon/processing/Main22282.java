92bbf6cf744b688213922c6fd8a944a8766f4e6e
hmkim <elppapie@naver.com>
2025-03-15 16:12:39 +0900

    :arrow_right: :athletic_shoe: 22282번 해결중 (시간초과)
    이전 제출에서 틀린 이유:
    - citation 입력된 값 중에서만 답을 찾으려 했음... 입력되지 않은 citation이 답이 될 수 있다!
    - 예를 들어 [7,6,5,1] 이렇게 입력되었다면 h_index 는 3이 된다
    
    하지만 이번에는 시간 초과가 떴다...! (환장)

--- questions/src/main/java/baekjoon/processing/Main22282.java
+++ questions/src/main/java/baekjoon/processing/Main22282.java
@@ -13,27 +13,31 @@ public class Main22282 {
 
     * 큰 값은 모두 누적되어 카운트되어야 한다
     * 따라서 큰 citation부터 내려가면서 누적합으로 연산하기
+    * (주의) h_index 는 꼭 입력된 citation 중에서 나와야 하는 게 아니다...!
     */
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
         int h_index = 0; //기본 h_index
+        int maxCitation = 0; //입력된 citation 최대값
         Map<Integer, Integer> citations = new TreeMap<>(Collections.reverseOrder()); //key(=citation 값) 내림차순 자동정렬
         //입력
         while(n-- > 0) {
             int c = Integer.parseInt(br.readLine());
             citations.compute(c, (key, value) -> value == null ? 1 : (value+1));
+            maxCitation = Math.max(maxCitation, c);
         }
         //h_index 연산
         long sum = 0; //누적합 첫 값으로 초기화
         int count; //citation 몇 개 있는지
-        for(Integer c : citations.keySet()) {
-            count = citations.get(c);
-            if((sum += count) / c >= 1) {
-                h_index = c; //가능한 citation 값, 내림차순 탐색이므로 최대값임
+        while(maxCitation > 0) { //maxCitation 변수값을 그대로 citation key값으로 사용
+            count = citations.getOrDefault(maxCitation, 0); //해당 citation 없을 시 0
+            if((sum += count) / maxCitation >= 1) {
+                h_index = maxCitation;
                 break;
             }
+            maxCitation--; //다음 확인할 citation값
         }
         bw.write(h_index + "\n");
         bw.flush();
