f9c6282f5e16e6602e72cde805cfdf36a43cb469
hmkim <elppapie@naver.com>
2025-03-19 08:58:03 +0900

    ⚡ 👟 2747번 재시도 (DP-Top-Down)
    Top-Down 방식으로 바꿔보았으나 Bottom-Up과 거의 유사한 채점결과가 나옴 (둘다 120ms)
    메모리는 Top-Down 방식에서 덜 소모됨 (배열 길이를 입력받은 값 기준+1 로 채워놓는 최적화가 가능해서 그런 듯)

--- questions/src/main/java/baekjoon/resolved/Main2747.java
+++ questions/src/main/java/baekjoon/resolved/Main2747.java
@@ -7,9 +7,8 @@ public class Main2747 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws IOException {
-        final int MAX_FIBO = 45;
         int n = Integer.parseInt(br.readLine());
-        Fibo fibo = new Fibo(MAX_FIBO + 1);
+        Fibo fibo = new Fibo(n + 1);
         bw.write(fibo.getFibo(n) + "\n");
         bw.flush();
         bw.close();
@@ -17,26 +16,23 @@ public class Main2747 {
     }
 
     public static class Fibo {
-        int max;
         int[] array;
-        public Fibo(int max) {
-            this.max = max;
-            array = new int[max];
-            processFibo();
-        }
-
-        public int getFibo(int n) {
-            return array[n];
+        public Fibo(int n) {
+            array = new int[n];
         }
 
         /**
-         * 피보나치 n최대값까지 Bottom-Up 방식 DP 사용
+         * 피보나치 수열 값 구하기 (Top-Down)
+         * @param n
          * @return
          */
-        public void processFibo() {
-            array[1] = 1;
-            for(int i=2; i<max; i++) {
-                array[i] = array[i-1] + array[i-2];
+        public int getFibo(int n) {
+            if(n == 0) return 0;
+            else if(n == 1) return 1;
+            else if(array[n] > 0) return array[n];
+            else {
+                int result = getFibo(n-1) + getFibo(n-2);
+                return (array[n] = result);
             }
         }
     }
