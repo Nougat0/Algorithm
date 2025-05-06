23d46ebf496d20ac2b743110752915c0de7955ef
hmkim <elppapie@naver.com>
2025-05-07 08:17:50 +0900

    👍 👟 23234번 해결

---
+++ questions/src/main/java/baekjoon/resolved/Main23234.java
@@ -0,0 +1,10 @@
+package baekjoon.resolved;
+
+import java.io.*;
+import java.util.*;
+
+public class Main23234 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("The world says hello!");
+    }
+}
