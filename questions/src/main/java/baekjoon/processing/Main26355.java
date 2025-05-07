529159fcb4b6bed0ad584cbd0340614a3ebab4b7
hmkim <elppapie@naver.com>
2025-05-07 22:05:51 +0900

    ➡️ 👟 26355번 풀어놓음 (미제출)
    에라토스테네스의 체 방식 적용
    예제에 있듯이 value의 최대값은 10000이지만 가장 가까운 소수를 찾아야 하므로 10201을 MAX로 잡고 소수 판정 연산을 진행함

---
+++ questions/src/main/java/baekjoon/processing/Main26355.java
@@ -0,0 +1,50 @@
+package baekjoon.processing;
+
+import java.io.*;
+
+public class Main26355 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/26355
+    */
+    public static final int MAX = 101 * 101;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        boolean[] isNotPrime = new boolean[MAX];
+        checkPrime(isNotPrime);
+
+        while(n-- > 0) {
+            int value = Integer.parseInt(br.readLine());
+            sb.append("Input value: ").append(value).append("\n");
+            if(isNotPrime[value]) {
+                int missedUp = 1, missedDown = 1;
+                while(isNotPrime[value + missedUp]) missedUp++;
+                while(isNotPrime[value - missedDown]) missedDown++;
+                sb.append("Missed it by that much (").append(Math.min(missedUp, missedDown)).append(")!");
+            } else {
+                sb.append("Would you believe it; it is a prime!");
+            }
+            if(n > 0) sb.append("\n\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static void checkPrime(boolean[] isNotPrime) {
+        int sqrt = (int) Math.sqrt(MAX);
+        for(int i=2; i<sqrt; i++) {
+            if(!isNotPrime[i]) {
+                int j=2;
+                while(i * j <= MAX) {
+                    isNotPrime[i * j++] = true;
+                }
+            }
+        }
+    }
+}
