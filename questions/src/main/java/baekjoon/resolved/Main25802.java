2a81a5c5f91820e2d3c84b29d06a5f91eeb0c0ae
hmkim <elppapie@naver.com>
2025-05-10 00:28:43 +0900

    👍 :athletic_shoe: 25802번 해결

---
+++ questions/src/main/java/baekjoon/resolved/Main25802.java
@@ -0,0 +1,32 @@
+package baekjoon.resolved;
+
+import java.io.*;
+import java.util.*;
+
+public class Main25802 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/25802
+    */
+    public static final int INIT_LENGTH = 2;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int initialA = a, initialB = b; //초기값 저장
+        int remainder; //시퀀스 새로운 값
+        int seqLength = INIT_LENGTH; //시퀀스 길이
+        while(!(seqLength > INIT_LENGTH && initialA == a && initialB == b)) {
+            remainder = (a + b) % 10;
+            a = b;
+            b = remainder;
+            seqLength++;
+        }
+        bw.write(seqLength + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
