38f2b9e1b1b7e635e9c19b328622b48fea81be41
hmkim <elppapie@naver.com>
2025-05-29 07:11:24 +0900

    💡 15727번 주석 수정
    이전 내용 잘못 들어감

--- questions/src/main/java/baekjoon/resolved/Main15727.java
+++ questions/src/main/java/baekjoon/resolved/Main15727.java
@@ -7,7 +7,7 @@ public class Main15727 {
         https://www.acmicpc.net/problem/15727
         https://www.acmicpc.net/user/bcdlife
     */
-    public static final int SPEED = 5; //1~5~9 (4씩)
+    public static final int SPEED = 5; //1~5중 하나 선택
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
