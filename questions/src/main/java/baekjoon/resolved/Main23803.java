d4a7ea80aca0df2f33cf6045965e6e7e5cd27ceb
hmkim <elppapie@naver.com>
2025-06-05 20:32:44 +0900

    👍 23803번 해결
    처음 제출 때 비어있어야 하는 부분들 괜히 공백으로 출력했다가 출력초과 받았음...
    공백 만나면 해당 line break 해서 해결함
    
    그나저나 어제 제출 건 스트릭에 반영되지 않았는데 설마 오늘도...

---
+++ questions/src/main/java/baekjoon/resolved/Main23803.java
@@ -0,0 +1,28 @@
+package baekjoon.resolved;
+
+import java.io.*;
+
+public class Main23803 {
+    /*
+        https://www.acmicpc.net/problem/23803
+        https://www.acmicpc.net/user/bcdlife
+    */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int k = Integer.parseInt(br.readLine());
+        int length = 5*k;
+        for(int row=0; row<length; row++) {
+            for(int col=0; col<length; col++) {
+                if(col/k == 0 || row/k == 4) sb.append('@');
+                else break;
+            }
+            sb.append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
