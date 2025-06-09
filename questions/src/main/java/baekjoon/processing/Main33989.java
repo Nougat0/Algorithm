23cb4e0917736bf4cc89bb20bbe19eef967750e2
hmkim <elppapie@naver.com>
2025-06-10 00:41:55 +0900

    :zap: 33989번 (대회문제) 로직 수정
    처음 짠 코드에서 혼합구간을 구하긴 했지만... 혼합구간은 구획별로 존재할 수 있다
    예를 들어 DBBBBBBBDDDDDDDB 의 경우 혼합구간은 전 구간이지만,
    첫 번째와 마지막 동전, 딱 2개만 바꾸면 올바른 문자열이다.
    이 경우 양끝이 혼합구간, 가운데 부분은 정상구간이라고 봐도 된다...
    
    결국 특정 위치에 B와 D의 경계선을 만들어야 하고,
    그 경계선의 위치를 순회하면서 뒤집을 게 가장 적은 곳을 찾아봐야 하는 문제인 듯...
    
    -> 혼합구간은 정해져 있는 게 아니라서... 경계선을 어디에다가 놓을지 전수검사를 해봐야 함

--- questions/src/main/java/baekjoon/processing/Main33989.java
+++ questions/src/main/java/baekjoon/processing/Main33989.java
@@ -12,44 +12,51 @@ public class Main33989 {
     public static final int B = 0;
     public static final int D = 1;
     public static void main(String[] args) throws IOException {
+        /*
+            처음 짠 코드에서 혼합구간을 구하긴 했지만... 혼합구간은 구획별로 존재할 수 있다
+            예를 들어 DBBBBBBBDDDDDDDB 의 경우 혼합구간은 전 구간이지만,
+            첫 번째와 마지막 동전, 딱 2개만 바꾸면 올바른 문자열이다.
+            이 경우 양끝이 혼합구간, 가운데 부분은 정상구간이라고 봐도 된다...
+
+            결국 특정 위치에 B와 D의 경계선을 만들어야 하고,
+            그 경계선의 위치를 순회하면서 뒤집을 게 가장 적은 곳을 찾아봐야 하는 문제인 듯...
+        */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         final int N = Integer.parseInt(st.nextToken());
         char[] coins = br.readLine().toCharArray();
-        int[][] sumCoins = new int[N+1][2]; //[B, D] N+1 길이로 지정 (첫번째 값 0)
-        int countB = 0;
-        int countD = 0;
+        int[][] sumCoins = new int[N+1][2]; //[B, D] 위치별 누적합 : coins 맨앞 [0,0] 추가됨
+        int countB = 0, countD = 0; //누적값 기록용
         int lastB = -1, lastD = -1; //마지막으로 등장하는 곳
-        int firstB = -1, firstD = -1; //최초로 등장하는 곳
         final int FIRST_INDEX = 0;
-        final int LAST_INDEX = N-1;
+        final int COINS_LAST_INDEX = N-1;
+        //입력
         for(int i=FIRST_INDEX; i<N; i++) {
             char frontCoin = coins[i];
-            char backCoin = coins[LAST_INDEX-i];
+            char backCoin = coins[COINS_LAST_INDEX-i];
             //동전 혼합구간 기록 (최초/마지막 B와 D)
             if(frontCoin == 'B') lastB = i;
-            if(backCoin == 'D') lastD = LAST_INDEX-i;
-
-            if(firstB == -1 && frontCoin == 'B') firstB = i;
-            if(firstD == -1 && backCoin == 'D') firstD = LAST_INDEX-i;
+            if(backCoin == 'D') lastD = COINS_LAST_INDEX-i;
             //동전 종류별 누적값 기록하기
             if(frontCoin == 'B') countB++;
             else countD++;
             sumCoins[i+1][B] = countB;
             sumCoins[i+1][D] = countD;
         }
-        //조정구간 탐색
+        //경계선 결정하기
         if((lastD == -1 || lastB == -1) || (lastB - lastD + 1 == 0)) { //[*] 혼합구간 부재
             bw.write(0 + "\n");
-        } else { //[*] 혼합구간 존재
-            int mixStart = lastD;
-            int mixEnd = lastB+1; //N+1 길이에 맞춰 index 조정
-
-            //혼합구간에서 적은 종류의 개수 구하기
-            int mixedB = sumCoins[mixEnd][B] - sumCoins[mixStart][B];
-            int mixedD = sumCoins[mixEnd][D] - sumCoins[mixStart][D];
-            bw.write(Math.min(mixedB, mixedD) + "\n");
+        } else { //[*] 혼합구간 존재 - 최적의 경계선 순회하면서 찾기
+            int minChange = N; //최소 변경값 기록
+            int change;
+            for(int borderLeft=FIRST_INDEX; borderLeft<=N; borderLeft++) {
+                //[borderLeft] (경계선) [borderLeft + 1]
+                //왼쪽의 D, 오른쪽의 B를 모두 변경
+                change = sumCoins[borderLeft][D] + (sumCoins[N][B] - sumCoins[borderLeft][B]);
+                if(change < minChange) minChange = change;
+            }
+            bw.write(minChange + "\n");
         }
         bw.flush();
         bw.close();
