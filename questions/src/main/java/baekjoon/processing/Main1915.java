cb8607712707e2ea93d935ae00e7741d6ed3002e
hmkim <elppapie@naver.com>
2025-06-09 11:11:43 +0900

    :arrow_right: 1915번 해결중 (검색한 DP방식 도입했지만 틀림)
    직접 떠올린 기존 방식은 2가지 단점이 존재하며, 이로 인해 시간초과가 뜸
      1. 사각형 크기가 커질수록 검색해야 할 범위도 넓어진다 (한 변의 길이*2)
      2. 모든 유효한 좌표별로 연산을 따로 해줘야 하는 단점 존재 (이전 연산값 재활용 못함)
    
    그래서 찾아보니 정반대의 방식으로 DP를 사용할 수 있다
    (https://breakcoding.tistory.com/366 설명 방식 참고)
      - 기존방식: 왼쪽 위 꼭짓점 기준
      - 찾은방식: 오른쪽 아래 꼭짓점 기준
        - 2*2 범위만 항상 검색하면 됨 (1번 단점 상쇄)
        - 이전에 연산한 사각형 최대크기를 활용하여 해당 좌표의 최대크기 연산 가능 (2번 단점 상쇄)
         11111
         12222
         12333
         12344
         12345

--- questions/src/main/java/baekjoon/processing/Main1915.java
+++ questions/src/main/java/baekjoon/processing/Main1915.java
@@ -31,6 +31,25 @@ public class Main1915 {
 
         각 길이 체크 때마다 전체 n*n 개를 모두 순회할 필요없이,
         n-1 크기에서 이미 확인된 정사각형 바깥의 칸만 체크하면 됨
+
+        ================================================================
+
+        직접 떠올린 위 방식은 2가지 단점이 존재하며, 이로 인해 시간초과가 뜸
+        1. 사각형 크기가 커질수록 검색해야 할 범위도 넓어진다 (한 변의 길이*2)
+        2. 모든 유효한 좌표별로 연산을 따로 해줘야 하는 단점 존재 (이전 연산값 재활용 못함)
+
+        그래서 찾아보니 정반대의 방식으로 DP를 사용할 수 있다
+        (https://breakcoding.tistory.com/366 설명 방식 참고)
+        - 기존방식: 왼쪽 위 꼭짓점 기준
+        - 찾은방식: 오른쪽 아래 꼭짓점 기준
+            - 2*2 범위만 항상 검색하면 됨 (1번 단점 상쇄)
+            - 이전에 연산한 사각형 최대크기를 활용하여 해당 좌표의 최대크기 연산 가능 (2번 단점 상쇄)
+        11111
+        12222
+        12333
+        12344
+        12345
+
     */
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -39,100 +58,32 @@ public class Main1915 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        boolean[][] sequence = new boolean[n][m];
+        int[][] maxLengthValues = new int[n+1][m+1]; //각 좌표(오른쪽 아래 꼭짓점)별로 사각형 최대길이값 저장용 (DP)
+        int maxLengthOfCoord; //해당 좌표의 사각형 최대길이값
+        int maxLength = 0;
         boolean filled;
-        Set<Coordinate> list = new HashSet<>();
+        //입력받으면서 동시에 maxLength DP 값 연산해서 저장
         for(int i=0; i<n; i++) {
             String line = br.readLine();
             for(int j=0; j<m; j++) {
                 //1: true, 0: false
                 filled = line.charAt(j) == '1';
                 if(filled) {
-                    sequence[i][j] = true;
-                    list.add(new Coordinate(i, j));
+                    //2*2 범위 탐색, 최소값+1 도출
+                    maxLengthOfCoord = Math.min(Math.min(maxLengthValues[i][j], maxLengthValues[i+1][j]), maxLengthValues[i][j+1]) + 1;
+                } else {
+                    //사각형이 되지 못하므로 무조건 0
+                    maxLengthOfCoord = 0;
                 }
+                //최대값 갱신하기
+                if(maxLength < maxLengthOfCoord) maxLength = maxLengthOfCoord;
+                maxLengthValues[i+1][j+1] = maxLength;
             }
         }
-        //연산, 출력
-        Sequence seq = new Sequence(n, m, sequence, list);
-        int maxLength = seq.getMaxSquare();
+        //출력
         bw.write(maxLength*maxLength + "\n");
         bw.flush();
         bw.close();
         br.close();
-    }
-
-    /**
-     * 좌표 저장용 클래스 구현
-     * 정사각형의 왼쪽 위 꼭지점 좌표 저장용
-     */
-    public static class Coordinate {
-        int y, x;
-        public Coordinate(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
-    }
-
-    /**
-     * 배열 저장, 정사각형 최대 크기 구하는 클래스 구현
-     */
-    public static class Sequence {
-        int n, m, minLength; //배열크기 행의 수, 열의 수, 더 작은 값
-        boolean[][] sequence; //입력받은 배열
-        boolean squareExist; //length(인덱스)의 정사각형 존재유무
-        Set<Coordinate> list; //length(인덱스)의 정사각형을 이룬 왼쪽 위 꼭지점 목록
-
-        public Sequence(int n, int m, boolean[][] sequence, Set<Coordinate> list) {
-            this.n = n; //행
-            this.m = m; //열
-            this.sequence = sequence; //배열 (n*m)
-            minLength = Math.min(n, m); //가능한 가장 큰 정사각형의 한 변 길이
-            squareExist = false;
-            this.list = list;
-        }
-
-        /**
-         * 가장 큰 정사각형의 한 변의 길이 반환
-         * @return
-         */
-        public int getMaxSquare() {
-            // 길이 1 먼저 체크 (확인 로직이 다름) -- list로 전달받음
-            squareExist = list.size() > 0;
-            // 길이 2 -> maxLength 까지 정사각형 순차 체크
-            int maxSquareLength = 1;
-            Iterator<Coordinate> iterator;
-            for(int l=2; l<=minLength && squareExist; l++) {
-                squareExist = false; //이번 길이 체크 초기화
-
-                iterator = list.iterator();
-                //이전 크기 정사각형들 꼭지점 순환
-                while(iterator.hasNext()) {
-                    Coordinate coord = iterator.next();
-                    boolean isSquare = true;
-                    int yMax = coord.y + l - 1;
-                    int xMax = coord.x + l - 1;
-                    if(yMax >= n || xMax >= m) {
-                        continue; //배열 범위 초과
-                    }
-                    //이번 크기의 오른쪽 끝, 아래 끝 한 줄씩 체크
-                    for(int i=0; i<l && isSquare; i++) {
-                        isSquare = sequence[coord.y + i][xMax - 1] && sequence[yMax - 1][coord.x + i];
-                    }
-                    isSquare = isSquare && sequence[yMax][xMax];
-
-                    if(isSquare) { //정사각형이 맞을 경우
-                        squareExist = true;
-                    } else { //이번 길이에서 해당 좌표는 정사각형 아님
-                        iterator.remove();
-                    }
-                }
-                //이번 길이에서 정사각형이 있었으면 최대값 갱신
-                if(squareExist) {
-                    maxSquareLength = l;
-                }
-            }
-            return maxSquareLength;
-        }
     }
 }
