64fcb0d4afaf3acbdb294f2fa2e3328f152620d7
hmkim <elppapie@naver.com>
2025-06-25 08:05:59 +0900

    :zap: 2193번 성능 떨어짐..
    혹시나 시간복잡도를 줄일 수 있지 않을까 기대하며 배열로도 풀어봄
    예상한 대로 사용 메모리가 많이 늘어났는데... 시간복잡도는 그대로였다...!
    성능 개선 실패!

--- questions/src/main/java/baekjoon/resolved/Main2193.java
+++ questions/src/main/java/baekjoon/resolved/Main2193.java
@@ -11,17 +11,13 @@ public class Main2193 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         final int n = Integer.parseInt(br.readLine());
         //배열 대신 직전값 저장변수만 사용
-        long zeroEndNumCnt = 0;
-        long oneEndNumCnt = 1;
-        long zeroEnds, oneEnds;
+        long[][] dp = new long[n+1][2];
+        dp[1][1] = 1;
         for(int i=2; i<=n; i++) {
-            zeroEnds = zeroEndNumCnt + oneEndNumCnt;
-            oneEnds = zeroEndNumCnt;
-            //계산값 담기(재할당)
-            zeroEndNumCnt = zeroEnds;
-            oneEndNumCnt = oneEnds;
+            dp[i][1] = dp[i-1][0];
+            dp[i][0] = dp[i-1][0] + dp[i-1][1];
         }
-        System.out.println(zeroEndNumCnt + oneEndNumCnt);
+        System.out.println(dp[n][0] + dp[n][1]);
         br.close();
     }
 }
