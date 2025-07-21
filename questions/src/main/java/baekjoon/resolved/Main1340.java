d36cf964cd4d6282234dc4f853012fdf8392bfa8
hmkim <elppapie@naver.com>
2025-07-21 20:45:22 +0900

    :zap: 1340번 직접 구현
    time 패키지 미사용하면서 구현함
    확실히 시간, 메모리 많이 줄긴 했지만... 조금 아쉽다
    
    제출 번호	아이디	문제	결과	메모리	시간	언어	코드 길이	제출한 시간
    96586983	bcdlife	 1340	맞았습니다!!	14344	120	Java 11 / 수정	2078

--- questions/src/main/java/baekjoon/resolved/Main1340.java
+++ questions/src/main/java/baekjoon/resolved/Main1340.java
@@ -2,26 +2,59 @@ package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
-import java.time.*;
-import java.time.format.DateTimeFormatter;
 
 public class Main1340 {
     /*
-        time, util 패키지 100% 활용하는 방법
+        직접 구현하는 방법
         https://www.acmicpc.net/user/bcdlife
         https://www.acmicpc.net/problem/1340
     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy HH:mm", Locale.ENGLISH);
-        LocalDateTime dateTime = LocalDateTime.parse(br.readLine(), dateFormatter);
-        double fullYear = dateTime.toLocalDate().lengthOfYear();
-        double daysPassed = dateTime.getDayOfYear() - 1;
-        //하루는 1440분
-        int hour = dateTime.getHour();
-        int minute = dateTime.getMinute();
+        //MMMM dd, yyyy HH:mm
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int month = Month.valueOf(st.nextToken()).ordinal() + 1;
+        int day = Integer.parseInt(st.nextToken().replace(",", ""));
+        int year = Integer.parseInt(st.nextToken());
+        String[] time = st.nextToken().split(":");
+        boolean isLeapYear = isLeapYear(year);
+        double fullYear = 365; //기본 일수
+        double daysPassed = 0;
+        //윤년일 시 확인 (윤달 +1)
+        if(isLeapYear) {
+            if(month > 2) daysPassed++;
+            fullYear++;
+        }
+        //지나간 월별 일수
+        for(int m=1; m<month; m++) {
+            if(m == 4 || m == 6 || m == 9 || m == 11) daysPassed += 30;
+            else if(m == 2) daysPassed += 28;
+            else daysPassed += 31;
+        }
+        //지나간 이번달 일수
+        daysPassed += (day - 1);
+        //지나간 오늘 시간(분)
+        int hour = Integer.parseInt(time[0]);
+        int minute = Integer.parseInt(time[1]);
         //분 단위로 쪼개서 연산하고 출력
         System.out.println((daysPassed * 1440 + (hour*60 + minute)) / (fullYear * 1440) * 100);
         br.close();
+    }
+
+    /**
+     * 윤년인지 체크
+     * @param year
+     * @return
+     */
+    public static boolean isLeapYear(int year) {
+        boolean isLeapYear = false;
+        if(year % 4 == 0) isLeapYear = true;
+        if(year % 100 == 0) isLeapYear = false;
+        if(year % 400 == 0) isLeapYear = true;
+        return isLeapYear;
+    }
+    //MMMM -> 숫자로 변환하기 위한 enum
+    public enum Month {
+        January, February, March, April, May, June, July, August, September, October, November, December
     }
 }
