5a9c667d450d78b99556062bbf3bb94fda0ee842
hmkim <elppapie@naver.com>
2025-07-21 20:57:05 +0900

    :arrow_right: 👟 17287번 풀어놓음 (미제출)
    단순하게 풀 수 있어서 만족스러움
    이제 안 틀리면 되는데...

---
+++ questions/src/main/java/baekjoon/processing/Main17287.java
@@ -0,0 +1,30 @@
+package baekjoon.processing;
+
+import java.io.*;
+
+public class Main17287 {
+    /*
+        https://www.acmicpc.net/problem/17287
+        https://www.acmicpc.net/user/bcdlife
+    */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String string = br.readLine();
+        int length = string.length();
+        int score = 0;
+        int maxScore = 0;
+        for(int i=0; i<length; i++) {
+            switch(string.charAt(i)) {
+                case '(': score += 1; break;
+                case '{': score += 2; break;
+                case '[': score += 3; break;
+                case ')': score -= 1; break;
+                case '}': score -= 2; break;
+                case ']': score -= 3; break;
+                default: if(score > maxScore) maxScore = score;
+            }
+        }
+        System.out.println(maxScore);
+        br.close();
+    }
+}
