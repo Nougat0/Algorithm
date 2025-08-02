4ed45319de8a0db542a8d1935c552ed73cbb04aa
hmkim <elppapie@naver.com>
2025-08-02 12:00:04 +0900

    ➡️ 👟 21623번 해결중 (78% 틀림)
    예외처리 로직 하나 넣었으나 그게 문제가 아니었나봄...
    라운드가 딱 맞아떨어지게 끝나면 score은 0으로 유지... 하는 로직이었음

---
+++ questions/src/main/java/baekjoon/processing/Main21623.java
@@ -0,0 +1,41 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+
+public class Main21623 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/21623
+    */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int moves = Integer.parseInt(st.nextToken());
+        int initial = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        /*
+        If the last move of the game had ended the round, Petya didn't not begin a new round and his score was 0.
+        */
+        int score = initial;
+        int round = 0;
+        while(moves-- > 0) {
+            int number = Integer.parseInt(st.nextToken());
+            if(score >= number) {
+                score -= number;
+                if(score == 0) {
+                    round++;
+                    if(moves > -1) score = initial;
+                }
+            }
+        }
+        sb.append(round).append("\n").append(score);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
