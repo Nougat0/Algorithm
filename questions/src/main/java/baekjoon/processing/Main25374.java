e801478e9d28848936ba80aff80fafa33fcc996d
hmkim <elppapie@naver.com>
2025-08-19 10:34:36 +0900

    :arrow_right: :athletic_shoe: 25374번 해결중 (38%? 틀림)
    단순 binarySearch 를 사용하려다가 같은 점수가 여러개라면 문제가 되므로 우회함...
    점수별로 몇 명이 있는지 세어놓고 진행하는 방식으로 바꿨더니 굳이 binarySearch 가 필요없음
    
    최대한 순회를 적게 하기 위해 iterator 1개만 사용
    
    하지만 38%?? 인가 (못 봤음) 에서 틀렸다
    음.... 등급 하나 건너뛰는 부분 예외처리를 추가해야 할 것 같은데 아마도...
    
    1등급 인원이 2등급 비율 (11%) 를 넘어서면 1등급 다음에는 3등급이 있다

---
+++ questions/src/main/java/baekjoon/processing/Main25374.java
@@ -0,0 +1,53 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+
+public class Main25374 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/25374
+    */
+    public static void main(String[] args) throws IOException {
+        /*
+        점수 중복이 존재할 수 있으므로 단순 binarySearch 는 안됨 (점부별 개수(범위) 가늠이 안됨)
+        */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int[] cutLines = new int[]{0, 4, 11, 23, 40, 60, 77, 89, 96, 100}; //1~9
+        int[] cutLineCount = new int[10];
+
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        int[] scoreCount = new int[n+1]; //0 ~ 100점까지 점수별 인원수 확인
+        Set<Integer> scores = new HashSet<>();
+        for(int i=1; i<=n; i++) {
+            int score = Integer.parseInt(st.nextToken());
+            scoreCount[score]++;
+            scores.add(score);
+        }
+        List<Integer> scoreList = new ArrayList<>(scores);
+        scoreList.sort(Comparator.reverseOrder());
+        Iterator<Integer> it = scoreList.iterator(); //iterator 사용해서 100개 점수 1번만 순회
+        int totalCount = 0;
+        int lastCount = 0;
+        for(int i=1; i<=9; i++) {
+            while(it.hasNext()) {
+                int score = it.next();
+                if((totalCount += scoreCount[score]) >= cutLines[i]) {
+                    cutLineCount[i] = totalCount - lastCount;
+                    lastCount = totalCount;
+                    break;
+                }
+            }
+        }
+        for(int i=1; i<=9; i++) sb.append(cutLineCount[i]).append("\n");
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
