52cbcc33ac72792050fd10adc824b898244fe930
hmkim <elppapie@naver.com>
2025-08-20 19:51:14 +0900

    :arrow_right: :athletic_shoe: 14724번 해결중 (1% 틀림)
    왜...? 내가 문제를 잘못 이해했나???
    그냥 어떤 row에서 최대값을 갖고 있는지 확인하면 끝 아닌가??

---
+++ questions/src/main/java/baekjoon/processing/Main14724.java
@@ -0,0 +1,38 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+
+public class Main14724 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/14724
+    */
+    public static enum NAMES {
+        PROBRAIN, GROW, ARGOS, ADMIN, ANT, MOTION, SPG, COMON, ALMIGHTY
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int count = NAMES.values().length;
+        int value, max = 0, maxRow = -1;
+        for(int row=0; row<count; row++) {
+            st = new StringTokenizer(br.readLine());
+            for(int i=0; i<n; i++) {
+                value = Integer.parseInt(st.nextToken());
+                if(value > max) {
+                    max = value;
+                    maxRow = row;
+                    break;
+                }
+            }
+        }
+        bw.write(NAMES.values()[maxRow].name());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
