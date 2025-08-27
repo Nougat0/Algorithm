eb759e48f0e086f761fba9ee159c11078ee8c56a
hmkim <elppapie@naver.com>
2025-08-27 22:48:23 +0900

    :arrow_right: :athletic_shoe: 27959번 해결중 (미제출)

---
+++ questions/src/main/java/baekjoon/processing/Main27959.java
@@ -0,0 +1,21 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+
+public class Main27959 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/27959
+    */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        if(n * 100 >= m) System.out.println("Yes");
+        else System.out.println("No");
+        br.close();
+    }
+}
