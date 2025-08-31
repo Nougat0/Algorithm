59ecfac0694cf37e20e183a7e51d5f63b25d2e61
hmkim <elppapie@naver.com>
2025-08-31 19:51:37 +0900

    :zap: :athletic_shoe: 4848번 리팩토링
    https://www.acmicpc.net/source/82399829
    15개이다 보니까 String.equals 를 쓰더라도 별개의 Map 을 사용하는 것보다는 loop을 도는 것이 낫다...

--- questions/src/main/java/baekjoon/resolved/Main4848.java
+++ questions/src/main/java/baekjoon/resolved/Main4848.java
@@ -1,7 +1,6 @@
 package baekjoon.resolved;
 
 import java.io.*;
-import java.util.*;
 
 public class Main4848 {
     /*
@@ -14,21 +13,22 @@ public class Main4848 {
         StringBuilder print = new StringBuilder();
         int testCases = Integer.parseInt(br.readLine());
         String[] output = new String[16]; //출력용 배열
-        Map<String, Integer> input = new HashMap<>(); //입력용 map
-        input.put("{}", 0);
         output[0] = "{}";
         for(int i=1; i<=15; i++) {
             StringBuilder string = new StringBuilder();
             string.append(output[0]);
             for(int j=1; j<i; j++) string.append(",").append(output[j]);
-            input.put("{" + string + "}", i);
             output[i] = "{" + string + "}";
         }
         //입력값으로 연산
         for(int t=0; t<testCases; t++) {
             int sum = 0;
-            sum += input.get(br.readLine());
-            sum += input.get(br.readLine());
+            String s1 = br.readLine();
+            String s2 = br.readLine();
+            for(int i=0; i<=15; i++) {
+                if(s1.equals(output[i])) sum += i;
+                if(s2.equals(output[i])) sum += i;
+            }
             print.append(output[sum]).append("\n");
         }
         bw.write(print.toString());
