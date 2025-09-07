206eab2a9a2140c74700b712d90d9c68a8325ff1
hmkim <elppapie@naver.com>
2025-09-07 15:16:50 +0900

    :arrow_right: 🏆 Lemon Cup - F번 부분 맞음 (31점)
    N <= 500 일 경우에 대해서만 맞았고... 제약 조건 없어지면 틀리고 있음

---
+++ questions/src/main/java/baekjoon/contest/Main1533_F.java
@@ -0,0 +1,56 @@
+package baekjoon.contest;
+
+import java.io.*;
+import java.util.*;
+
+public class Main1533_F {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/contest/problem/1533/6
+    */
+    public static final String WellKnown = "WellKnown";
+    public static final String AdHoc = "AdHoc";
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        final int N = Integer.parseInt(st.nextToken());
+        final int K = Integer.parseInt(st.nextToken());
+        //먼저 출제된 문제들 중 이번 문제에서 사용하는 알고리즘을 모두 사용하는 것이 있다면
+        //이번 문제는 WellKnown 이 된다
+        Set<String> cases = new HashSet<>();
+        boolean z = false;
+        boolean[][] S = new boolean[N+1][K+1];
+        for(int i=1; i<=N; i++) {
+            String line = br.readLine();
+            StringBuilder builder = new StringBuilder();
+            int count = 0;
+            for(int j=1; j<=K; j++) {
+                int index = z ? K-j+1 : j;
+                boolean ij = line.charAt(index-1) == '1';
+                builder.append((S[i][j] = ij) ? 1 : 0);
+                if(ij) count++;
+            }
+            //이전 문제들에서 이번 문제의 wellKnown 여부(z) 확인
+            z = true;
+            for(String s : cases) {
+                int containsCount = 0;
+                for(int j=1; j<=K; j++) {
+                    if(S[i][j] == true && s.charAt(j-1) == '1') containsCount++;
+                }
+                if(containsCount == count) {
+                    z = false;
+                    break;
+                }
+            }
+            if(i > 1 && !z) sb.append(WellKnown).append("\n");
+            else sb.append(AdHoc).append("\n");
+            cases.add(builder.toString());
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
