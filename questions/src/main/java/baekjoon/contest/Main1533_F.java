3d5d83bee52a7e7c4acf920493270517d7768ace
hmkim <elppapie@naver.com>
2025-09-07 16:25:07 +0900

    ➡️ 🏆 Lemon Cup - F번 부분 맞음 (31점)
    String Set을 계속 사용해서 문제인가... 싶어서 이진수 Integer처럼 사용하도록 변경함 (비트연산 활용)
    그래도 여전히 31점 대체 뭐가 문제일까...
    
    N 크기에 따라 맞고 틀리고가 결정된다면 분명 속도나 메모리 문제일 것 같은데.....

--- questions/src/main/java/baekjoon/contest/Main1533_F.java
+++ questions/src/main/java/baekjoon/contest/Main1533_F.java
@@ -19,10 +19,11 @@ public class Main1533_F {
         final int K = Integer.parseInt(st.nextToken());
         //먼저 출제된 문제들 중 이번 문제에서 사용하는 알고리즘을 모두 사용하는 것이 있다면
         //이번 문제는 WellKnown 이 된다
-        Set<String> cases = new HashSet<>();
+        Set<Integer> cases = new HashSet<>();
         boolean z = false;
         int[] usages = new int[21]; //알고리즘 사용된 것들의 index 기록해두기
         for(int i=1; i<=N; i++) {
+            int currentValue = 0;
             int usageIndex = 0;
             String line = br.readLine();
             StringBuilder builder = new StringBuilder();
@@ -32,16 +33,17 @@ public class Main1533_F {
                 boolean ij = line.charAt(index-1) == '1';
                 builder.append(ij ? 1 : 0);
                 if(ij) {
+                    currentValue += Math.pow(2, K-j);
                     usages[++usageIndex] = j;
                     count++;
                 }
             }
             //이전 문제들에서 이번 문제의 wellKnown 여부(z) 확인
             z = true;
-            for(String s : cases) {
+            for(Integer value : cases) {
                 int containsCount = 0;
                 for(int j=1; j<=usageIndex; j++) {
-                    if(builder.charAt(usages[j]-1) == '1' && s.charAt(usages[j]-1) == '1') containsCount++;
+                    if(getUsage(currentValue, K-usages[j]) && getUsage(value, K-usages[j])) containsCount++;
                     else break;
                 }
                 if(containsCount == count) {
@@ -51,11 +53,16 @@ public class Main1533_F {
             }
             if(i > 1 && !z) sb.append(WellKnown).append("\n");
             else sb.append(AdHoc).append("\n");
-            cases.add(builder.toString());
+            cases.add(currentValue);
         }
         bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
+    }
+
+    //이진수 자릿수 확인을 위해 비트 연산 활용
+    public static boolean getUsage(int value, int digit) {
+        return (value >> digit & 1) == 1;
     }
 }
