be1ad7a0f2ad944751878ef2fc34bc48e5decf9f
hmkim <elppapie@naver.com>
2025-09-21 00:14:30 +0900

    :zap: :athletic_shoe: 28307번 리팩토링
    입력과 동시에 분석하도록 했으나.. 큰 변화는 없었음
    
    제출 번호	아이디	문제	결과	메모리	시간	언어	코드 길이	제출한 시간
    98731115	bcdlife	 28307	맞았습니다!!	38712	324	Java 11 / 수정	1685
    98730647	bcdlife	 28307	맞았습니다!!	39440	336	Java 11 / 수정	1549

--- questions/src/main/java/baekjoon/resolved/Main28307.java
+++ questions/src/main/java/baekjoon/resolved/Main28307.java
@@ -16,32 +16,34 @@ public class Main28307 {
         final int N = Integer.parseInt(br.readLine());
         boolean[][] tri = new boolean[2][N];
         int colorCount = 0;
-        //입력
-        for(int i=0; i<2; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++) {
-                boolean colored = st.nextToken().equals("1");
-                tri[i][j] = colored;
-                if(colored) colorCount++;
-            }
+        int lines = 0;
+        //윗줄 입력
+        st = new StringTokenizer(br.readLine());
+        for(int j=0; j<N; j++) {
+            boolean colored = st.nextToken().equals("1");
+            tri[UP][j] = colored;
+            if(colored) colorCount++;
         }
-        int lines = colorCount*3;
-        //인접 체크해서 변 제거
+        //아랫줄 입력 + 체크
+        st = new StringTokenizer(br.readLine());
         for(int j=0; j<N; j++) {
             boolean coloredUp = tri[UP][j];
-            boolean coloredDown = tri[DOWN][j];
+            boolean coloredDown = st.nextToken().equals("1");
+            tri[DOWN][j] = coloredDown;
+            //인접 체크해서 변 제거
             if(coloredUp) {
                 //윗줄 좌우 인접한지 확인
-                if(j+1 < N && tri[UP][j+1]) lines -= 2;
+                if(j-1 >= 0 && tri[UP][j-1]) lines -= 2;
                 //(홀수일 경우) 아래와 인접한지 확인
                 if(j % 2 == 0 && coloredDown) lines -= 2;
             }
             if(coloredDown) {
                 //아랫줄 좌우 인접한지 확인
-                if(j+1 < N && tri[DOWN][j+1]) lines -= 2;
+                if(j-1 >= 0 && tri[DOWN][j-1]) lines -= 2;
+                colorCount++; //갯수 추가
             }
         }
-        System.out.println(lines);
+        System.out.println(lines + colorCount*3);
         br.close();
     }
 }
