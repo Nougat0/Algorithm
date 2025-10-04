fc5f048eae9d4eb8f702bccdbf2d012f12701c15
hmkim <elppapie@naver.com>
2025-10-04 20:34:00 +0900

    👍 👟 6571번 해결
    DP로 바꾸니까 바로 해결...
    시간초과가 틀렸다고 뜬 거였나...?

--- questions/src/main/java/baekjoon/resolved/Main6571.java
+++ questions/src/main/java/baekjoon/resolved/Main6571.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.math.BigInteger;
@@ -14,24 +14,25 @@ public class Main6571 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
+        //dp 로 피보나치
+        BigInteger[] arr = new BigInteger[1001];
+        arr[1] = BigInteger.ONE;
+        arr[2] = BigInteger.TWO;
+        for(int i=3; i<1001; i++) {
+            arr[i] = arr[i-1].add(arr[i-2]);
+        }
+
         while(true) {
             st = new StringTokenizer(br.readLine());
             BigInteger a = new BigInteger(st.nextToken());
             BigInteger b = new BigInteger(st.nextToken());
             if(a.equals(BigInteger.ZERO) && b.equals(BigInteger.ZERO)) break;
 
-            BigInteger v1 = BigInteger.TWO;
-            BigInteger v2 = BigInteger.ONE;
-            BigInteger v0;
             int count = 0;
-
-            if(v1.compareTo(a) >= 0) count++;
-            if(v2.compareTo(a) >= 0) count++;
-
-            while((v0 = v1.add(v2)).compareTo(b) <= 0) {
-                if(v0.compareTo(a) >= 0) count++;
-                v2 = v1;
-                v1 = v0;
+            int i = 1;
+            while(arr[i].compareTo(b) <= 0) {
+                if(arr[i].compareTo(a) >= 0) count++;
+                i++;
             }
             sb.append(count).append("\n");
         }
