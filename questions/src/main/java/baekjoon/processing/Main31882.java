3b41774b9871090dbcad1899b1e70b2b51361d43
hmkim <elppapie@naver.com>
2025-10-06 11:52:51 +0900

    :arrow_right: 👟 31882번 해결중 (16% 틀림)
    크아악

---
+++ questions/src/main/java/baekjoon/processing/Main31882.java
@@ -0,0 +1,38 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+
+public class Main31882 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/31882
+    */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int N = Integer.parseInt(br.readLine());
+        String line = br.readLine();
+
+        Set<Integer> lengths = new HashSet<>();
+        int[] count = new int[1_000_001];
+        int sum = 0;
+        int thisCount = 0;
+        for(int i=0; i<N; i++) {
+            int number = line.charAt(i) - '0';
+            if(number == 2) {
+                thisCount++;
+            } else {
+                count[thisCount]++;
+                lengths.add(thisCount);
+                thisCount = 0;
+            }
+        }
+
+        for(Integer num : lengths) {
+            int c = count[num];
+            for(int i=1; i<=num; i++) sum += i*(num-i+1)*c;
+        }
+        System.out.println(sum);
+        br.close();
+    }
+}
