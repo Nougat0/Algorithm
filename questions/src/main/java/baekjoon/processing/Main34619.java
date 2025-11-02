723d37fccb0821d2f685b4f6220412583414b8cc
hmkim <90332022+Nougat0@users.noreply.github.com>
2025-11-02 11:39:44 +0900

    :+1: 34619번 해결
    전체 소대 번호만 구하고 중대 내 순서를 안 구함
    
    깃허브 어플에서는 파일 다른 폴더로 옮기는 방법을 몰라서 우선은 맞았다고 커밋하고 :truck: 으로 옮기려고 함.

--- questions/src/main/java/baekjoon/processing/Main34619.java
+++ questions/src/main/java/baekjoon/processing/Main34619.java
@@ -14,10 +14,11 @@ public class Main34619 {
         int c = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
         
-        int platoon = n/c + (n%c == 0 ? 0 : 1); //소대 번호
-        int company = platoon/b + (platoon%b == 0 ? 0 : 1); //중대 번호
+        int platoon = n/c + (n%c == 0 ? 0 : 1); //전체 소대 순서
+        int companyNo = platoon/b + (platoon%b == 0 ? 0 : 1); //중대 번호
+        int platoonNo = platoon%b == 0 ? b : platoon%b; //소대 번호
         
-        System.out.println(company + " " + platoon);
+        System.out.println(companyNo + " " + platoonNo);
         br.close();
     }
 }
