7ab5801c8d4065fd04b24923e65ea0cc3c3b1f2d
hmkim <elppapie@naver.com>
2025-11-06 22:36:34 +0900

    👍 :athletic_shoe: 9575번 해결

---
+++ questions/src/main/java/baekjoon/resolved/Main9575.java
@@ -0,0 +1,67 @@
+package baekjoon.resolved;
+
+import java.io.*;
+import java.util.*;
+
+public class Main9575 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/9575
+    */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int testCase = Integer.parseInt(br.readLine());
+        for(int t=0; t<testCase; t++) {
+            Set<Integer> sum = new HashSet<>();
+
+            int n = Integer.parseInt(br.readLine());
+            int[] a = new int[n];
+            st = new StringTokenizer(br.readLine());
+            for(int i=0; i<n; i++) a[i] = Integer.parseInt(st.nextToken());
+
+            int m = Integer.parseInt(br.readLine());
+            int[] b = new int[m];
+            st = new StringTokenizer(br.readLine());
+            for(int i=0; i<m; i++) b[i] = Integer.parseInt(st.nextToken());
+
+            int k = Integer.parseInt(br.readLine());
+            int[] c = new int[k];
+            st = new StringTokenizer(br.readLine());
+            for(int i=0; i<k; i++) c[i] = Integer.parseInt(st.nextToken());
+
+            for(int i=0; i<n; i++) {
+                int one = a[i];
+                for(int j=0; j<m; j++) {
+                    int two = b[j];
+                    for(int l=0; l<k; l++) {
+                        int three = c[l];
+                        sum.add(one + two + three);
+                    }
+                }
+            }
+            int count = 0;
+            for(Integer number : sum) {
+                if(checkLucky(number)) {
+                    count++;
+                }
+            }
+            sb.append(count).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static boolean checkLucky(Integer number) {
+        while(number/10 > 0) {
+            if(number%10 != 5 && number%10 != 8) return false;
+            number /= 10;
+        }
+        if(number%10 != 5 && number%10 != 8) return false;
+        return true;
+    }
+}
