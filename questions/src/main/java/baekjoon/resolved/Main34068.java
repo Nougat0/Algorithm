6e0e681ccc067d833c9cf34f917d10939d69e30c
hmkim <elppapie@naver.com>
2025-11-13 13:02:19 +0900

    👍 :athletic_shoe: 34068번 해결
    질문게시판 갔더니 반례요정님이 올려주신 보석같은 반례들이... 덕분에 해결했다 ㅠㅠ
    
    제출하니 오우... 메모리와 시간 엄청 많이 씀
    1위는 대체 어떻게 한 거지
    
    등수 제출 번호 시도 아이디 메모리 시간 언어 코드 길이 제출한 시간
    1 96580179 2 nabina1395 17420 200 Java 11 965

--- questions/src/main/java/baekjoon/resolved/Main34068.java
+++ questions/src/main/java/baekjoon/resolved/Main34068.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
@@ -24,16 +24,17 @@ public class Main34068 {
             winCount[winner]++;
         }
         //점수 나열하기
-        int[] score = new int[n+1];
+        int maxScore = Math.max(n, m);
+        int[] score = new int[maxScore+1];
         for(int i=1; i<=n; i++) {
             score[winCount[i]]++;
         }
         //2명 이상 있는 점수대에서 결투하기
         long neededRounds = 0;
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<=maxScore; i++) {
             int scoreCount = score[i];
             //n명이면 점수별로 1명씩 고르게 폈을 때 0~(n-1)점
-            //0점에 20만명 있는 경우 400억 회까지 갈 수 있음
+            //0점에 20만명 있는 경우 long 범위의 경기횟수가 필요할 수 있음
             //토너먼트가 아니라서 중복경기가 가능...하지만 그래도 최대점수 한계는 존재
             if(scoreCount > 1) {
                 int rounds = scoreCount-1;
