8f602f89c5d76d2cfc79df3374fa3586360e0e03
hmkim <elppapie@naver.com>
2025-11-14 12:56:55 +0900

    :arrow_right: :athletic_shoe: 13217번 해결중 (1% 틀림)
    나름 머리굴린 건데 틀려서 속상함...

---
+++ questions/src/main/java/baekjoon/processing/Main13217.java
@@ -0,0 +1,47 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+
+public class Main13217 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/13217
+    */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int hives = Integer.parseInt(st.nextToken());
+        int ml = Integer.parseInt(st.nextToken());
+        int tries = Integer.parseInt(st.nextToken());
+        int[] hiveHoney = new int[ml + 1];
+        for(int i=0; i<hives; i++) {
+            int amount = Integer.parseInt(br.readLine());
+            // 미리 honeyPot 용량에 맞춰 분리
+            if(amount >= ml) {
+                int fullCount = amount / ml;
+                int remainder = amount % ml;
+                hiveHoney[ml] += fullCount;
+                hiveHoney[remainder]++;
+            } else {
+                hiveHoney[amount]++;
+            }
+        }
+        int count = 0;
+        int totalAmount = 0;
+        int honey = ml;
+        while(count < tries && honey > 0) {
+            if(hiveHoney[honey] > 0) {
+                int usingTimes = Math.min(hiveHoney[honey], tries - count);
+                count += usingTimes;
+                totalAmount += usingTimes * honey;
+            }
+            honey--;
+        }
+        bw.write(totalAmount + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
