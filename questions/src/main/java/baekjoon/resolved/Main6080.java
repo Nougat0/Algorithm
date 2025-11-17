7b670b97842d58e2bb52de59bf6d421bfbaef129
hmkim <elppapie@naver.com>
2025-11-17 20:49:55 +0900

    :zap: :athletic_shoe: 6080번 해결
    다른 분 코드 참고함) grass=0 이면 체크 안 하는 점을 활용해서 checked 배열 미사용
    시간은 반이나 줄었으나 메모리는 소폭 증가...
    
    제출 번호	아이디	문제	결과	메모리	시간	언어	코드 길이	제출한 시간
    100312890	bcdlife	 6080	맞았습니다!!	82736	1048	Java 11 / 수정	1844	2분 전
    100312540	bcdlife	 6080	맞았습니다!!	74180	2292	Java 11 / 수정	1902	13분 전

--- questions/src/main/java/baekjoon/resolved/Main6080.java
+++ questions/src/main/java/baekjoon/resolved/Main6080.java
@@ -7,9 +7,10 @@ public class Main6080 {
     /*
         https://www.acmicpc.net/user/bcdlife
         https://www.acmicpc.net/problem/6080
+        맞은 후 다른 분 코드 참고!
+        https://www.acmicpc.net/source/82833224
     */
     public static int[][] grass; //풀 높이
-    public static boolean[][] checked; //확인한 곳
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -18,7 +19,6 @@ public class Main6080 {
         int rows = Integer.parseInt(st.nextToken());
         int cols = Integer.parseInt(st.nextToken());
         grass = new int[rows+2][cols+2]; //ArrayIndexOutOfBounds 대비용 크기
-        checked = new boolean[rows+2][cols+2];
 
         //입력
         for(int i=1; i<=rows; i++) {
@@ -32,7 +32,7 @@ public class Main6080 {
         for(int i=1; i<=rows; i++) {
             for(int j=1; j<=cols; j++) {
                 //확인 시작 칸 (i, j)
-                if(grass[i][j] > 0 && !checked[i][j]) {
+                if(grass[i][j] > 0) {
                     check(i, j);
                     count++;
                 }
@@ -48,8 +48,8 @@ public class Main6080 {
         //주변 체크 -- 왼쪽→, 아래쪽↓, 대각선↘ ... 보다 더 다양하게 8방위로
         for(int i=-1; i<=1; i++) {
             for(int j=-1; j<=1; j++) {
-                if(!checked[row+i][col+j] && grass[row+i][col+j] > 0) {
-                    checked[row+i][col+j] = true;
+                if(grass[row+i][col+j] > 0) {
+                    grass[row+i][col+j] = 0;
                     check(row+i, col+j);
                 }
             }
