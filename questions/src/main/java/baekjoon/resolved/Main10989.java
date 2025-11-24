a4ee7bfe1ce8c5070f2984abb3146561fcce02d5
hmkim <elppapie@naver.com>
2025-11-24 22:31:54 +0900

    :zap: 10989번 성능 개선 (?)
    알고리즘 방법론인 countingSort 이나 병합정렬법 사용한 것보다 배열셈?법? 이 더 빠르고 메모리가 덜 쓰인다니...
    하긴 자동정렬이니까...

--- questions/src/main/java/baekjoon/resolved/Main10989.java
+++ questions/src/main/java/baekjoon/resolved/Main10989.java
@@ -1,38 +1,39 @@
 package baekjoon.resolved;
 
 import java.io.*;
-import java.util.*;
 
 public class Main10989 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/10989
+    */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        int[] inputArr = new int[n];
-        int[] outputArr = new int[n];
-        int[] countingSort = new int[10001];
-        for(int i=0; i<n; i++) {
-            inputArr[i] = Integer.parseInt(br.readLine());
-            countingSort[inputArr[i]]++;
+        int[] arr = new int[10_000_001];
+        int max = 0;
+        int min = 10_000_001;
+        while(n-- > 0) {
+            int number = Integer.parseInt(br.readLine());
+            if(max < number) max = number;
+            if(min > number) min = number;
+            arr[number]++;
         }
-        br.close();
-        //CountingSort 방식 사용
-        for(int i=1; i<=10000; i++) {
-            countingSort[i] = countingSort[i-1] + countingSort[i];
-        }
-        for(int i=n-1; i>=0; i--) {
-            int outputIndex = countingSort[inputArr[i]]-1; //이번 인덱스
-            outputArr[outputIndex] = inputArr[i]; //출력배열 특정위치(인덱스)에 값 넣기
-            countingSort[inputArr[i]]--; //같은 수의 다음인덱스 업데이트
-        }
-        
+
         //출력
-        for(int i=0; i<n; i++) {
-            sb.append(outputArr[i]).append("\n");
+        for(int i=min; i<=max; i++) {
+            if(arr[i] > 0) {
+                int count = arr[i];
+                while(count-- > 0) {
+                    sb.append(i).append("\n");
+                }
+            }
         }
         bw.write(sb.toString());
         bw.flush();
         bw.close();
+        br.close();
     }
 }
