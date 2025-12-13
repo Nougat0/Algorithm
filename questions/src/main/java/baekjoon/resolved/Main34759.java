96101d381a8c1e45eec5b68c410780559c5c2202
hmkim <elppapie@naver.com>
2025-12-14 00:41:42 +0900

    👍 34759번 해결

---
+++ questions/src/main/java/baekjoon/resolved/Main34759.java
@@ -0,0 +1,16 @@
+package baekjoon.resolved;
+
+import java.io.*;
+
+public class Main34759 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/34759
+    */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        System.out.println(n * 2);
+        br.close();
+    }
+}
