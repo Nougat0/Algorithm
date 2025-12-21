87c453d5727d341e2b9401ac27c1bf2d58d4d893
hmkim <elppapie@naver.com>
2025-12-21 15:40:47 +0900

    👍 👟 17291번 해결
    += 를 썼어야 했는데 실수로 = 으로 초기화를 해버림

--- questions/src/main/java/baekjoon/resolved/Main17291.java
+++ questions/src/main/java/baekjoon/resolved/Main17291.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 
@@ -10,13 +10,13 @@ public class Main17291 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] count = new int[n+1];
-        int[] death = new int[n+5];
+        int[] count = new int[n+1]; //N년 말에 존재하는 수
+        int[] death = new int[n+5]; //N년에 분열 후 사망 수
         count[1] = 1;
         death[1+3] = 1;
         for(int y=2; y<=n; y++) {
             count[y] = count[y-1] * 2 - death[y];
-            death[y+(y%2 == 0 ? 4 : 3)] = count[y-1];
+            death[y+(y%2 == 0 ? 4 : 3)] += count[y-1];
         }
         System.out.println(count[n]);
         br.close();
