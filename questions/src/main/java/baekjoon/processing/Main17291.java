9fafbef82c5ac57876f27bb72f77262f30fe6d97
hmkim <elppapie@naver.com>
2025-12-21 15:28:36 +0900

    ➡️ 👟 17291번 해결중

---
+++ questions/src/main/java/baekjoon/processing/Main17291.java
@@ -0,0 +1,24 @@
+package baekjoon.processing;
+
+import java.io.*;
+
+public class Main17291 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/17291
+    */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] count = new int[n+1];
+        int[] death = new int[n+5];
+        count[1] = 1;
+        death[1+3] = 1;
+        for(int y=2; y<=n; y++) {
+            count[y] = count[y-1] * 2 - death[y];
+            death[y+(y%2 == 0 ? 4 : 3)] = count[y-1];
+        }
+        System.out.println(count[n]);
+        br.close();
+    }
+}
