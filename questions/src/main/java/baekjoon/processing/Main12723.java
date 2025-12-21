df66cdff2313baca9703392d4dc7224cb1704de7
hmkim <elppapie@naver.com>
2025-12-22 08:48:07 +0900

    :arrow_right: :athletic_shoe: 12723번 해결중

---
+++ questions/src/main/java/baekjoon/processing/Main12723.java
@@ -0,0 +1,46 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+
+public class Main12723 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/12723
+    */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        Integer[][] arr;
+        int testCase = Integer.parseInt(br.readLine());
+        for(int t=1; t<=testCase; t++) {
+            int length = Integer.parseInt(br.readLine());
+            arr = new Integer[2][length];
+            for(int i=0; i<2; i++) {
+                st = new StringTokenizer(br.readLine());
+                for(int j=0; j<length; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            //2가지 방식 모두 시도해봐야 함
+            Arrays.sort(arr[0]);
+            Arrays.sort(arr[1], Collections.reverseOrder());
+            int resultA = 0;
+            for(int i=0; i<length; i++) {
+                resultA += arr[0][i] * arr[1][0];
+            }
+            int resultB = 0;
+            for(int i=length-1; i>=0; i--) {
+                resultB += arr[0][i] * arr[1][i];
+            }
+            sb.append("Case #").append(t).append(": ");
+            sb.append(Math.min(resultA, resultB)).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
