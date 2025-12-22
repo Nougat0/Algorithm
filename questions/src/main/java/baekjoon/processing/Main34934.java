5316e7cf2d938fb63d3332400b689c140bf0edb0
hmkim <elppapie@naver.com>
2025-12-22 16:06:35 +0900

    :arrow_right: 34934번 풀어놓음 (미제출)

---
+++ questions/src/main/java/baekjoon/processing/Main34934.java
@@ -0,0 +1,29 @@
+package baekjoon.processing;
+
+import java.io.*;
+import java.util.*;
+
+public class Main34934 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/34934
+    */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int foundedYear = 0;
+        String foundedName = "";
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int year = Integer.parseInt(st.nextToken());
+            if(year > foundedYear) {
+                foundedYear = year;
+                foundedName = name;
+            }
+        }
+        System.out.println(foundedName);
+        br.close();
+    }
+}
