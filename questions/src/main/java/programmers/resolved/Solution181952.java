300e84a41448357aff45245387f05cf37b4cf6ef
hmkim <elppapie@naver.com>
2026-01-09 13:31:55 +0900

    👍 :bird: 문자열 출력하기
    첫 프로그래머스 문제 (입문)

---
+++ questions/src/main/java/programmers/resolved/Solution181952.java
@@ -0,0 +1,15 @@
+package programmers.resolved;
+
+import java.util.Scanner;
+
+public class Solution181952 {
+    /*
+        프로그래머스는 공개 계정이라는 것이 없다
+        https://school.programmers.co.kr/learn/courses/30/lessons/181952
+    */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(str);
+    }
+}
