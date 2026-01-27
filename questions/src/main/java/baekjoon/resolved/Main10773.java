0bcfe041b1cb772e22047b1b9b49983a0638f37c
hmkim <elppapie@naver.com>
2026-01-27 09:33:52 +0900

    :zap: :two: 10773번 리팩토링
    직접 Stack을 구현해서 써봤는데 메모리와 시간 조금씩만 줄어들고... 드라마틱한 변화는 없었음
    
    102344821	bcdlife	 10773	맞았습니다!!	22964	196	Java 11 / 수정	1096	1분 전
    102344567	bcdlife	 10773	맞았습니다!!	23180	204	Java 11 / 수정	751	13분 전

--- questions/src/main/java/baekjoon/resolved/Main10773.java
+++ questions/src/main/java/baekjoon/resolved/Main10773.java
@@ -1,7 +1,6 @@
 package baekjoon.resolved;
 
 import java.io.*;
-import java.util.*;
 
 public class Main10773 {
     /*
@@ -11,15 +10,35 @@ public class Main10773 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int k = Integer.parseInt(br.readLine());
-        Stack<Integer> book = new Stack<>();
+        Stack s = new Stack(k);
         int sum = 0;
         for(int i=0; i<k; i++) {
             int num = Integer.parseInt(br.readLine());
             //문제에서 EmptyStackException 발생할 일이 없음을 보장함
-            if(num == 0) sum -= book.pop();
-            else sum += book.push(num);
+            if(num == 0) sum -= s.pop();
+            else sum += s.push(num);
         }
         System.out.println(sum);
         br.close();
+    }
+
+    //직접 구현 시도
+    public static class Stack {
+        int current;
+        int[] arr;
+
+        public Stack(int k) {
+            this.current = -1;
+            this.arr = new int[k];
+        }
+
+        public int pop() {
+            return arr[current--];
+        }
+
+        public int push(int n) {
+            arr[++current] = n;
+            return n;
+        }
     }
 }
