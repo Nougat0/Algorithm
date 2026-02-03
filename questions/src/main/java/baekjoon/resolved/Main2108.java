7a41360deffec7c45aa3c3054184371bd4de53d1
hmkim <elppapie@naver.com>
2026-02-03 19:18:59 +0900

    :zap: :two: 2108번 성능 개선
    정렬 로직 없앴더니 시간 거의 반절 줄었다~~~ 메모리는 거의 그대로
    제출 번호	아이디	문제	결과	메모리	시간	언어	코드 길이	제출한 시간
    102592026	bcdlife	 2108	맞았습니다!!	40644	272	Java 11 / 수정	2120	58초 전
    102591633	bcdlife	 2108	맞았습니다!!	48352	500	Java 11 / 수정	1865	14분 전

--- questions/src/main/java/baekjoon/resolved/Main2108.java
+++ questions/src/main/java/baekjoon/resolved/Main2108.java
@@ -1,7 +1,6 @@
 package baekjoon.resolved;
 
 import java.io.*;
-import java.util.*;
 
 public class Main2108 {
     /*
@@ -14,36 +13,42 @@ public class Main2108 {
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         int[] mo = new int[8_001]; // -4000 ~ 0 ~ 4000
-        int[] no = new int[n];
         double sum = 0;
+        int min = 4_001, max = -4_001;
         for(int i=0; i<n; i++) {
             int num = Integer.parseInt(br.readLine());
+            if(min > num) min = num;
+            if(max < num) max = num;
             sum += num;
             mo[num + 4_000]++; //보정 계수 4_000
-            no[i] = num;
         }
 
-        Arrays.sort(no);
-        int min = no[0];
-        int max = no[n-1];
         double avg = sum / n;
-        int center = no[n/2]; //N은 홀수
+        int center = -4_001;
         int[] most = new int[2]; //최빈값 최소값 2번째까지 확인
         int range = max - min;
 
-        int count = 0;
+        final int MID = n/2 + 1;
+        int moCount = 0, count = 0, former;
         int index = 0;
         for(int i=min; i<=max; i++) {
-            if(mo[i+4_000] > count) {
+            //1) 중앙값 도출
+            if(center == -4_001) {
+                former = count;
+                count += mo[i+4_000];
+                if(former < MID && count >= MID) center = i;
+            }
+            //2) 최빈값 도출
+            if(mo[i+4_000] > moCount) {
                 //최빈값 갱신
                 index = 0;
                 most[index++] = i;
-                count = mo[i+4_000];
-            } else if(mo[i+4_000] == count){
+                moCount = mo[i+4_000];
+            } else if(mo[i+4_000] == moCount){
                 //동일 최빈값 2개까지만 저장
                 if(index < 2) {
                     most[index++] = i;
-                    count = mo[i+4_000];
+                    moCount = mo[i+4_000];
                 }
             }
         }
