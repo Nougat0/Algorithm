69f3d3f025278f10ec19d589c05789ed1a37b027
hmkim <elppapie@naver.com>
2026-02-22 00:31:47 +0900

    👍 11942번 해결

---
+++ questions/src/main/java/baekjoon/resolved/Main11942.java
@@ -0,0 +1,13 @@
+package baekjoon.resolved;
+
+import java.io.*;
+
+public class Main11942 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/11942
+    */
+    public static void main(String[] args) throws IOException {
+        System.out.println("고려대학교");
+    }
+}
