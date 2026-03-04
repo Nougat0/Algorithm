2159cad93853b446011ed65b2b01811eeb179cf6
hmkim <elppapie@naver.com>
2026-03-04 19:40:08 +0900

    👍 :athletic_shoe: 15151번 해결
    그냥 단순 구현으로 해결....

--- questions/src/main/java/baekjoon/resolved/Main15151.java
+++ questions/src/main/java/baekjoon/resolved/Main15151.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
@@ -12,8 +12,9 @@ public class Main15151 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int k = Integer.parseInt(st.nextToken()); //소설 제작 기간 (초기값)
-        int death = Integer.parseInt(st.nextToken()) + 1; //사망 일자
-        int novelsWritten = 1;
+        int d = Integer.parseInt(st.nextToken()); //사망 일자 (작성 경과 일자)
+        int daysPassed = 0;
+        int novels = 0;
         /**
          * death(d+1)일째에 사망 예정 -> 그 당시 작성 완료 권수 확인
          * 1일 걸림 + 1일 후 사망(2일차) = 1권
@@ -29,11 +30,17 @@ public class Main15151 {
          *  k+1 k+2k+1  k+2k+4k+1   k+2k+4k+8k+1    (작성완 기준 일자 계산)
          *  0   0+1     0+1+2       0+1+2+3         (2^)
          *  지수의 합 공식: n^0 + n^1 + ... + n^l = n^(l+1) - 1
+         *  
+         *  결국 이 방식으로는 답을 못 찾음... ㅠ 너무 복잡하게 생각했나봄
+         *  구현 방식으로 더하면서 진행
          */
-        while(Math.pow(2, novelsWritten+1) * k <= death) {
-            novelsWritten++;
+        while(true) {
+            daysPassed += k;
+            if(daysPassed > d) break;
+            k *= 2;
+            novels++;
         }
-        System.out.println(novelsWritten);
+        System.out.println(novels);
         br.close();
     }
 }
