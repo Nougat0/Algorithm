81b1e3f1d97cc2c57b9dfe01c7ad12d5f391d138
hmkim <elppapie@naver.com>
2026-04-03 08:38:46 +0900

    👍 :athletic_shoe: 30873번 해결

---
+++ questions/src/main/java/baekjoon/resolved/Main30873.java
@@ -0,0 +1,25 @@
+package baekjoon.resolved;
+
+import java.io.*;
+import java.util.*;
+
+public class Main30873 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/30873
+    */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int x = 0;
+        for(int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int p = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            if(Math.abs(p - x) <= c) x++;
+        }
+        System.out.println(x);
+        br.close();
+    }
+}
