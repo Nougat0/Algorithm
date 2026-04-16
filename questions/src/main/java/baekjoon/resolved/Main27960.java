1632f03219e2e7352cb04f99b2f463b512403559
hmkim <elppapie@naver.com>
2026-04-16 10:50:16 +0900

    👍 :athletic_shoe: 27960번 해결
    채점 3시간 동안 기다리다가 자러 갔는데 그새 채점이 진행돼서 맞았음 휴~

--- questions/src/main/java/baekjoon/resolved/Main27960.java
+++ questions/src/main/java/baekjoon/resolved/Main27960.java
@@ -1,4 +1,4 @@
-package baekjoon.processing;
+package baekjoon.resolved;
 
 import java.io.*;
 import java.util.*;
@@ -15,33 +15,21 @@ public class Main27960 {
         int b = Integer.parseInt(st.nextToken());
         int c = 0;
 
-        if(a > 0 && b > 0) {
-            boolean[] aShot = getShotList(a);
-            boolean[] bShot = getShotList(b);
+        String aShot = getShotList(a);
+        String bShot = getShotList(b);
 
-            for(int i=0; i<10; i++) {
-                if(aShot[i] ^ bShot[i]) {
-                    c += biPow(i);
-                }
+        for(int i=0; i<10; i++) {
+            if(aShot.charAt(9 - i) != bShot.charAt(9 - i)) {
+                c += biPow(i);
             }
-        } else {
-            // 한쪽이라도 0점인 경우 기존 로직으로 오류 발생
-            c = Math.max(a, b);
         }
         System.out.println(c);
         br.close();
     }
 
-    public static boolean[] getShotList(int score) {
-        boolean[] shot = new boolean[10];
-        for(int i=9; i>=0; i--) {
-            int pow = biPow(i);
-            if(score > pow) {
-                score -= pow;
-                shot[i] = true;
-            }
-        }
-        return shot;
+    public static String getShotList(int score) {
+        String shotList = Integer.toBinaryString(score);
+        return "0".repeat(10 - shotList.length()) + shotList;
     }
 
     // Math.pow 로 double 결과 받는 게 싫어서 직접 만들어 사용함
