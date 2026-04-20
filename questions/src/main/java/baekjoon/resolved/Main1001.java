3a6e9ef0899cbeecb475b1efc5257e0cf9396bf6
hmkim <elppapie@naver.com>
2026-04-20 20:57:40 +0900

    :package: 1001번 제출 백업

---
+++ questions/src/main/java/baekjoon/resolved/Main1001.java
@@ -0,0 +1,19 @@
+package baekjoon.resolved;
+
+import java.util.Scanner;
+
+public class Main1001 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/1001
+        https://www.acmicpc.net/source/50161730
+        2022.10.06
+    */
+    public static void main(String [] agrs){
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.print(num1-num2);
+    }
+}
