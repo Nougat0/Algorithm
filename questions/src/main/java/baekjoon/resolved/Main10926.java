5fe23b3dcb76c9aa79521043f6a61e148b18d071
hmkim <elppapie@naver.com>
2026-04-20 20:54:39 +0900

    :package: 10926번 제출 백업

---
+++ questions/src/main/java/baekjoon/resolved/Main10926.java
@@ -0,0 +1,18 @@
+package baekjoon.resolved;
+
+import java.util.Scanner;
+
+public class Main10926 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/10926
+        https://www.acmicpc.net/source/50372077
+        2022.10.11
+    */
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        String nickname = new String();
+        nickname = sc.nextLine();
+        System.out.println(nickname+"??!");
+    }
+}
