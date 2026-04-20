71d99e0641c87960c086a3ddbbb1ed43276056d1
hmkim <elppapie@naver.com>
2026-04-20 19:27:53 +0900

    :package: 2231번 제출 백업

--- questions/src/main/java/baekjoon/resolved/Main2231.java
+++ questions/src/main/java/baekjoon/resolved/Main2231.java
@@ -3,6 +3,11 @@ package baekjoon.resolved;
 import java.io.*;
 
 public class Main2231 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/2231
+        https://www.acmicpc.net/source/79525029
+    */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -11,12 +16,11 @@ public class Main2231 {
         int constructor = 0;
 
         for(int i=1; i<=n; i++) {
-            if(getDisassemble(i) == n) {
+            if(getDisassemble(i) == n) { //생성자 확인
                 constructor = i;
                 break;
             }
         }
-
         bw.write(String.format("%d%n", constructor));
         bw.flush();
         bw.close();
@@ -25,14 +29,14 @@ public class Main2231 {
 
     /**
      * 분해합 구하기
-     * @param n
-     * @return
+     * @param n (생성자)
+     * @return disassemble n의 분해합
      */
     public static int getDisassemble(int n) {
         int disassemble = n;
-        char[] digits = String.valueOf(n).toCharArray();
-        for(char number: digits) {
-            disassemble += Character.getNumericValue(number);
+        while(n > 0) {
+            disassemble += n % 10; //뒷자리부터 더하기
+            n /= 10;
         }
         return disassemble;
     }
