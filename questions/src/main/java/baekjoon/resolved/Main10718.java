cc36f9d0072e1171edf175a98aa710c6ef6578ba
hmkim <elppapie@naver.com>
2026-04-20 20:59:19 +0900

    :package: 10718번 제출 백업

---
+++ questions/src/main/java/baekjoon/resolved/Main10718.java
@@ -0,0 +1,16 @@
+package baekjoon.resolved;
+
+import java.util.*;
+
+public class Main10718 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/10718
+        https://www.acmicpc.net/source/50161570
+        2022.10.03
+    */
+    public static void main(String args[]){
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
