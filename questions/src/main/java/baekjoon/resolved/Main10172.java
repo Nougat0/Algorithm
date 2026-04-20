df28acff424f686f4eaa6e5fcd4b5b1fe066867c
hmkim <elppapie@naver.com>
2026-04-20 20:49:22 +0900

    :package: 10172번 제출 백업

---
+++ questions/src/main/java/baekjoon/resolved/Main10172.java
@@ -0,0 +1,17 @@
+package baekjoon.resolved;
+
+public class Main10172 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/10172
+        https://www.acmicpc.net/source/50449057
+        2022.10.12
+    */
+    public static void main(String[]args){
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
