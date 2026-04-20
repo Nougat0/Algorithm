76114aed09ddebaa4e73fdcc2baed35673c76b5c
hmkim <elppapie@naver.com>
2026-04-20 18:57:21 +0900

    :arrow_right: :athletic_shoe: 15360번 해결중
    RunTime Error (/ by zero)

---
+++ questions/src/main/java/baekjoon/processing/Main15360.java
@@ -0,0 +1,40 @@
+package baekjoon.processing;
+
+import java.io.*;
+
+public class Main15360 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/15360
+    */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int length = Integer.parseInt(br.readLine()); // 1~length
+        int lights = Integer.parseInt(br.readLine());
+        int updown = Integer.parseInt(br.readLine());
+        final int range = updown*2;
+        int covered = 0;
+        int neededTotal = 0;
+        for(int i=0; i<lights; i++) {
+            int position = Integer.parseInt(br.readLine());
+            int min = position - updown;
+            int max = position + updown;
+            if(min - covered > 1) {
+                //비어있는 공간 너비 계산
+                int distance = min - covered;
+                int neededCount = distance / range + (distance % range > 0 ? 1 : 0);
+                neededTotal +=  neededCount;
+            }
+            covered = max;
+        }
+        //마지막 가로등과 거리의 끝 비교
+        if(length > covered) {
+            //비어있는 공간 너비 계산
+            int distance = length - covered;
+            int neededCount = distance / range + (distance % range > 0 ? 1 : 0);
+            neededTotal += neededCount;
+        }
+        System.out.println(neededTotal);
+        br.close();
+    }
+}
