3ca56a80bceadc6ec083446a01ce25619d301a99
hmkim <elppapie@naver.com>
2026-04-20 20:33:57 +0900

    :package: 11654번 제출 백업

---
+++ questions/src/main/java/baekjoon/resolved/Main11654.java
@@ -0,0 +1,17 @@
+package baekjoon.resolved;
+
+import java.util.*;
+
+public class Main11654 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/11654
+        https://www.acmicpc.net/source/50648057
+        2022.10.17
+    */
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String what = sc.nextLine();
+        System.out.print((int)(what.charAt(0)));
+    }
+}
