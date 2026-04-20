d35ba5c75d1cf021c68316bd7f2449461651ebba
hmkim <elppapie@naver.com>
2026-04-20 21:01:04 +0900

    :package: 2557번 백업 - 백준 최초 제출 Source

---
+++ questions/src/main/java/baekjoon/resolved/Main2557.java
@@ -0,0 +1,13 @@
+package baekjoon.resolved;
+
+public class Main2557 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/2557
+        https://www.acmicpc.net/source/50029139
+        2022.10.03
+
+        백준 최초 제출 Source
+    */
+    System.out.println("Hello World!");
+}
