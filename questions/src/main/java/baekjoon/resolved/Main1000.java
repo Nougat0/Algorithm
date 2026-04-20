a5485fb77827255a4c334c6a833525a1df1dd835
hmkim <elppapie@naver.com>
2026-04-20 20:58:18 +0900

    :package: 1000번 제출 백업

---
+++ questions/src/main/java/baekjoon/resolved/Main1000.java
@@ -0,0 +1,20 @@
+package baekjoon.resolved;
+
+import java.util.Scanner;
+
+public class Main1000 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/1001
+        https://www.acmicpc.net/source/50161570
+        2022.10.06
+    */
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int firstNum = sc.nextInt();
+        int secondNum = sc.nextInt();
+
+        System.out.println(firstNum+secondNum);
+
+    }
+}
