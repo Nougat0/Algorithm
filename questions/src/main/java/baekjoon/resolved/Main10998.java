24122007f197d545836fa6905783ef6e14773dd3
hmkim <elppapie@naver.com>
2026-04-20 20:57:08 +0900

    :package: 10998번 제출 백업

---
+++ questions/src/main/java/baekjoon/resolved/Main10998.java
@@ -0,0 +1,19 @@
+package baekjoon.resolved;
+
+import java.util.Scanner;
+
+public class Main10998 {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/problem/10998
+        https://www.acmicpc.net/source/50164764
+        2022.10.06
+    */
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.print(num1*num2);
+    }
+}
