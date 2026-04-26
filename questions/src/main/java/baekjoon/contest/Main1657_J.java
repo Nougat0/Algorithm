671c3394ced38d3ee62ca9a78823cadbb8291eda
hmkim <elppapie@naver.com>
2026-04-26 20:46:37 +0900

    👍 :trophy: Good Bye, BOJ! - J번 해결 *방명록*

---
+++ questions/src/main/java/baekjoon/contest/Main1657_J.java
@@ -0,0 +1,26 @@
+package baekjoon.contest;
+
+import java.io.*;
+import java.util.*;
+
+public class Main1657_J {
+    /*
+        https://www.acmicpc.net/user/bcdlife
+        https://www.acmicpc.net/contest/problem/1657/10
+    */
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        sb.append("/****************************************************************************").append("\n");
+        sb.append(" * [ 와 방명록 문제다! ]").append("\n");
+        sb.append(" * 그동안 정말 감사했습니다 백준님!").append("\n");
+        sb.append(" * 알고리즘 문제에 대한 진입장벽을 없애준 고마운 BOJ 였는데... 없어진다니 너무 아쉽네요").append("\n");
+        sb.append(" * 2022년도 개발자 교육 받으면서 처음 접해서 다른 분들에 비해 오래 사용하지는 못했지만").append("\n");
+        sb.append(" * 안녕은 영원한 헤어짐은 아니겠지요... 언젠가 다시 만날 수 있기를 바라요").append("\n");
+        sb.append(" * - bcdlife").append("\n");
+        sb.append(" ***************************************************************************/").append("\n");
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
